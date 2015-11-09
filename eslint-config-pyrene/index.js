4f9fbe7172823ade2328acce61f05d083f166405
mie <mie@open.ch>
2015-11-09 19:38:27 +0100

    added custom indent and react rules to override airbnb style

--- eslint-config-pyrene/index.js
+++ eslint-config-pyrene/index.js
@@ -3,7 +3,23 @@ module.exports = {
   extends: 'eslint-config-airbnb',
 
   rules: {
+    // this option sets a specific tab width for your code, VariableDeclarator indents are for nice alignment of multi-line var declaration
+    "indent": [2, 2, { "SwitchCase": 2, "VariableDeclarator": { "var": 2, "let": 2, "const": 3 } }],
 
+    // Prevent missing displayName in a React component definition
+    "react/display-name": 1,
+    // Enforce boolean attributes notation in JSX
+    "react/jsx-boolean-value": [2, "never"],
+    // Enforce or disallow spaces inside of curly braces in JSX attributes
+    "react/jsx-curly-spacing": [2, "never"],
+    // Prevent duplicate props in JSX
+    "react/jsx-no-duplicate-props": 2,
+    // Prevent React to be incorrectly marked as unused - covered by no-unused-vars
+    "react/jsx-uses-react": 0,
+    // Prevent usage of dangerous JSX properties
+    "react/no-danger": 2,
+    // Prevent multiple component definition per file
+    "react/no-multi-comp": [2, "ignoreStateless"]
   }
 
 };
