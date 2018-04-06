fce6ae4e99d81e7055d8c606168304eeddd4886c
mie <mie@open.ch>
2018-04-06 10:20:47 +0200

    add style-loader to webpack

--- pyrene/webpack.config.babel.js
+++ pyrene/webpack.config.babel.js
@@ -25,6 +25,7 @@ const config = {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
+          'style-loader',
           {
             loader: 'css-loader',
             options: {
