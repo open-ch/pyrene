d2a8ddb64fb5d931d636de24e13f618685516f90
lucletruc <lucas.neubauer@me.com>
2018-04-26 11:32:38 +0200

    Spacing in buttonBar with spacer elements.

--- pyrene/src/components/ButtonBar/ButtonBar.jsx
+++ pyrene/src/components/ButtonBar/ButtonBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import './buttonBar.css';
 
@@ -6,14 +6,24 @@ import './buttonBar.css';
 const ButtonBar = props => (
   <div styleName={'buttonBar'}>
     <div styleName={'leftButtonSection'}>
-      {props.leftButtonSectionElements.map(element =>
-        <element.type {...element.props} key={`${element.type}${element.props.label}${element.props.type}`} />
-      )}
+      {props.leftButtonSectionElements.map((element) => {
+        return (
+          <Fragment>
+            <element.type {...element.props} key={`${element.type}${element.props.label}${element.props.type}`} />
+            <div styleName={'spacer'} />
+          </Fragment>
+        );
+      })}
     </div>
     <div styleName={'rightButtonSection'}>
-      {props.rightButtonSectionElements.map(element =>
-        <element.type {...element.props} key={`${element.props.label}${element.props.type}${element.type}`} />
-      )}
+      {props.rightButtonSectionElements.map((element, index) => {
+        return (
+          <Fragment>
+            {index !== 0 && <div styleName={'spacer'} />}
+            <element.type {...element.props} key={`${element.props.label}${element.props.type}${element.type}`} />
+          </Fragment>
+        );
+      })}
     </div>
   </div>
 );
