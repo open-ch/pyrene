8cf4c0e26b6f6e1eaaaf9720962fe80af2a7649a
lucletruc <lucas.neubauer@me.com>
2018-05-23 15:49:24 +0200

    Resolved renaming error.

--- kitchensink/src/app/pages/foundations/ColorsPage.jsx
+++ kitchensink/src/app/pages/foundations/ColorsPage.jsx
@@ -6,7 +6,9 @@ const ColorsPage = ({ match }) => (
     <div styleName="header">
       <div styleName="title">Colors</div>
       <div styleName="description">
-        Selection elements are used primarily on ....
+        Our color palette contains primary, neutrals, interaction colors as well as qualitative and sequential color schemes used for data visualizations.
+        They’ve been designed to work harmoniously with each other.
+        The color palette starts with a base color (500) and fills in the spectrum to create a complete and usable palette.
       </div>
     </div>
 
