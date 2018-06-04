62d2a639d56e8658c10d900ef17057df8d77c16f
lucletruc <lucas.neubauer@me.com>
2018-06-04 10:07:56 +0200

    Created Utils for generally useable functions.

--- kitchensink/src/app/common/PageElements/FoundationElements/IconBox/IconBox.jsx
+++ kitchensink/src/app/common/PageElements/FoundationElements/IconBox/IconBox.jsx
@@ -1,25 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Utils from '../../../Utils';
 
 import './iconBox.css';
-
-const copyStringToClipboard = (copyString) => {
-  const textarea = document.createElement('textarea');
-  textarea.textContent = copyString;
-  textarea.style.position = 'fixed'; // Prevent scrolling to bottom of page in MS Edge.
-  document.body.appendChild(textarea);
-  textarea.select();
-
-  try {
-    return document.execCommand('copy'); // Security exception may be thrown by some browsers.
-  } catch (ex) {
-    console.warn('Copy to clipboard failed.', ex);
-    return false;
-  } finally {
-    document.body.removeChild(textarea);
-  }
-};
 
 
 export default class IconBox extends React.Component {
@@ -33,7 +17,7 @@ export default class IconBox extends React.Component {
   handleIconClick = (name, downloadable) => {
     if (!downloadable) {
       this.displayCopyNotifier(1000);
-      copyStringToClipboard(name);
+      Utils.copyStringToClipboard(name);
     } else {
       return console.log('start download');
     }
---
+++ kitchensink/src/app/common/Utils.js
@@ -0,0 +1,20 @@
+export default class Utils {
+
+  static copyStringToClipboard(copyString) {
+    const textarea = document.createElement('textarea');
+    textarea.textContent = copyString;
+    textarea.style.position = 'fixed'; // Prevent scrolling to bottom of page in MS Edge.
+    document.body.appendChild(textarea);
+    textarea.select();
+
+    try {
+      return document.execCommand('copy'); // Security exception may be thrown by some browsers.
+    } catch (ex) {
+      console.warn('Copy to clipboard failed.', ex);
+      return false;
+    } finally {
+      document.body.removeChild(textarea);
+    }
+  }
+
+}
