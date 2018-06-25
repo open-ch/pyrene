f3a8638d95eaac83419cd893d5b8ae405b01aa5d
lucletruc <lucas.neubauer@me.com>
2018-06-25 13:49:30 +0200

    Turned off consistent-return rule.

--- eslint-config-pyrene/index.js
+++ eslint-config-pyrene/index.js
@@ -15,6 +15,8 @@ module.exports = {
   },
 
   "rules": {
+    // No need to return something in an arrow function
+    "consistent-return": 0,
     // require trailing commas for multiline (arrays, objects, imports, exports & functions), disallowed for same line
     "comma-dangle": [2, "always-multiline"],
     // require function expressions to have a name
