58e6017fff51817831a711265d63c0d07e41c415
lucletruc <lucas.neubauer@me.com>
2018-07-06 15:33:21 +0200

    Loader sizes implemented

--- pyrene/src/components/Loader/Loader.jsx
+++ pyrene/src/components/Loader/Loader.jsx
@@ -8,8 +8,8 @@ import './loader.css';
  * You spin my head right round..
  */
 const Loader = (props) => (
-  <div styleName={'canvas'}>
-    <div styleName={classNames('pyreneLoader', {[`type-${props.type}`]: true})} />
+  <div styleName={classNames('canvas', {[`size-${props.size}`]: true })}>
+    <div styleName={classNames('pyreneLoader', {[`type-${props.type}`]: true}, {[`size-${props.size}`]: true })} />
   </div>
 );
 
@@ -21,9 +21,13 @@ Loader.defaultProps = {
 
 Loader.propTypes = {
   /**
+   * Sets the size of the loader.
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large', 'xlarge']).isRequired,
+  /**
    * Changes the overall appearance of the loader.
    */
-  type: PropTypes.oneOf(['default', 'inverted'])
+  type: PropTypes.oneOf(['default', 'inverted']),
 };
 
 export default Loader;
