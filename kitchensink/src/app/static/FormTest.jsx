9371b4237ee7b8a7414837cb67fa73ed8e240704
lucletruc <lucas.neubauer@me.com>
2018-07-18 14:54:51 +0200

    Higher Order Component Form

--- kitchensink/src/app/static/FormTest.jsx
+++ kitchensink/src/app/static/FormTest.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
 import '../../css/componentPage.css';
-import { Form, Checkbox, ChildrenForm, Button, TextField } from 'pyrene';
+import { withFormLogic, Checkbox, Button, TextField } from 'pyrene';
 
+const Form = (props) => (
+  <React.Fragment>
+    <Checkbox label={'Male'} {...props.initField('checkBox1', props.errors)} />
+    <Checkbox label={'Female'} {...props.initField('checkBox2', props.errors)} />
+    <Checkbox label={'Helicopter'} {...props.initField('checkBox3', props.errors)} />
+
+    <TextField width={300} placeholder={'Email'} disabled={props.values.checkBox1} {...props.initField('email', props.errors)} />
+    <TextField width={300} placeholder={'Password'} {...props.initField('password', props.errors)} />
+
+    <Button label={'Submit'} type={'danger'} disabled={props.submitDisabled} />
+  </React.Fragment>
+);
+
+function delayAlert(values, ms){
+  console.log('Submitting', values);
+  var ctr, rej, p = new Promise(function (resolve, reject) {
+    ctr = setTimeout(resolve, ms);
+    rej = reject;
+  });
+  p.cancel = function(){ clearTimeout(ctr); rej(Error("Cancelled"))};
+  return p;
+}
+
+const WrappedForm = withFormLogic(Form)({
+  initialValues: {
+    checkBox1: false,
+    checkBox2: true,
+    checkBox3: true,
+    email: 'blablabla',
+    password: 'secure'
+  },
+  validation: (values) => ({
+    email: values.email.length === 0,
+    password: values.password.length === 0,
+    checkBox1: values.checkBox1 === values.checkBox2,
+    checkBox2: false,
+    checkBox3: values.checkBox3 !== values.checkBox2,
+  }),
+  onSubmit: (values) => delayAlert(values, 2000),
+});
 
 const FormTest = () => (
   <div styleName="page">
@@ -10,23 +50,8 @@ const FormTest = () => (
       <div styleName="description">
       </div>
       <div className="topicContent">
-        <Form />
+        <WrappedForm />
       </div>
-
-      <div styleName="title">Children form</div>
-      <ChildrenForm>
-        <Checkbox label={'Male'} />
-        <Checkbox label={'Female'} />
-        <div>Test 123
-          <Checkbox label={'Helicopter'} />
-          <Checkbox label={'Unicorn'} />
-          <div>
-            <TextField placeholder={'Nested'} width={300} />
-          </div>
-        </div>
-        <TextField placeholder={'Email'} width={300} />
-        <TextField placeholder={'password'} width={300} />
-      </ChildrenForm>
     </div>
   </div>
 );
