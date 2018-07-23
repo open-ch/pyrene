d273c8f2a43daf0ccb9e435ee97a581e2480a700
lucletruc <lucas.neubauer@me.com>
2018-07-23 16:43:53 +0200

    Changed aria label for consistency

--- pyrene/src/components/Collapsible/Collapsible.jsx
+++ pyrene/src/components/Collapsible/Collapsible.jsx
@@ -36,7 +36,7 @@ export default class Collapsible extends React.Component {
   render() {
     return (
       <div styleName={classNames('collapsibleBox', {expanded: this.state.expanded})}>
-        <div styleName={'collapsibleButton'} onClick={this.toggleCollapse} role="button" aria-label="collapsible button">
+        <div styleName={'collapsibleButton'} onClick={this.toggleCollapse} role="button" aria-label="Show or hide content">
           <div styleName={"centeringBox"}>
             {this.state.expanded && this.props.titleExpanded ? this.props.titleExpanded : this.props.title}
             <span className={'icon-collapsDown'} styleName={'collapsArrow'}/>
