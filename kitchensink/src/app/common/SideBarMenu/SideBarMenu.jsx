7c9059848c0619a5228716272f0b3f8be0bd4b0e
lucletruc <lucas.neubauer@me.com>
2018-07-26 09:06:58 +0200

    Removed hoc

--- kitchensink/src/app/common/SideBarMenu/SideBarMenu.jsx
+++ kitchensink/src/app/common/SideBarMenu/SideBarMenu.jsx
@@ -18,12 +18,19 @@ const SideBarMenu = props => (
       <SideBarMenuSection
         title="Components"
         sectionElements={
+<<<<<<< Updated upstream
           Object.values(Components)
             .filter(component => component.hoc !== true)
             .map((component) => {
               const lowercaseComponentName = component.displayName.replace(/\s/g, '').toLowerCase();
               return ({ name: component.displayName, linkToPath: `/${lowercaseComponentName}` });
             }).sort((a, b) => ((a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)))}
+=======
+          Object.values(Components)git .map((component) => {
+            const lowercaseComponentName = component.displayName.replace(/\s/g, '').toLowerCase();
+            return ({ name: component.displayName, linkToPath: `/${lowercaseComponentName}` });
+          }).sort((a, b) => ((a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)))}
+>>>>>>> Stashed changes
       />
       <SideBarMenuSection title="Resources" sectionElements={[]} linkToPath={'/resources'} />
       <SideBarMenuSection title="Form" sectionElements={[]} linkToPath={'/test'} />
