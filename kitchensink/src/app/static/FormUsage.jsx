57755c11b072a5af8f3d2aa7c40ad4c0e6067779
lucletruc <lucas.neubauer@me.com>
2018-08-08 17:12:47 +0200

    Feedback improvements

--- kitchensink/src/app/static/FormUsage.jsx
+++ kitchensink/src/app/static/FormUsage.jsx
@@ -25,49 +25,44 @@ const errorStyle = {
   marginTop: 4,
   fontSize: 12,
   fontWeight: 600,
+  width: 300,
   textAlign: 'left',
   color: 'var(--red-500)',
 };
 
 const Form = props => (
   <React.Fragment>
-    <div style={{
-      display: 'flex',
-      width: 300,
-      justifyContent: 'space-between',
-    }}
-    >
-      <Checkbox label={'Male'} {...props.initField('checkBox1')} />
-      <Checkbox label={'Female'} {...props.initField('checkBox2')} />
-      <Checkbox label={'Apache Helicopter'} {...props.initField('checkBox3')} />
-    </div>
-    {props.errors.checkBox1 && <div style={errorStyle}>{props.errors.checkBox1}</div>}
-    {props.errors.checkBox3 && <div style={errorStyle}>{props.errors.checkBox3}</div>}
-
-    <div style={{ height: 24 }} />
-
     <TextField width={300} title={'Email'} placeholder={'Email'} disabled={props.values.checkBox1} {...props.initField('email')} />
     <div style={{ height: 24 }} />
-    <TextField width={300} title={'Password in plain text'} placeholder={'Password'} {...props.initField('password')} />
+    <TextField width={300} title={'Password in plain text 👀'} placeholder={'Password'} {...props.initField('password')} />
     <div style={{ height: 24 }} />
 
-
-    <div style={{ height: 24 }} />
     <div style={{ width: 300 }}>
       <RadioGroup
-        alignment={'vertical'}
-        radioLabels={['option 1', 'option 2', 'option 3', 'option 22', 'option 32']}
+        alignment={'horizontal'}
+        radioLabels={['Beer', 'Coffee', 'Coffeebeer']}
         {...props.initField('radioGroup')}
       />
-      {props.errors.radioGroup && <div style={errorStyle}>{props.errors.radioGroup}</div>}
+      {props.errors.radioGroup && props.touched.radioGroup && <div style={errorStyle}>{props.errors.radioGroup}</div>}
     </div>
 
     <div style={{ height: 24 }} />
     <div style={{ width: 300 }}>
-      <SingleSelect title={'Select favorite icecream'} options={testOptionsWithoutInvalid} clearable {...props.initField('select')} />
+      <SingleSelect title={'Select favorite icecream'} options={testOptionsWithoutInvalid} clearable {...props.initField('iceSelect')} />
       <div style={{ height: 24 }} />
-      <MultiSelect title={'Select icecream.. again'} options={testOptionsWithoutInvalid} creatable clearable keepMenuOnSelect {...props.initField('multiselect1')} />
+      <MultiSelect title={'Select icecream.. again'} options={testOptionsWithoutInvalid} creatable clearable keepMenuOnSelect {...props.initField('iceMultiselect')} />
     </div>
+
+    <div style={{ height: 24 }} />
+    <div style={{
+      width: 300,
+    }}
+    >
+      <Checkbox label={'I accept the terms of conditioners.'} {...props.initField('terms')} />
+      <Checkbox label={'Hit me with them juicy spam mails.'} {...props.initField('spam')} />
+    </div>
+    {props.errors.terms && props.touched.terms && <div style={errorStyle}>{props.errors.terms}</div>}
+    {props.errors.spam && <div style={errorStyle}>{props.errors.spam}</div>}
 
     <div style={{ height: 24 }} />
     <Button label={'Submit'} type={'danger'} disabled={props.submitDisabled} loading={props.isSubmitting} />
@@ -81,37 +76,50 @@ const validationSchema = yup.object({
   password: yup.string()
     .required('Password is required.')
     .min(8, 'Needs at least 8 chars.')
-    .matches(/[a-z]/, 'Needs one lowercase char.')
     .matches(/[A-Z]/, 'Needs one uppercase char.'),
 
-  checkBox3: yup.boolean(),
-  checkBox1: yup.boolean()
-    .when('checkBox3', {
+  radioGroup: yup.string()
+    .required('Need to choose one option.'),
+
+  // select needs nullable to cover case where user only touches the empty select
+  iceSelect: yup.string()
+    .required('You need to choose an icecream!')
+    .test('isVanilla', 'Only vanilla is the real deal..', value => value === 'vanilla')
+    .nullable(),
+
+  // Validation structure for multiselects
+  iceMultiselect: yup.array().of(yup.object().shape({
+    value: yup.string()
+      .required()
+      .matches(/[a]/, 'An icecream without an A is not A-cream!')
+      .matches(/[i]/, 'An icecream without an I is not I-cecream!'),
+  })),
+
+  // conditional validation for spam checkbox depending on terms checkbox
+  terms: yup.boolean().test('acceptTerms', 'You need to accept the terms of conditioners for nicer hair!', value => value === true),
+  spam: yup.boolean()
+    .when('terms', {
       is: true,
-      then: yup.boolean().oneOf([true], 'If helicopter, you have to be male.'),
+      then: yup.boolean().oneOf([true], 'Our terms and conditions need you to sign up to spam mails. 🙂'),
     }),
 });
 
 
 const WrappedForm = withFormLogic(Form)({
   initialValues: {
-    checkBox1: false,
-    checkBox2: true,
-    checkBox3: true,
     email: 'blabl@abla.com',
     password: '',
     radioGroup: '',
-    select: null,
-    multiselect1: [],
+    iceSelect: null,
+    iceMultiselect: [],
+    terms: false,
+    spam: false,
   },
   validationSchema: validationSchema,
-  multiSelectOptionValidation: (multiSelectName, values, selectedOption) => {
-    return /a/g.test(selectedOption.value);
-  },
   onSubmit: values => delayAlert(values, 2000),
   onChange: (values, setFieldValue) => {
-    if (values.select === 'vanilla') {
-      setFieldValue('radioGroup', 'option 3');
+    if (values.iceSelect === 'vanilla') {
+      setFieldValue('radioGroup', 'coffeebeer');
     }
   },
 });
@@ -211,11 +219,6 @@ const FormCode2 = `const WrappedForm = withFormLogic(Form)({
   onSubmit: (values) => null, 
   
   // Conditionally change other field values
-  /* Example
-  if (values.select === 'vanilla') {
-      setFieldValue('radioGroup', 'option 3');
-  }
-  */
   onChange: (values, setFieldValue) => null,
 });`;
 
@@ -232,15 +235,31 @@ const Yupscheme = `const validationSchema = yup.object({
   password: yup.string()
     .required('Password is required.')
     .min(8, 'Needs at least 8 chars.')
-    .matches(/[a-z]/, 'Needs one lowercase char.')
     .matches(/[A-Z]/, 'Needs one uppercase char.'),
 
-  // conditional validation for checkbox 1 depending on checkbox 2
-  checkBox2: yup.boolean(),
-  checkBox1: yup.boolean()
-    .when('checkBox2', {
+  radioGroup: yup.string()
+    .required('Need to choose one option.'),
+
+  // select needs nullable to cover case where user only touches the empty select
+  iceSelect: yup.string()
+    .required('You need to choose an icecream!')
+    .test('isVanilla', 'Only vanilla is the real deal..', value => value === 'vanilla')
+    .nullable(),
+
+  // Validation structure for multiselects
+  iceMultiselect: yup.array().of(yup.object().shape({
+    value: yup.string()
+      .required()
+      .matches(/[a]/, 'An icecream without an A is not A-cream!')
+      .matches(/[i]/, 'An icecream without an I is not I-cecream!'),
+  })),
+
+  // conditional validation for spam checkbox depending on terms checkbox
+  terms: yup.boolean().test('acceptTerms', 'You need to accept the terms of conditioners for nicer hair!', value => value === true),
+  spam: yup.boolean()
+    .when('terms', {
       is: true,
-      then: yup.boolean().oneOf([true], 'Error message'),
+      then: yup.boolean().oneOf([true], 'Our terms and conditions need you to sign up to spam mails. 🙂'),
     }),
 });`;
 
@@ -254,6 +273,10 @@ const CustomValidation = `const WrappedForm = withFormLogic(Form)({
     checkBox: values.checkBox === values.checkBox ? 'Must choose male or female' : null,
   }),
   ...
+  multiSelectOptionValidation: (multiSelectName, values, selectedOption) => {
+    return /a/g.test(selectedOption.value);
+  },
+  ...
 });`;
 
 const ErrorDisplay = `const Form = (props) => (
@@ -264,25 +287,91 @@ const ErrorDisplay = `const Form = (props) => (
     />
     
     // Display errors for the checkbox
-    {props.errors.checkBox && <div style={errorStyle}>{props.errors.checkBox}</div>}
+    {props.errors.checkBox && props.touched.checkBox && <div>{props.errors.checkBox}</div>}
   ...
 );`;
 
-const MultiSelectValidation = `const WrappedForm = withFormLogic(Form)({
-  ...
-  multiSelectOptionValidation: (multiSelectName, values, selectedOption) => {
-    return /a/g.test(selectedOption.value);
+const BigForm = `// Code for the form above
+const Form = props => (
+  <React.Fragment>
+    <TextField
+      title={'Email'}
+      placeholder={'Email'}
+      disabled={props.values.checkBox1}
+      {...props.initField('email')}
+     />
+     
+    <TextField 
+      title={'Password in plain text 👀'}
+      placeholder={'Password'}
+      {...props.initField('password')}
+    />
+   
+    <RadioGroup
+      alignment={'horizontal'}
+      radioLabels={['Beer', 'Coffee', 'Coffeebeer']}
+      {...props.initField('radioGroup')}
+    />
+    {props.errors.radioGroup && props.touched.radioGroup && <div>{props.errors.radioGroup}</div>}
+
+    <SingleSelect
+      title={'Select favorite icecream'}
+      options={iceOptions}
+      clearable
+      {...props.initField('iceSelect')}
+    />
+     
+    <MultiSelect
+      title={'Select icecream.. again'}
+      options={iceOptions}
+      creatable
+      clearable
+      keepMenuOnSelect
+      {...props.initField('iceMultiselect')}
+    />
+   
+    <Checkbox label={'I accept the terms of conditioners.'} {...props.initField('terms')} />
+    <Checkbox label={'Hit me with them juicy spam mails.'} {...props.initField('spam')} />
+    {props.errors.terms && props.touched.terms && <div style={errorStyle}>{props.errors.terms}</div>}
+    {props.errors.spam && <div style={errorStyle}>{props.errors.spam}</div>}
+
+    <Button
+      label={'Submit'}
+      type={'danger'}
+      disabled={props.submitDisabled}
+      loading={props.isSubmitting}
+     />
+  </React.Fragment>
+);`;
+
+const WrappedBigForm = `const WrappedForm = withFormLogic(Form)({
+  initialValues: {
+    email: 'blabl@abla.com',
+    password: '',
+    radioGroup: '',
+    iceSelect: null,
+    iceMultiselect: [],
+    terms: false,
+    spam: false,
   },
-  ...
+  validationSchema: validationSchema,
+  onSubmit: values => delayAlert(values, 2000),
+  onChange: (values, setFieldValue) => {
+    if (values.iceSelect === 'vanilla') {
+      setFieldValue('radioGroup', 'coffeebeer');
+    }
+  },
 });`;
 
 const FormUsage = () => (
   <div styleName="page">
     <div className="header">
       <div styleName="title">Form</div>
-      <div styleName="description" />
+      <div styleName="description">
+        Forms need a central place to keep the state of all the components in order to allow the implementation of important features like dynamic input disabling based on other inputs and to guarantee a single source of truth. This makes the Form component a very custom implementation as it needs to manage a lot of different input components. With this higher order component we try to standardise the general behaviour and logic behind the React way of doing forms with the intent to reduce boilerplate code.
+      </div>
       <div className="topicContent">
-        <Paragraph title={'Getting started'}>
+        <Paragraph title={'Getting started'} large>
           <DescriptionBox>
             <p>
             Start by declaring a functional component containing all the form elements that you need.
@@ -313,7 +402,8 @@ const FormUsage = () => (
           </DisplayBox>
         </Paragraph>
 
-        <Paragraph title={'Validation'}>
+
+        <Paragraph title={'Forms with validation'} large>
           <DescriptionBox>
             It is highly encouraged to use <Link type={'inline'} label={'Yup'} path={'https://github.com/jquense/yup'} /> for validation.
             Simply create your validation schema like below and pass it to the wrapped form via the validationScheme property. For further information go to the <Link type={'inline'} label={'Yup github page'} path={'https://github.com/jquense/yup'} />. Remember that the keys used in the schema need to equal the names given to the fields with the initField method.
@@ -323,7 +413,7 @@ const FormUsage = () => (
           </CodeBox>
           <DescriptionBox>
             <p>
-              Alternatively you can also specify a custom validation function by passing a function to the validationFunction property of the wrapped form. For each field you need to chain the errors with ternary expressions and return null if everything is fine.
+              Alternatively you can also specify a custom validation function by passing a function to the validationFunction property of the wrapped form. For each field you need to chain the errors with ternary expressions and return null if everything is fine. Multiselect elements need another validation if you wish to validate their inner selected options and not only the entire multiselect as true or false. In order to do this add the multiSelectOptionValidation function when wrapping the form.
             </p>
             <strong>Note: </strong> Please really try to use Yup instead. Only use this if there is no other way.
           </DescriptionBox>
@@ -336,23 +426,27 @@ const FormUsage = () => (
           <CodeBox>
             {ErrorDisplay}
           </CodeBox>
+        </Paragraph>
+        <Paragraph title={'Full Example'}>
           <DescriptionBox>
-            Multiselect elements need another validation if you wish to validate their inner selected options and not only the entire multiselect as true or false. In order to do this add the multiSelectOptionValidation function when wrapping the form.
-          </DescriptionBox>
-          <CodeBox>
-            {MultiSelectValidation}
-          </CodeBox>
-          <DescriptionBox>
-            Example of a bigger form with validation:
+            Using the yup validation from above:
           </DescriptionBox>
           <DisplayBox>
             <WrappedForm />
           </DisplayBox>
+          <CodeBox>
+            {BigForm}
+          </CodeBox>
+          <CodeBox>
+            {WrappedBigForm}
+          </CodeBox>
         </Paragraph>
       </div>
     </div>
   </div>
 );
+
+
 
 export default FormUsage;
 
