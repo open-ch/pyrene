ce6fd0e29b0acb4b67f930cc569d14b560d47ec4
lucletruc <lucas.neubauer@me.com>
2018-08-23 18:07:26 +0200

    Rewamped Formusage to fit renderprop pattern

--- kitchensink/src/app/static/cookBooks/FormUsage.jsx
+++ kitchensink/src/app/static/cookBooks/FormUsage.jsx
@@ -30,6 +30,132 @@ const errorStyle = {
   color: 'var(--red-500)',
 };
 
+const SmallForm = (
+  <Form
+    render={({initField, values, errors, touched, isSubmitting, submitDisabled}) => (
+      <React.Fragment>
+        <TextField
+          width={300}
+          title={'Email'}
+          placeholder={'Email'}
+          {...initField('email')}
+        />
+        <TextField
+          width={300}
+          title={'Password in plain text 👀'}
+          placeholder={'Password'}
+          disabled={values.checkBox}
+          {...initField('password')}
+        />
+        <Checkbox
+          label={'Lock password input'}
+          {...initField('checkBox')}
+        />
+
+        <Button
+          label={'Submit'}
+          type={'danger'}
+          disabled={submitDisabled}
+          loading={isSubmitting}
+        />
+      </React.Fragment>
+    )}
+
+    initialValues={{
+      email: 'blabl@abla.com',
+      password: '',
+      checkBox: false,
+    }}
+    onSubmit={values => delay(values, 2000)}
+  />
+);
+
+const SmallFormCode = `<Form
+
+  // initField: func, used to connect an element to the form
+  // values: object, all the current element values
+  // errors: object, error messages
+  // touched: object, true for each element that the user interacted with
+  // isSubmitting: bool, true between submission and completion of onSubmit
+  // submitDisabled: bool, true when any validation errors exist
+  
+  render={({initField, values, errors, touched, isSubmitting, submitDisabled}) => (
+    <React.Fragment>
+      <TextField
+        width={300}
+        title={'Email'}
+        placeholder={'Email'}
+        {...initField('email')} // Adding textfield 'email' to the form via initField
+      />
+      <TextField
+        width={300}
+        title={'Password in plain text 👀'}
+        placeholder={'Password'}
+        disabled={values.checkBox} // Conditionaly disable this field if the checkbox is set
+        {...initField('password')} // Adding textfield 'password' to the form via initField
+      />
+      <Checkbox
+        label={'Lock password input'}
+        {...initField('checkBox')}
+      />
+
+      // A button pressed inside of the form, or hitting enter on a textfield triggers a submit
+      // No need to add an onSubmit handler to the button
+      <Button
+        label={'Submit'}
+        type={'danger'}
+        disabled={submitDisabled}
+        loading={isSubmitting}
+      />
+    </React.Fragment>
+  )}
+   
+  // Needed for every registered form field
+  initialValues={{
+    email: 'blabl@abla.com',
+    password: '',
+    checkBox: false,
+  }}
+  
+  onSubmit={values => delay(values, 2000)}
+/>`;
+
+const Yupscheme = `const validationSchema = yup.object({
+  email: yup.string()
+    .required('No Email passed.')
+    .email('Not an email.'),
+  password: yup.string()
+    .required('Password is required.')
+    .min(8, 'Needs at least 8 chars.')
+    .matches(/[A-Z]/, 'Needs one uppercase char.'),
+
+  radioGroup: yup.string()
+    .required('Need to choose one option.'),
+
+  // select needs nullable to cover case where user only touches the empty select
+  iceSelect: yup.string()
+    .required('You need to choose an icecream!')
+    .test('isVanilla', 'Only vanilla is the real deal..', value => value === 'vanilla')
+    .nullable(),
+
+  // Validation structure for multiselects
+  iceMultiselect: yup.array().of(yup.object().shape({
+    value: yup.string()
+      .required()
+      .matches(/[a]/, 'An icecream without an A is not A-cream!')
+      .matches(/[i]/, 'An icecream without an I is not I-cecream!'),
+  })),
+
+  // conditional validation for spam checkbox depending on terms checkbox
+  terms: yup.boolean()
+    .test('acceptTerms', 'You need to accept the terms of conditioners for nicer hair!', value => value === true),
+  spam: yup.boolean()
+    .when('terms', {
+      is: true,
+      then: yup.boolean().oneOf([true], 'Our terms and conditions need you to sign up to spam mails. 🙂'),
+    }),
+});`;
+
 const validationSchema = yup.object({
   email: yup.string()
     .required('No Email passed.')
@@ -65,9 +191,22 @@ const validationSchema = yup.object({
     }),
 });
 
-const Form2 = (
+const ErrorDisplay = `<Form 
+  render={(...) => (
+    <Checkbox
+      label={'Lock password input'}
+      {...initField('checkBox')}
+    />
+    
+    // Display errors for the checkbox
+    {errors.checkBox && touched.checkBox && <div>{errors.checkBox}</div>}
+  )}
+  ...
+/>`;
+
+const BigForm = (
   <Form
-    render={(values, errors, touched, isSubmitting, submitDisabled, initField) => (
+    render={({initField, values, errors, touched, isSubmitting, submitDisabled}) => (
       <React.Fragment>
         <TextField width={300} title={'Email'} placeholder={'Email'} disabled={values.checkBox1} required {...initField('email')} />
         <div style={{ height: 24 }} />
@@ -126,12 +265,71 @@ const Form2 = (
         setFieldValue('radioGroup', 'coffeebeer');
       }
     }}
-/>);
+  />);
 
-
-/*
-const WrappedForm = withFormLogic(Form)({
-  initialValues: {
+const BigFormCode = `<Form
+  render={({initField, values, errors, touched, isSubmitting, submitDisabled}) => (
+    <React.Fragment>
+      <TextField
+        title={'Email'}
+        placeholder={'Email'}
+        disabled={props.values.checkBox1}
+        required // Note that the required prop only applies some styling 
+        {...props.initField('email')}
+      />
+     
+      <TextField 
+        title={'Password in plain text 👀'}
+        placeholder={'Password'}
+        required
+        {...props.initField('password')}
+      />
+    
+      <RadioGroup
+        alignment={'horizontal'}
+        options={[
+          {label: 'Beer', value: 'beer'},
+          {label:'Coffee', value: 'coffee'},
+          {label:'Coffeebeer', value: 'coffeebeer'}
+        ]}
+        {...props.initField('radioGroup')}
+      />
+      {props.errors.radioGroup && props.touched.radioGroup && <div>{props.errors.radioGroup}</div>}
+      
+      <SingleSelect
+        title={'Select favorite icecream'}
+        options={iceOptions}
+        clearable
+        required
+        {...props.initField('iceSelect')}
+      />
+       
+      <MultiSelect
+        title={'Select icecream.. again'}
+        options={iceOptions}
+        creatable
+        clearable
+        required
+        keepMenuOnSelect
+        {...props.initField('iceMultiselect')}
+      />
+      
+      <Checkbox label={'I accept the terms of conditioners.'} required {...props.initField('terms')} />
+      <Checkbox label={'Hit me with them juicy spam mails.'} {...props.initField('spam')} />
+      {props.errors.terms && props.touched.terms && <div style={errorStyle}>{props.errors.terms}</div>}
+      {props.errors.spam && <div style={errorStyle}>{props.errors.spam}</div>}
+      
+      <Button
+        label={'Submit'}
+        type={'danger'}
+        disabled={props.submitDisabled}
+        loading={props.isSubmitting}
+       />
+    </React.Fragment>
+  )}
+  
+  validationSchema={validationSchema}
+  initialValues={{
     email: 'blabl@abla.com',
     password: '',
     radioGroup: null,
@@ -139,286 +337,40 @@ const WrappedForm = withFormLogic(Form)({
     iceMultiselect: [],
     terms: false,
     spam: false,
-  },
-  validationSchema: validationSchema,
-  onSubmit: values => delay(values, 2000),
-  onChange: (values, setFieldValue) => {
+  }}
+  onSubmit={values => delay(values, 2000)}
+  onChange={(values, setFieldValue) => {
     if (values.iceSelect === 'vanilla') {
       setFieldValue('radioGroup', 'coffeebeer');
     }
-  },
-});
+  }}
+/>`;
 
-const SmallForm = (props) => (
-  <React.Fragment>
-    <TextField
-      width={300}
-      title={'Email'}
-      placeholder={'Email'}
-      {...props.initField('email')}
-    />
-    <TextField
-      width={300}
-      title={'Password in plain text 👀'}
-      placeholder={'Password'}
-      disabled={props.values.checkBox}
-      {...props.initField('password')}
-    />
-    <Checkbox
-      label={'Lock password input'}
-      {...props.initField('checkBox')}
-    />
-
-    <Button
-      label={'Submit'}
-      type={'danger'}
-      disabled={props.submitDisabled}
-      loading={props.isSubmitting}
-    />
-  </React.Fragment>
-);
-
-const ExampleForm = withFormLogic(SmallForm)({
-  initialValues: {
-    email: 'blabl@abla.com',
-    password: '',
-    checkBox: false,
-  },
-  validationSchema: yup.object({}),
-  onSubmit: (values) => delay(values, 3000),
-});
-
-*/
-
-const FormCode1 = `const Form = (props) => (
-  // PROPS: 
-  // All props passed to the wrapped form +
-  // values: object, all the current element values
-  // errors: object, error messages
-  // touched: object, true for each element that the user interacted with
-  // isSubmitting: bool, true between submission and completion of onSubmit
-  // initField: func, used to connect an element to the form
-  
-  <React.Fragment>
-  
-    <TextField
-      width={300}
-      title={'Email'}
-      placeholder={'Email'}
-      {...props.initField('email')} // Adding textfield 'email' to the form via initField
-    />
-    
-    <TextField
-      width={300}
-      title={'Password in plain text'}
-      placeholder={'Password'}
-      disabled={props.values.checkBox}  // Conditionaly disable this field if the checkbox is set
-      {...props.initField('password')} // Adding textfield 'password' to the form via initField
-    />
-    
-    <Checkbox
-      label={'Lock password input'}
-      {...props.initField('checkBox')}
-    />
-    
-    // A button pressed inside of the form, or hitting enter on a textfield triggers a submit
-    // No need to add an onSubmit handler to the button
-    <Button
-      label={'Submit'}
-      type={'danger'}
-      disabled={props.submitDisabled}
-      loading={props.isSubmitting}
-    />
-  </React.Fragment>
-);`;
-
-const FormCode2 = `const WrappedForm = withFormLogic(Form)({
- // Every field that is connected to the form with initField() needs an initial value!
-  initialValues: {
-    email: 'blabl@abla.com', // Note that the keys must match the string in the initField functions
-    password: '',
-    checkBox: false,
-  },
-  // Pass Yup validation schema, more info below
-  validationSchema: yup.object({}),
-  
-  // Define your onSubmit function, needs to be a Promise
-  onSubmit: (values) => null, 
-  
-  // Conditionally change other field values
-  onChange: (values, setFieldValue) => null,
-});`;
-
-const FormCode3 = `class YesThisIsDog extends React.Component {
-  render() {
-    return <WrappedForm />;
-  }
-}`;
-
-const Yupscheme = `const validationSchema = yup.object({
-  email: yup.string()
-    .required('No Email passed.')
-    .email('Not an email.'),
-  password: yup.string()
-    .required('Password is required.')
-    .min(8, 'Needs at least 8 chars.')
-    .matches(/[A-Z]/, 'Needs one uppercase char.'),
-
-  radioGroup: yup.string()
-    .required('Need to choose one option.'),
-
-  // select needs nullable to cover case where user only touches the empty select
-  iceSelect: yup.string()
-    .required('You need to choose an icecream!')
-    .test('isVanilla', 'Only vanilla is the real deal..', value => value === 'vanilla')
-    .nullable(),
-
-  // Validation structure for multiselects
-  iceMultiselect: yup.array().of(yup.object().shape({
-    value: yup.string()
-      .required()
-      .matches(/[a]/, 'An icecream without an A is not A-cream!')
-      .matches(/[i]/, 'An icecream without an I is not I-cecream!'),
-  })),
-
-  // conditional validation for spam checkbox depending on terms checkbox
-  terms: yup.boolean()
-    .test('acceptTerms', 'You need to accept the terms of conditioners for nicer hair!', value => value === true),
-  spam: yup.boolean()
-    .when('terms', {
-      is: true,
-      then: yup.boolean().oneOf([true], 'Our terms and conditions need you to sign up to spam mails. 🙂'),
-    }),
-});`;
-
-const ErrorDisplay = `const Form = (props) => (
-  ...
-    <Checkbox
-      label={'Lock password input'}
-      {...props.initField('checkBox')}
-    />
-    
-    // Display errors for the checkbox
-    {props.errors.checkBox && props.touched.checkBox && <div>{props.errors.checkBox}</div>}
-  ...
-);`;
-
-const BigForm = `// Code for the form above
-const Form = props => (
-  <React.Fragment>
-    <TextField
-      title={'Email'}
-      placeholder={'Email'}
-      disabled={props.values.checkBox1}
-      required // Note that the required prop only applies some styling 
-      {...props.initField('email')}
-     />
-     
-    <TextField 
-      title={'Password in plain text 👀'}
-      placeholder={'Password'}
-      required
-      {...props.initField('password')}
-    />
-   
-    <RadioGroup
-      alignment={'horizontal'}
-      options={[
-        {label: 'Beer', value: 'beer'},
-        {label:'Coffee', value: 'coffee'},
-        {label:'Coffeebeer', value: 'coffeebeer'}
-      ]}
-      {...props.initField('radioGroup')}
-    />
-    {props.errors.radioGroup && props.touched.radioGroup && <div>{props.errors.radioGroup}</div>}
-
-    <SingleSelect
-      title={'Select favorite icecream'}
-      options={iceOptions}
-      clearable
-      required
-      {...props.initField('iceSelect')}
-    />
-     
-    <MultiSelect
-      title={'Select icecream.. again'}
-      options={iceOptions}
-      creatable
-      clearable
-      required
-      keepMenuOnSelect
-      {...props.initField('iceMultiselect')}
-    />
-   
-    <Checkbox label={'I accept the terms of conditioners.'} required {...props.initField('terms')} />
-    <Checkbox label={'Hit me with them juicy spam mails.'} {...props.initField('spam')} />
-    {props.errors.terms && props.touched.terms && <div style={errorStyle}>{props.errors.terms}</div>}
-    {props.errors.spam && <div style={errorStyle}>{props.errors.spam}</div>}
-
-    <Button
-      label={'Submit'}
-      type={'danger'}
-      disabled={props.submitDisabled}
-      loading={props.isSubmitting}
-     />
-  </React.Fragment>
-);`;
-
-const WrappedBigForm = `const WrappedForm = withFormLogic(Form)({
-  initialValues: {
-    email: 'blabl@abla.com',
-    password: '',
-    radioGroup: '',
-    iceSelect: null,
-    iceMultiselect: [],
-    terms: false,
-    spam: false,
-  },
-  validationSchema: validationSchema,
-  onSubmit: (values) => null,
-  onChange: (values, setFieldValue) => {
-    if (values.iceSelect === 'vanilla') {
-      setFieldValue('radioGroup', 'coffeebeer');
-    }
-  },
-});`;
 
 const FormUsage = () => (
   <div styleName="page">
     <div className="header">
       <div styleName="title">Form</div>
       <div styleName="description">
-        Forms need a central place to keep the state of all the components in order to allow the implementation of important features like dynamic input disabling and to guarantee a <strong>single source of truth</strong>. As it needs to manage a lot of different input components and types, the Form component needs a very custom implementation. With this higher order component we try to standardise the general behaviour and logic behind the React way of doing forms with the intent to reduce boilerplate code.
+        Forms need a central place to keep the state of all the components in order to allow the implementation of important features like dynamic input disabling and to guarantee a <strong>single source of truth</strong>. As it needs to manage a lot of different input components and types, the Form component needs a very custom implementation. With this component using the <strong>render props pattern</strong> we try to standardise the general behaviour and logic behind the React way of doing forms with the intent to reduce boilerplate code.
       </div>
       <div className="topicContent">
         <Paragraph title={'Getting started'} large>
           <DescriptionBox>
             <p>
-            Start by declaring a functional component containing all the form elements that you need.
+            Start by using the Form component and fill its render prop with all the form elements that you need.
             Register each element with the initField function and add a button for submission at the end of the form.
             </p>
             <strong>Note:</strong> The initField function needs to be called with the spread operator and as the *LAST* prop of the element.
           </DescriptionBox>
           <CodeBox>
-            {FormCode1}
-          </CodeBox>
-          <DescriptionBox>
-            Wrap the created component...
-          </DescriptionBox>
-          <CodeBox>
-            {FormCode2}
-          </CodeBox>
-          <DescriptionBox>
-            Use the wrapped component in your app.
-          </DescriptionBox>
-          <CodeBox>
-            {FormCode3}
+            {SmallFormCode}
           </CodeBox>
           <DescriptionBox>
             Let's have a look at that beauty. ✨
           </DescriptionBox>
           <DisplayBox>
-            {/*<ExampleForm />*/}
+            {SmallForm}
           </DisplayBox>
         </Paragraph>
 
@@ -442,13 +394,10 @@ const FormUsage = () => (
             Using the yup validation from above:
           </DescriptionBox>
           <DisplayBox>
-            {Form2}
+            {BigForm}
           </DisplayBox>
           <CodeBox>
-            {BigForm}
-          </CodeBox>
-          <CodeBox>
-            {WrappedBigForm}
+            {BigFormCode}
           </CodeBox>
         </Paragraph>
       </div>
