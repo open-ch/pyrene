d3e32ff77567fb5d174254c5036cd8a1fe1d81ee
ekeuus <ekke@tdl.ee>
2018-10-03 11:08:48 +0200

    Fix shallow gobal var issue

--- eslint-config-pyrene/index.js
+++ eslint-config-pyrene/index.js
@@ -5,7 +5,10 @@ module.exports = {
   "env": {
     "browser": "true",
     "es6": "true",
-    "jest/globals": "true",
+    "jest/globals": "true"
+  },
+
+  "globals": {
     "shallow": "true"
   },
 
