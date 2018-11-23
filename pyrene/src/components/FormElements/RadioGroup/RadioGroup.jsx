1cb6e1c7ea2e39349cc8cb905e8cd8961af65b3b
ekeuus <ekke@tdl.ee>
2018-11-23 18:19:21 +0100

    Implement inline svg stuff for Radio 📻

--- pyrene/src/components/FormElements/RadioGroup/RadioGroup.jsx
+++ pyrene/src/components/FormElements/RadioGroup/RadioGroup.jsx
@@ -1,50 +1,125 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './radioSelection.css';
 
+import SVG from 'react-svg-inline';
+
+import normal from './radio-blank.svg';
+import normalHover from './radio-hover.svg';
+import selected from './radio-selected.svg';
+import selectedHover from './radio-selected-hover.svg';
+import invalid from './radio-invalid.svg';
+import invalidHover from './radio-invalid-hover.svg';
+
+const iconMap = {
+  normal: {
+    default: normal,
+    hover: normalHover,
+  },
+  checked: {
+    default: selected,
+    hover: selectedHover,
+  },
+  invalid: {
+    default: invalid,
+    hover: invalidHover,
+  },
+};
+
+const getRadioIcon = (options, hovered) => {
+  const { checked, disabled, invalid } = options;
+  const iconKey = !disabled && hovered ? 'hover' : 'default';
+  let icon = iconMap.normal[iconKey];
+  if (invalid) {
+    icon = iconMap.invalid[iconKey];
+  } else if (checked) {
+    icon = iconMap.checked[iconKey];
+  }
+  return <SVG svg={icon} />;
+};
+
 /**
  * Radio groups are used primarily on ....
  */
-const RadioGroup = (props) => {
-  const rand = Math.floor(Math.random() * 1e10);
-  const lastElementIndex = props.options.length - 1;
-  return (
-    <div
-      styleName={classNames('radioSelectionContainer', { [`alignment-${props.alignment}`]: true }, { invalid: props.invalid && !props.selectedOption })}
-      tabIndex={0}
-      onBlur={props.onBlur}
-      id={props.name}
-    >
-      {props.options.map((option, index) => (
-        <React.Fragment key={`radio_${option.label}_${option.value}`}>
-          <div className={'radioContainer'}>
-            <input
-              styleName={'radioInput'}
-              checked={props.value === option.value}
-              id={`radio_${option.label}_${option.value}_${rand}`}
-              onChange={props.onChange}
-              type="radio"
-              value={option.value}
-              name={props.name}
-            />
+class RadioGroup extends Component {
+  state = {
+    hovered: {},
+  };
 
-            <label
-              htmlFor={`radio_${option.label}_${option.value}_${rand}`}
-              styleName={
-                classNames('radioLabel',
-                  { checked: props.value === option.value },
-                  { disabled: props.disabled })}
-            >
-              <span styleName={'radioIcon'} />
-              {option.label}
-            </label>
-          </div>
-          {index !== lastElementIndex && <div styleName={classNames({ [`spacer-${props.alignment}`]: true })} />}
-        </React.Fragment>
-      ))}
-    </div>
-  );
+  onMouseEnter = (key) => {
+    this.setState((prevState) => ({
+      hovered: {
+        ...prevState.hovered,
+        [key]: true,
+      },
+    }));
+  };
+
+  onMouseLeave = (key) => {
+    this.setState((prevState) => ({
+      hovered: {
+        ...prevState.hovered,
+        [key]: undefined,
+      },
+    }));
+  };
+
+  render () {
+    const { props } = this;
+    const rand = Math.floor(Math.random() * 1e10);
+    const lastElementIndex = props.options.length - 1;
+
+    return (
+      <div
+        styleName={classNames('radioSelectionContainer', { [`alignment-${props.alignment}`]: true }, { invalid: props.invalid && !props.selectedOption })}
+        tabIndex={0}
+        onBlur={props.onBlur}
+        id={props.name}
+      >
+        {props.options.map((option, index) => {
+          const key = `radio_${option.label}_${option.value}`;
+          const htmlId = `${key}_${rand}`;
+          return (
+            <React.Fragment key={key}>
+              <div
+                className={'radioContainer'}
+                onMouseEnter={() => this.onMouseEnter(key)}
+                onMouseLeave={() => this.onMouseLeave(key)}
+              >
+                <input
+                  styleName={'radioInput'}
+                  checked={props.value === option.value}
+                  id={htmlId}
+                  onChange={props.onChange}
+                  type="radio"
+                  value={option.value}
+                  name={props.name}
+                />
+
+                <label
+                  htmlFor={htmlId}
+                  styleName={
+                    classNames('radioLabel',
+                      { disabled: props.disabled })}
+                >
+                  <span styleName={'radioIcon'}>
+                    {getRadioIcon({
+                        checked: props.value === option.value,
+                        disabled: props.disabled,
+                        invalid: props.invalid && !props.selectedOption,
+                      }, this.state.hovered[key])}
+                  </span>
+                  {option.label}
+                </label>
+              </div>
+              {index !== lastElementIndex && <div styleName={classNames({ [`spacer-${props.alignment}`]: true })} />}
+            </React.Fragment>
+          );
+        })}
+      </div>
+    )
+}
 };
 
 RadioGroup.displayName = 'Radio Group';
