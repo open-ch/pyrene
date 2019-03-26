fa6a209066e07cce4ef9c70f41da97566370c4db
maf <maf@open.ch>
2019-03-26 13:19:30 +0100

    disable jsx-a11y/anchor-is-valid

--- eslint-config-pyrene/index.js
+++ eslint-config-pyrene/index.js
@@ -85,6 +85,7 @@ module.exports = {
     "jsx-a11y/mouse-events-have-key-events": 0,
     "jsx-a11y/no-static-element-interactions": 0,
     "jsx-a11y/role-has-required-aria-props": 0,
+    "jsx-a11y/anchor-is-valid": 0,
     // Our imports work differently in MC
     "import/no-extraneous-dependencies": 0,
     "import/no-unresolved": 0,
