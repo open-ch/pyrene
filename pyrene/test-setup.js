43e0e5b0e3652756992030f7e69e9fd84d2fd524
maf <maf@open.ch>
2019-04-15 11:20:47 +0200

    Remove manual jsdom handling, jest can do that out of the box

--- pyrene/test-setup.js
+++ pyrene/test-setup.js
@@ -1,36 +1,7 @@
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
-const { JSDOM } = require('jsdom');
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>', {
-  url: 'http://localhost',
-});
-const { window } = jsdom;
-
-function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce((result, prop) => ({
-      ...result,
-      [prop]: Object.getOwnPropertyDescriptor(src, prop),
-    }), {});
-  Object.defineProperties(target, props);
-}
-
-global.window = window;
 global.shallow = Enzyme.shallow;
 global.mount = Enzyme.mount;
-global.document = window.document;
-global.navigator = {
-  userAgent: 'node.js',
-};
-
-global.requestAnimationFrame = (callback) => {
-  setTimeout(callback, 0);
-};
-
-global.shallow = Enzyme.shallow;
-
-copyProps(window, global);
