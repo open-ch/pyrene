4e23f4bea34ac4d9b23bc706598c9d28798f8857
sladomic <michael.sladoje@outlook.com>
2019-07-09 10:35:19 +0200

    playground: Add React component keys

--- tuktuktwo/src/examples/BarTable/pyrene/SimpleTable.jsx
+++ tuktuktwo/src/examples/BarTable/pyrene/SimpleTable.jsx
@@ -14,18 +14,27 @@ const SimpleTable = props => (
       <tbody>
         <tr styleName="tableRow">
           {props.columns.length > 0 && props.columns.map(column => (
-            <th styleName="tableCell">
+            <th
+              styleName="tableCell"
+              key={column.id}
+            >
               {column.headerName}
             </th>
           ))}
         </tr>
         {props.data.length > 0 && props.data.map(row => (
-          <tr styleName="tableRow">
+          <tr
+            styleName="tableRow"
+            key={Object.values(row)}
+          >
             {props.columns.length > 0 && props.columns.map((column) => {
               const valueRow = row;
               valueRow.value = typeof column.accessor === 'string' ? getBy(row, column.accessor) : column.accessor(row);
               return (
-                <td styleName="tableCell">
+                <td
+                  styleName="tableCell"
+                  key={column.id.concat(Object.values(valueRow))}
+                >
                   {column.cellRenderCallback ? column.cellRenderCallback(valueRow) : valueRow.value}
                 </td>
               );
