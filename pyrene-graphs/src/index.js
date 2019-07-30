4d65f12260a34749d47a03815ab11dbcc039ca91
sladomic <michael.sladoje@outlook.com>
2019-07-30 17:15:36 +0200

    Add new ComparisonChartTable component

--- pyrene-graphs/src/index.js
+++ pyrene-graphs/src/index.js
@@ -3,13 +3,16 @@
 
 // Sort alphabetically to find stuff easily
 import { default as BarChartTable } from './components/BarChartTable/BarChartTable';
+import { default as ComparisonChartTable } from './components/ComparisonChartTable/ComparisonChartTable';
 
 // Sort alphabetically to find stuff easily
 const Components = {
   BarChartTable,
+  ComparisonChartTable,
 };
 
 // Sort alphabetically to find stuff easily
 export { BarChartTable };
+export { ComparisonChartTable };
 
 export default Components;
