9f3b16b593be71deeb45ae8889b2c51c2897377d
lga <lga@open.ch>
2019-08-02 11:40:04 +0200

    added meaningful component tests

--- pyrene/src/components/Badge/Badge.spec.jsx
+++ pyrene/src/components/Badge/Badge.spec.jsx
@@ -9,7 +9,55 @@ const props = {
 
 describe('<Badge />', () => {
   it('renders without crashing', () => {
-    shallow(<Badge {...props} />);
+    const rendered = shallow(<Badge {...props} />);
+    expect(rendered.contains('Security Alert')).toBe(true);
   });
 
+  it('renders a neutral badge', () => {
+    const rendered = shallow(<Badge type={'neutral'} label={'Neutral'}/>);
+    const text = rendered.text();
+    const className = rendered.prop('className');
+    expect(text).toBe('Neutral');
+    expect(className).toContain('type-neutral');
+  });
+
+  it('renders a danger badge', () => {
+    const rendered = shallow(<Badge {...props}/>);
+    const text = rendered.text();
+    const className = rendered.prop('className');
+    expect(text).toBe('Security Alert');
+    expect(className).toContain('type-danger');
+  });
+
+  it('renders a info badge', () => {
+    const rendered = shallow(<Badge type={'info'} label={'Info'}/>);
+    const text = rendered.text(); // warning  success
+    const className = rendered.prop('className');
+    expect(text).toBe('Info');
+    expect(className).toContain('type-info');
+  });
+
+  it('renders a warning badge', () => {
+    const rendered = shallow(<Badge type={'warning'} label={'Warning'}/>);
+    const text = rendered.text();
+    const className = rendered.prop('className');
+    expect(text).toBe('Warning');
+    expect(className).toContain('type-warning');
+  });
+
+  it('renders a success badge', () => {
+    const rendered = shallow(<Badge type={'success'} label={'Success'}/>);
+    const text = rendered.text();
+    const className = rendered.prop('className');
+    expect(text).toBe('Success');
+    expect(className).toContain('type-success');
+  });
+
+  it('reacts to clicking', () => {
+    const onClick = jest.fn();
+    const rendered = shallow(<Badge {...props} onClick={onClick}/>);
+    rendered.simulate('click');
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+  
 });
