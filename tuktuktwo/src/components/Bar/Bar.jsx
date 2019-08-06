4b8349aa4a93af8ff96548a3c7b8697296116466
sladomic <michael.sladoje@outlook.com>
2019-08-06 16:23:22 +0200

    Move styling from TukTukTwo to Pyrene Graphs

--- tuktuktwo/src/components/Bar/Bar.jsx
+++ tuktuktwo/src/components/Bar/Bar.jsx
@@ -1,42 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { Bar as VxBar } from '@vx/shape';
-import Responsive from '../Misc/Responsive';
-import './bar.css';
 
 const Bar = props => (
-  <div styleName={classNames({
-    verticalBar: props.direction === 'vertical',
-    parentDiv: props.direction === 'vertical',
-  })}
-  >
-    <Responsive>
-      {parent => (
-        props.direction === 'horizontal' ? (
-          <svg width={parent.width} height={props.barWeight}>
-            <VxBar
-              x="0"
-              y="0"
-              height={props.barWeight}
-              width={props.value * (parent.width / props.maxValue)}
-              fill={props.color}
-            />
-          </svg>
-        ) : (
-          <svg width={props.barWeight} height={parent.height}>
-            <VxBar
-              x="0"
-              y={(props.maxValue - props.value) * (parent.height / props.maxValue)}
-              height={props.value * (parent.height / props.maxValue)}
-              width={props.barWeight}
-              fill={props.color}
-            />
-          </svg>
-        )
-      )}
-    </Responsive>
-  </div>
+  props.direction === 'horizontal' ? (
+    <svg width={props.parentLength} height={props.barWeight}>
+      <VxBar
+        x="0"
+        y="0"
+        height={props.barWeight}
+        width={props.value * (props.parentLength / props.maxValue)}
+        fill={props.color}
+      />
+    </svg>
+  ) : (
+    <svg width={props.barWeight} height={props.parentLength}>
+      <VxBar
+        x="0"
+        y={(props.maxValue - props.value) * (props.parentLength / props.maxValue)}
+        height={props.value * (props.parentLength / props.maxValue)}
+        width={props.barWeight}
+        fill={props.color}
+      />
+    </svg>
+  )
 );
 
 Bar.displayName = 'Bar';
@@ -45,6 +32,7 @@ Bar.defaultProps = {
   barWeight: 6,
   color: 'var(--blue-700)',
   direction: 'horizontal',
+  parentLength: 150,
 };
 
 Bar.propTypes = {
@@ -52,6 +40,7 @@ Bar.propTypes = {
   color: PropTypes.string,
   direction: PropTypes.string,
   maxValue: PropTypes.number.isRequired,
+  parentLength: PropTypes.number,
   value: PropTypes.number.isRequired,
 };
 
--- tuktuktwo/src/components/Bar/RelativeBar.jsx
+++ tuktuktwo/src/components/Bar/RelativeBar.jsx
@@ -1,70 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { BarStackHorizontal, BarStack } from '@vx/shape';
 import { scaleBand, scaleLinear, scaleOrdinal } from '@vx/scale';
-import Responsive from '../Misc/Responsive';
-import './bar.css';
 
-const RelativeBar = props => (
-  <div styleName={classNames({
-    verticalBar: props.direction === 'vertical',
-    parentDiv: props.direction === 'vertical',
-  })}
-  >
-    <Responsive>
-      {(parent) => {
-        const keys = [
-          'value',
-          'rest',
-        ];
-        const data = [{
-          value: props.value,
-          rest: props.maxValue - props.value,
-          y: 0,
-        },
-        ];
-        const color = scaleOrdinal({
-          domain: keys,
-          range: [props.colorScheme.primary, props.colorScheme.secondary],
-        });
-        const valueScale = scaleLinear({
-          domain: props.direction === 'horizontal' ? [0, props.maxValue] : [props.maxValue, 0],
-          rangeRound: [0, props.direction === 'horizontal' ? parent.width : parent.height],
-        });
-        const weightScale = scaleBand({
-        });
-        return (
-          props.direction === 'horizontal' ? (
-            <svg width={parent.width} height={props.barWeight}>
-              <BarStackHorizontal
-                y={d => d}
-                height={props.barWeight}
-                data={data}
-                keys={keys}
-                xScale={valueScale}
-                yScale={weightScale}
-                color={color}
-              />
-            </svg>
-          ) : (
-            <svg width={props.barWeight} height={parent.height}>
-              <BarStack
-                x={d => d}
-                width={props.barWeight}
-                data={data}
-                keys={keys}
-                xScale={weightScale}
-                yScale={valueScale}
-                color={color}
-              />
-            </svg>
-          )
-        );
-      }}
-    </Responsive>
-  </div>
-);
+const RelativeBar = (props) => {
+  const keys = [
+    'value',
+    'rest',
+  ];
+  const data = [{
+    value: props.value,
+    rest: props.maxValue - props.value,
+    y: 0,
+  },
+  ];
+  const color = scaleOrdinal({
+    domain: keys,
+    range: [props.colorScheme.primary, props.colorScheme.secondary],
+  });
+  const valueScale = scaleLinear({
+    domain: props.direction === 'horizontal' ? [0, props.maxValue] : [props.maxValue, 0],
+    rangeRound: [0, props.parentLength],
+  });
+  const weightScale = scaleBand({
+  });
+  return (
+    props.direction === 'horizontal' ? (
+      <svg width={props.parentLength} height={props.barWeight}>
+        <BarStackHorizontal
+          y={d => d}
+          height={props.barWeight}
+          data={data}
+          keys={keys}
+          xScale={valueScale}
+          yScale={weightScale}
+          color={color}
+        />
+      </svg>
+    ) : (
+      <svg width={props.barWeight} height={props.parentLength}>
+        <BarStack
+          x={d => d}
+          width={props.barWeight}
+          data={data}
+          keys={keys}
+          xScale={weightScale}
+          yScale={valueScale}
+          color={color}
+        />
+      </svg>
+    )
+  );
+};
 
 RelativeBar.displayName = 'Relative Bar';
 
@@ -75,6 +62,7 @@ RelativeBar.defaultProps = {
     secondary: 'var(--blue-050)',
   },
   direction: 'horizontal',
+  parentLength: 150,
 };
 
 RelativeBar.propTypes = {
@@ -82,6 +70,7 @@ RelativeBar.propTypes = {
   colorScheme: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   direction: PropTypes.string,
   maxValue: PropTypes.number.isRequired,
+  parentLength: PropTypes.number,
   value: PropTypes.number.isRequired,
 };
 
