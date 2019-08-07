89c18bec600e977b0e4e09eceff6f16861927f55
sladomic <michael.sladoje@outlook.com>
2019-08-07 08:44:48 +0200

    Remove ComparisonChartTable references

--- pyrene-graphs/src/examples/index.js
+++ pyrene-graphs/src/examples/index.js
@@ -3,12 +3,10 @@
 
 // Sort alphabetically to find stuff easily
 import { default as BarChartTable } from '../components/BarChartTable/BarChartTable.examples';
-import { default as ComparisonChartTable } from '../components/ComparisonChartTable/ComparisonChartTable.examples';
 
 // Sort alphabetically to find stuff easily
 const examples = {
   BarChartTable,
-  ComparisonChartTable,
 };
 
 export default examples;
