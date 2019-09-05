dd3a199f1245c821f6793d30b50c9354294d584c
alo <alo@open.ch>
2019-09-05 11:48:02 +0200

    Merge pull request #169 pyrene from feature/ to develop
    * commit 'a5660cb56e30084a8af70c510f2beeadf17e78e9':
      adding border to simple table, fixing padding & height
      more rows to key value table in example :)
      key value table fix wrapping of key

--- pyrene/src/components/KeyValueTable/KeyValueTable.examples.jsx
+++ pyrene/src/components/KeyValueTable/KeyValueTable.examples.jsx
@@ -3,6 +3,8 @@ const examples = {
     title: 'KeyValue table',
     rows: [
       { key: 'Key', value: 'value' },
+      { key: 'Key2', value: 'value2' },
+      { key: 'Key3', value: 'value3' },
       {
         key: 'Super Long Key Super Long Key Super Long Key Super Long Key Super Long Key Super Long Key SuperLongKey SuperLongKey SuperLongKey SuperLongKey SuperLongKey SuperLongKey SuperLongKey SuperLongKey ',
         value: 'Super Long Value Super Long Value Super Long Value Super Long Value Super Long Value SuperLongValue SuperLongValue SuperLongValue SuperLongValue SuperLongValue SuperLongValue SuperLongValue',
