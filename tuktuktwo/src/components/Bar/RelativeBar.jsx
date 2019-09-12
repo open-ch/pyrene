9228cda289c46f072e992b1e73cd99d12cb35099
sladomic <michael.sladoje@outlook.com>
2019-09-12 16:13:41 +0200

    Add corner radius to bar components

--- tuktuktwo/src/components/Bar/RelativeBar.jsx
+++ tuktuktwo/src/components/Bar/RelativeBar.jsx
@@ -1,71 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BarStackHorizontal, BarStack } from '@vx/shape';
-import { scaleBand, scaleLinear, scaleOrdinal } from '@vx/scale';
 import Responsive from '../Misc/Responsive';
 
 /**
  * Relative Bars are used to display a numerical value.
  * It has an underlying secondary bar, which extends to the full length.
  */
-const RelativeBar = (props) => {
-  const keys = [
-    'value',
-    'rest',
-  ];
-  const data = [{
-    value: props.value,
-    rest: props.maxValue - props.value,
-    y: 0,
-  },
-  ];
-  const color = scaleOrdinal({
-    domain: keys,
-    range: props.colorScheme,
-  });
-  const weightScale = scaleBand({
-  });
-  return (
-    <Responsive>
-      {(parent) => {
-        const valueScale = scaleLinear({
-          domain: props.direction === 'horizontal' ? [0, props.maxValue] : [props.maxValue, 0],
-          rangeRound: [0, props.direction === 'horizontal' ? parent.width : parent.height],
-        });
-        return props.direction === 'horizontal' ? (
-          <svg width={parent.width} height={props.barWeight} transform={props.mirrored ? 'rotate(180 0 0)' : undefined}>
-            <BarStackHorizontal
-              y={d => d}
-              height={props.barWeight}
-              data={data}
-              keys={keys}
-              xScale={valueScale}
-              yScale={weightScale}
-              color={color}
-            />
-          </svg>
-        ) : (
-          <svg width={props.barWeight} height={parent.height} transform={props.mirrored ? 'rotate(180 0 0)' : undefined}>
-            <BarStack
-              x={d => d}
-              width={props.barWeight}
-              data={data}
-              keys={keys}
-              xScale={weightScale}
-              yScale={valueScale}
-              color={color}
-            />
-          </svg>
-        );
-      }}
-    </Responsive>
-  );
-};
+const RelativeBar = props => (
+  <Responsive>
+    {parent => (
+      props.direction === 'horizontal1' ? (
+        <svg width={parent.width} height={props.barWeight} transform={props.mirrored ? 'rotate(180 0 0)' : undefined}>
+          <g>
+            <path d={`M0,0H${parent.width - props.cornerRadius}a${props.cornerRadius},${props.cornerRadius} 0 0 1 ${props.cornerRadius},${props.cornerRadius}v${props.barWeight - (2 * props.cornerRadius)}a${props.cornerRadius},${props.cornerRadius} 0 0 1 -${props.cornerRadius},${props.cornerRadius}H${props.value * (parent.width / props.maxValue)}z`} fill={props.colorScheme[1]} />
+            <path d={`M0,0h${props.value * (parent.width / props.maxValue) - props.cornerRadius}a${props.cornerRadius},${props.cornerRadius} 0 0 1 ${props.cornerRadius},${props.cornerRadius}v${props.barWeight - (2 * props.cornerRadius)}a${props.cornerRadius},${props.cornerRadius} 0 0 1 -${props.cornerRadius},${props.cornerRadius}h${props.cornerRadius - (props.value * (parent.width / props.maxValue))}z`} fill={props.colorScheme[0]} />
+          </g>
+        </svg>
+      ) : (
+        <svg width={props.barWeight} height={20} transform={props.mirrored ? 'rotate(180 0 0)' : undefined}>
+          <g>
+            <path d={`M0,${parent.height}V${props.cornerRadius}a${props.cornerRadius},${props.cornerRadius} 0 0 1 ${props.cornerRadius},-${props.cornerRadius}h${props.barWeight - (2 * props.cornerRadius)}a${props.cornerRadius},${props.cornerRadius} 0 0 1 ${props.cornerRadius},${props.cornerRadius}V${parent.height}z`} fill={props.colorScheme[1]} />
+            <path d={`M0,${parent.height}v${-(props.value * (parent.height / props.maxValue) - props.cornerRadius)}a${props.cornerRadius},${props.cornerRadius} 0 0 1 ${props.cornerRadius},-${props.cornerRadius}h${props.barWeight - (2 * props.cornerRadius)}a${props.cornerRadius},${props.cornerRadius} 0 0 1 ${props.cornerRadius},${props.cornerRadius}V${parent.height}z`} fill={props.colorScheme[0]} />
+          </g>
+        </svg>
+      )
+    )}
+  </Responsive>
+);
 
 RelativeBar.displayName = 'Relative Bar';
 
 RelativeBar.defaultProps = {
   barWeight: 6,
+  cornerRadius: 1,
   direction: 'horizontal',
   mirrored: false,
 };
@@ -79,6 +46,10 @@ RelativeBar.propTypes = {
    * Sets the colors of the bars. Type: [ string ]
    */
   colorScheme: PropTypes.arrayOf(PropTypes.string).isRequired,
+  /**
+   * Sets the corner radius.
+   */
+  cornerRadius: PropTypes.number,
   /**
    * Sets the direction of the bars.
    */
