d8f79ba1112490c4081c6ba26fbeecfc6cfa33d9
sladomic <michael.sladoje@outlook.com>
2019-09-17 16:45:43 +0200

    Add first version of Axis component

--- pyrene-graphs/src/components/BarChart/BarChart.jsx
+++ pyrene-graphs/src/components/BarChart/BarChart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Bar } from 'tuktuktwo/dist/tuktuktwo.dev';
+import { Axis, Bar } from 'tuktuktwo/dist/tuktuktwo';
 import Header from '../Header/Header';
 import './barChart.css';
 import colorSchemes from '../../styles/colorSchemes';
@@ -12,7 +12,7 @@ function getValueWithAccessor(row, accessor) {
 
 const BarChart = (props) => {
   const maxValue = Math.max(...props.data.map(dataRow => getValueWithAccessor(dataRow, props.columns.value.accessor)));
-  const barWeight = 6;
+  const barWeight = 10;
   const barChart = (row, accessor) => (
     <Bar
       barWeight={barWeight}
@@ -22,6 +22,7 @@ const BarChart = (props) => {
       direction={props.direction}
     />
   );
+  const labels = props.data.map(row => getValueWithAccessor(row, props.columns.label.accessor));
   return (
     <div styleName="container">
       <Header
@@ -29,12 +30,30 @@ const BarChart = (props) => {
         description={props.description}
         colorScheme={props.colorScheme}
       />
-      <div styleName={classNames('chartContainer', { verticalContainer: props.direction === 'vertical' })}>
-        {props.data.map(row => (
-          <div>
-            {barChart(row, props.columns.value.accessor)}
+      <div styleName="columnContainer">
+        <div styleName="axisLeft">
+          <Axis
+            labels={props.direction === 'horizontal' ? labels : []}
+            maxValue={props.direction === 'vertical' && maxValue}
+            position="left"
+          />
+        </div>
+        <div styleName="rowContainer">
+          <div styleName={classNames('barsContainer', { horizontalContainer: props.direction === 'horizontal' })}>
+            {props.data.map(row => (
+              <div styleName="barContainer">
+                {barChart(row, props.columns.value.accessor)}
+              </div>
+            ))}
           </div>
-        ))}
+          <div styleName="axisBottom">
+            <Axis
+              labels={props.direction === 'vertical' ? labels : []}
+              maxValue={props.direction === 'horizontal' && maxValue}
+              position="bottom"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
