ad9f62dd2209b8a79f3773f8fd9421222029270c
sladomic <michael.sladoje@outlook.com>
2019-09-19 09:39:17 +0200

    Add BarChart example data

---
+++ pyrene-graphs/src/examples/barData.js
@@ -0,0 +1,63 @@
+/* eslint-disable import/prefer-default-export */
+export const applications = [
+  {
+    label: 'Dropbox',
+    values: {
+      volume: 1147.4,
+    },
+  },
+  {
+    label: 'Youtube',
+    values: {
+      volume: 849.9,
+    },
+  },
+  {
+    label: 'Google',
+    values: {
+      volume: 558.1,
+    },
+  },
+  {
+    label: 'Facebook',
+    values: {
+      volume: 345.4,
+    },
+  },
+  {
+    label: 'Qlik',
+    values: {
+      volume: 200.2,
+    },
+  },
+  {
+    label: 'We Transfer',
+    values: {
+      volume: 192.1,
+    },
+  },
+  {
+    label: 'Asana',
+    values: {
+      volume: 145.3,
+    },
+  },
+];
+
+export const nsmSensors = [
+  {
+    label: 'amande',
+    values: {
+      high: 7,
+      moderate: 7,
+      low: 29,
+    },
+  }, {
+    label: 'tau',
+    values: {
+      high: 2,
+      moderate: 0,
+      low: 0,
+    },
+  },
+];
