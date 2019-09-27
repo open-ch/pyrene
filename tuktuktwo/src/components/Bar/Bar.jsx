a8201267181b87a415566796aee99a6f395cd5dd
sladomic <michael.sladoje@outlook.com>
2019-09-27 16:56:19 +0200

    Revert change from

--- tuktuktwo/src/components/Bar/Bar.jsx
+++ tuktuktwo/src/components/Bar/Bar.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Bar as VxBar } from '@vx/shape';
 import Responsive from '../Misc/Responsive';
 
 /**
  * Bars are used to display a numerical value.
  */
-
 const Bar = props => (
   <Responsive>
     {parent => (
       props.direction === 'horizontal' ? (
         <svg width={parent.width} height={props.barWeight} transform={props.mirrored ? 'rotate(180 0 0)' : undefined}>
-          <path d={`M0,0h${props.value * (parent.width / props.maxValue) - props.cornerRadius}a${props.cornerRadius},${props.cornerRadius} 0 0 1 ${props.cornerRadius},${props.cornerRadius}v${props.barWeight - (2 * props.cornerRadius)}a${props.cornerRadius},${props.cornerRadius} 0 0 1 -${props.cornerRadius},${props.cornerRadius}h${props.cornerRadius - (props.value * (parent.width / props.maxValue))}z`} fill={props.color} />
+          <VxBar
+            x="0"
+            y="0"
+            height={props.barWeight}
+            width={props.value * (parent.width / props.maxValue)}
+            fill={props.color}
+          />
         </svg>
       ) : (
         <svg width={props.barWeight} height={parent.height} transform={props.mirrored ? 'rotate(180 0 0)' : undefined}>
-          <path d={`M0,${parent.height}v${-(props.value * (parent.height / props.maxValue) - props.cornerRadius)}a${props.cornerRadius},${props.cornerRadius} 0 0 1 ${props.cornerRadius},-${props.cornerRadius}h${props.barWeight - (2 * props.cornerRadius)}a${props.cornerRadius},${props.cornerRadius} 0 0 1 ${props.cornerRadius},${props.cornerRadius}V${parent.height}z`} fill={props.color} />
+          <VxBar
+            x="0"
+            y={(props.maxValue - props.value) * (parent.height / props.maxValue)}
+            height={props.value * (parent.height / props.maxValue)}
+            width={props.barWeight}
+            fill={props.color}
+          />
         </svg>
       ))}
   </Responsive>
@@ -25,7 +37,6 @@ Bar.displayName = 'Bar';
 
 Bar.defaultProps = {
   barWeight: 6,
-  cornerRadius: 1,
   direction: 'horizontal',
   mirrored: false,
 };
@@ -39,10 +50,6 @@ Bar.propTypes = {
    * Sets the bar color.
    */
   color: PropTypes.string.isRequired,
-  /**
-   * Sets the corner radius.
-   */
-  cornerRadius: PropTypes.number,
   /**
    * Sets the bar direction.
    */
