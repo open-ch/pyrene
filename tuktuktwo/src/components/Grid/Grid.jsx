474df5f6d8b5e51ebefe9f29d265dd86fd5ec8ce
sladomic <michael.sladoje@outlook.com>
2019-10-02 11:22:06 +0200

    Add grid height

--- tuktuktwo/src/components/Grid/Grid.jsx
+++ tuktuktwo/src/components/Grid/Grid.jsx
@@ -3,37 +3,40 @@ import PropTypes from 'prop-types';
 import { GridColumns, GridRows } from '@vx/grid';
 import Responsive from '../Misc/Responsive';
 import Utils from '../../Utils';
+import './grid.css';
 
 const Grid = props => (
-  <Responsive>
-    {(parent) => {
-      const parentSize = props.direction === 'horizontal' ? parent.width : parent.height;
-      const color = '#979ca8';
-      return (
-        <svg width={parent.width} height={parent.height}>
-          {props.direction === 'vertical' ? (
-            <GridRows
-              scale={Utils.scaleLinear(parentSize, props.maxValue, props.direction)}
-              stroke={color}
-              strokeWidth={0.5}
-              width={parent.width}
-              height={parent.height}
-              numTicks={3}
-            />
-          ) : (
-            <GridColumns
-              scale={Utils.scaleLinear(parentSize, props.maxValue, props.direction)}
-              stroke={color}
-              strokeWidth={0.5}
-              width={parent.width}
-              height={parent.height}
-              numTicks={6}
-            />
-          )}
-        </svg>
-      );
-    }}
-  </Responsive>
+  <div styleName="grid">
+    <Responsive>
+      {(parent) => {
+        const parentSize = props.direction === 'horizontal' ? parent.width : parent.height;
+        const color = '#979ca8';
+        return (
+          <svg width={parent.width} height={parent.height}>
+            {props.direction === 'vertical' ? (
+              <GridRows
+                scale={Utils.scaleLinear(parentSize, props.maxValue, props.direction)}
+                stroke={color}
+                strokeWidth={0.5}
+                width={parent.width}
+                height={parent.height}
+                numTicks={3}
+              />
+            ) : (
+              <GridColumns
+                scale={Utils.scaleLinear(parentSize, props.maxValue, props.direction)}
+                stroke={color}
+                strokeWidth={0.5}
+                width={parent.width}
+                height={parent.height}
+                numTicks={6}
+              />
+            )}
+          </svg>
+        );
+      }}
+    </Responsive>
+  </div>
 );
 
 Grid.displayName = 'Grid';
