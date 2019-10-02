62e8b3007e7b0b02eab9406096d68e561c3cb3f9
sladomic <michael.sladoje@outlook.com>
2019-10-02 11:22:23 +0200

    Refactor Axis

--- tuktuktwo/src/components/Axis/CategoricalAxis.jsx
+++ tuktuktwo/src/components/Axis/CategoricalAxis.jsx
@@ -1,29 +1,78 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Axis from './Axis';
+import { AxisLeft, AxisBottom } from '@vx/axis';
+import Responsive from '../Misc/Responsive';
+import Utils from '../../Utils';
 import './axis.css';
+
+const getScale = (parentSize, labels) => (
+  Utils.scaleCategorical(parentSize, labels)
+);
 
 const CategoricalAxis = props => (
   <div styleName={classNames({ axisLeft: props.position === 'left', axisLeftWide: props.position === 'left', axisBottom: props.position === 'bottom' })}>
-    <Axis
-      labels={props.labels}
-      position={props.position}
-      tickFormat={props.tickFormat}
-      tickValues={props.loading ? [] : props.tickValues}
-    />
+    <Responsive>
+      {(parent) => {
+        const parentSize = props.position === 'left' ? parent.height : parent.width;
+        const scale = getScale(parentSize, props.labels);
+        // const color = '#979ca8';
+        const color = 'red';
+        return (
+          <svg width={parent.width} height={parent.height}>
+            {props.position === 'left' ? (
+              <AxisLeft
+                scale={scale}
+                left={98}
+                tickLabelProps={() => ({
+                  textAnchor: 'start', fontSize: 10, fill: color, dx: '-90px', dy: '0.25em',
+                })}
+                stroke={color}
+                strokeWidth={2}
+                tickStroke={color}
+                numTicks={props.labels.length}
+                hideAxisLine={props.hideAxisLine}
+                tickFormat={props.tickFormat}
+                tickValues={props.loading ? [] : props.tickValues}
+                hideTicks
+                hideZero
+              />
+            ) : (
+              <AxisBottom
+                scale={scale}
+                tickLabelProps={() => ({
+                  textAnchor: 'middle', fontSize: 10, fill: color, dy: '-0.25em',
+                })}
+                stroke={color}
+                strokeWidth={2}
+                tickStroke={color}
+                numTicks={props.labels.length}
+                hideAxisLine={props.hideAxisLine}
+                tickFormat={props.tickFormat}
+                tickValues={props.loading ? [] : props.tickValues}
+                hideTicks
+                hideZero
+              />
+            )}
+          </svg>
+        );
+      }}
+
+    </Responsive>
   </div>
 );
 
 CategoricalAxis.displayName = 'Categorical Axis';
 
 CategoricalAxis.defaultProps = {
+  hideAxisLine: false,
   tickFormat: d => d,
   loading: false,
   tickValues: undefined,
 };
 
 CategoricalAxis.propTypes = {
+  hideAxisLine: PropTypes.bool,
   labels: PropTypes.arrayOf(PropTypes.string).isRequired,
   loading: PropTypes.bool,
   position: PropTypes.oneOf(['left', 'bottom']).isRequired,
--- tuktuktwo/src/components/Axis/NumericalAxis.jsx
+++ tuktuktwo/src/components/Axis/NumericalAxis.jsx
@@ -1,24 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Axis from './Axis';
+import { AxisLeft, AxisBottom } from '@vx/axis';
+import Responsive from '../Misc/Responsive';
+import Utils from '../../Utils';
 import './axis.css';
+
+const getScale = (parentSize, maxValue, position, scale) => (
+  scale !== undefined ? scale : Utils.scaleLinear(parentSize, maxValue, position === 'left' ? 'vertical' : 'horizontal')
+);
 
 const NumericalAxis = props => (
   <div styleName={classNames({ axisLeft: props.position === 'left', axisLeftNarrow: props.position === 'left', axisBottom: props.position === 'bottom' })}>
-    <Axis
-      maxValue={props.maxValue}
-      position={props.position}
-      scale={props.scale}
-      tickFormat={props.tickFormat}
-      tickValues={props.loading ? [] : props.tickValues}
-    />
+    <Responsive>
+      {(parent) => {
+        const parentSize = props.position === 'left' ? parent.height : parent.width;
+        const scale = getScale(parentSize, props.maxValue, props.position, props.scale);
+        // const color = '#979ca8';
+        const color = 'red';
+        return (
+          <svg width={parent.width} height={parent.height}>
+            {props.position === 'left' ? (
+              <AxisLeft
+                scale={scale}
+                left={34}
+                tickLabelProps={() => ({
+                  textAnchor: 'start', fontSize: 10, fill: color, dx: '-26px', dy: '0.25em',
+                })}
+                stroke={color}
+                strokeWidth={2}
+                tickStroke={color}
+                numTicks={3}
+                hideAxisLine={props.hideAxisLine}
+                tickFormat={props.tickFormat}
+                tickValues={props.loading ? [] : props.tickValues}
+                hideTicks
+                hideZero
+              />
+            ) : (
+              <AxisBottom
+                scale={scale}
+                tickLabelProps={() => ({
+                  textAnchor: 'middle', fontSize: 10, fill: color, dy: '-0.25em',
+                })}
+                stroke={color}
+                strokeWidth={2}
+                tickStroke={color}
+                numTicks={6}
+                hideAxisLine={props.hideAxisLine}
+                tickFormat={props.tickFormat}
+                tickValues={props.loading ? [] : props.tickValues}
+                hideTicks
+                hideZero
+              />
+            )}
+          </svg>
+        );
+      }}
+
+    </Responsive>
   </div>
 );
 
 NumericalAxis.displayName = 'Numerical Axis';
 
 NumericalAxis.defaultProps = {
+  hideAxisLine: false,
   loading: false,
   scale: undefined,
   tickFormat: d => d,
@@ -26,6 +73,7 @@ NumericalAxis.defaultProps = {
 };
 
 NumericalAxis.propTypes = {
+  hideAxisLine: PropTypes.bool,
   loading: PropTypes.bool,
   maxValue: PropTypes.number.isRequired,
   position: PropTypes.oneOf(['left', 'bottom']).isRequired,
