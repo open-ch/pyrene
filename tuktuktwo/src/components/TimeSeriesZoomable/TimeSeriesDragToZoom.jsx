517017a2f27a9df707288079807bf99ef0fa755b
xuz <xuz@open.ch>
2019-10-16 17:27:26 +0200

    fixed zoom tooltip

--- tuktuktwo/src/components/TimeSeriesZoomable/TimeSeriesDragToZoom.jsx
+++ tuktuktwo/src/components/TimeSeriesZoomable/TimeSeriesDragToZoom.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment-timezone';
 import { Drag } from '@vx/drag';
 import { scaleTime } from '@vx/scale';
-import { checkZoomInBound } from './TimeSeriesZoomButtons';
+import { checkZoomInBound, minZoomRangeReached } from './ZoomUtil';
 import './timeSeriesZoomable.css';
 
+const MARGIN_TOP = 16;
 const MARGIN_LEFT = 36;
+const MOUSE_TOOLTIP_OFFSET_X = 12;
+const MOUSE_TOOLTIP_OFFSET_Y = 17;
+const MOUSE_TOOLTIP_TEXT_OFFSET_X = 28;
+const MOUSE_TOOLTIP_TEXT_OFFSET_Y = 22;
+const TOOLTIP_WIDTH = 267;
+const TOOLTIP_HEIGHT = 34;
+const TOOLTIP_TEXT_WIDTH = 237;
+const TOOLTIP_BG_COLOR = '#454d61';
+const TOOLTIP_COLOR = '#ffffff';
 
 let dragStartX = 0;
 let dragEndX = 0;
+let xScale = null;
 
 const _onDragEnd = (props) => {
   // Convert drag area to timestamp
-  const xScale = scaleTime({
-    range: [props.from, props.to],
-    domain: [MARGIN_LEFT, props.width + MARGIN_LEFT],
-  });
-
   const newFrom = Math.ceil(Math.min(xScale(dragStartX), xScale(dragEndX)));
   const newTo = Math.floor(Math.max(xScale(dragStartX), xScale(dragEndX)));
   const boundedTimeRange = checkZoomInBound(newFrom, newTo, props.minZoomRange);
@@ -25,71 +32,150 @@ const _onDragEnd = (props) => {
   props.onZoom(boundedTimeRange.from, boundedTimeRange.to);
 };
 
-const _minZoomRangeReached = props => props.to - props.from === props.minZoomRange
+const _getZoomWidth = (x, dx, maxWidthLeft) => {
+  return ((x + dx < MARGIN_LEFT) && Math.abs(dx) > maxWidthLeft) ? maxWidthLeft : Math.abs(dx);
+};
+
+const _getZoomX = (x, dx) => {
+  if (dx > 0) {
+    return x;
+  }
+  return (x + dx) < MARGIN_LEFT ? MARGIN_LEFT : (x + dx);
+};
+
+const _getTooltipX = (x, dx, maxWidth) => {
+  if (x + dx + MOUSE_TOOLTIP_OFFSET_X + TOOLTIP_WIDTH > maxWidth) {
+    return x + dx - MOUSE_TOOLTIP_OFFSET_X - TOOLTIP_WIDTH;
+  }
+  return x + dx + MOUSE_TOOLTIP_OFFSET_X;
+};
+
+const _getTooltipTextX = (x, dx, maxWidth) => {
+  if (x + dx + MOUSE_TOOLTIP_OFFSET_X + TOOLTIP_WIDTH > maxWidth) {
+    return x + dx - MOUSE_TOOLTIP_TEXT_OFFSET_X - TOOLTIP_TEXT_WIDTH;
+  }
+  return x + dx + MOUSE_TOOLTIP_TEXT_OFFSET_X;
+};
+
+const _getTooltipY = (y, dy, maxHeight) => {
+  if (y + dy + MOUSE_TOOLTIP_OFFSET_Y > maxHeight) {
+    return 2 * maxHeight - 3 * MOUSE_TOOLTIP_OFFSET_Y - y - dy;
+  }
+  return Math.abs(y + dy - MOUSE_TOOLTIP_OFFSET_Y);
+};
+
+const _getTooltipTextY = (y, dy, maxHeight) => {
+  if (y + dy + MOUSE_TOOLTIP_OFFSET_Y > maxHeight) {
+    return 2 * maxHeight - 3 * MOUSE_TOOLTIP_OFFSET_Y - y - dy + MOUSE_TOOLTIP_TEXT_OFFSET_Y;
+  }
+  return Math.abs(y + dy - MOUSE_TOOLTIP_OFFSET_Y) + MOUSE_TOOLTIP_TEXT_OFFSET_Y;
+};
+
+const _getTooltipData = (x, dx, timezone) => {
+  const from = Math.ceil(xScale(Math.min(x, x + dx)));
+  const to = Math.floor(xScale(Math.max(x, x + dx)));
+  return `${moment.tz(from, timezone).format('DD.MM.YYYY, HH:mm')} - ${moment.tz(to, timezone).format('DD.MM.YYYY, HH:mm')}`;
+};
 
 const TimeSeriesDragToZoom = (props) => {
+  const cursorStyle = (minZoomRangeReached(props) ? '' : 'dragArea');
+
+  xScale = scaleTime({
+    range: [props.from, props.to],
+    domain: [MARGIN_LEFT, props.width + MARGIN_LEFT],
+  });
+
   return (
-    <Drag
-      width={props.width}
-      height={props.height}
-      onDragStart={({ x, y }) => {
-        dragStartX = x;
-      }}
-      onDragMove={({ x, y, dx, dy }) => {
-        // Display tooltip
-      }}
-      onDragEnd={({ x, y, dx, dy }) => {
-        dragEndX = x + dx;
-        _onDragEnd(props);
-      }}
-      resetOnStart
-    >
-      {({
-        x,
-        y,
-        dx,
-        dy,
-        isDragging,
-        dragStart,
-        dragEnd,
-        dragMove,
-      }) => {
-        return (
-          <g>
-            {isDragging && (
+    <g styleName={cursorStyle}>
+      <Drag
+        width={props.width}
+        height={props.height}
+        onDragStart={({ x, y }) => {
+          dragStartX = x;
+        }}
+        onDragMove={({ x, y, dx, dy }) => {
+          // Display tooltip
+        }}
+        onDragEnd={({ x, y, dx, dy }) => {
+          dragEndX = x + dx;
+          _onDragEnd(props);
+        }}
+        resetOnStart
+      >
+        {({
+          x,
+          y,
+          dx,
+          dy,
+          isDragging,
+          dragStart,
+          dragEnd,
+          dragMove,
+        }) => {
+          const maxWidthLeft = x - MARGIN_LEFT;
+          return (
+            <g>
+              {/* Draw rectangle */}
+              {isDragging && (
+                <g>
+                  <rect
+                    fill="#1d273b"
+                    opacity={0.25}
+                    width={_getZoomWidth(x, dx, maxWidthLeft)}
+                    height={props.height}
+                    x={_getZoomX(x, dx)}
+                    y={MARGIN_TOP}
+                    style={{ pointerEvents: 'none' }}
+                  />
+                  {/* Display the tooltip */}
+                  <svg shapeRendering="geometricPrecision">
+                    <g>
+                      <rect
+                        x={_getTooltipX(x, dx, props.width)}
+                        y={_getTooltipY(y, dy, props.height + MARGIN_TOP)}
+                        fill={TOOLTIP_BG_COLOR}
+                        width={TOOLTIP_WIDTH}
+                        height={TOOLTIP_HEIGHT}
+                        rx={2}
+                        ry={2}
+                      />
+                      <text
+                        x={_getTooltipTextX(x, dx, props.width)}
+                        y={_getTooltipTextY(y, dy, props.height + MARGIN_TOP)}
+                        fill={TOOLTIP_COLOR}
+                        styleName="tooltipText"
+                      >
+                        {_getTooltipData(x, dx, props.timezone)}
+                      </text>
+                    </g>
+                  </svg>
+                </g>
+              )}
+              {/* Define the drawing area for zoom */}
               <rect
-                fill="#1d273b"
-                opacity={0.25}
-                width={Math.abs(dx)}
-                height={props.height}
-                x={dx > 0 ? x : x + dx}
-                y={0}
-                style={{ pointerEvents: 'none' }}
+                y={MARGIN_TOP}
+                x={MARGIN_LEFT}
+                fill="transparent"
+                width={props.width > 0 ? props.width : 0}
+                height={props.height > 0 ? props.height : 0}
+                onMouseDown={minZoomRangeReached(props) ? () => {} : dragStart}
+                onMouseUp={minZoomRangeReached(props) ? () => {} : dragEnd}
+                onMouseMove={minZoomRangeReached(props) ? () => {} : dragMove}
+                onTouchStart={minZoomRangeReached(props) ? () => {} : dragStart}
+                onTouchEnd={minZoomRangeReached(props) ? () => {} : dragEnd}
+                onTouchMove={minZoomRangeReached(props) ? () => {} : dragMove}
               />
-            )}
-            <rect
-              x={MARGIN_LEFT}
-              fill="transparent"
-              width={props.width > 0 ? props.width : 0}
-              height={props.height > 0 ? props.height : 0}
-              onMouseDown={_minZoomRangeReached(props) ? () => {} : dragStart}
-              onMouseUp={_minZoomRangeReached(props) ? () => {} : dragEnd}
-              onMouseMove={_minZoomRangeReached(props) ? () => {} : dragMove}
-              onTouchStart={_minZoomRangeReached(props) ? () => {} : dragStart}
-              onTouchEnd={_minZoomRangeReached(props) ? () => {} : dragEnd}
-              onTouchMove={_minZoomRangeReached(props) ? () => {} : dragMove}
-            />
-          </g>
-        );
-      }}
-    </Drag>
+            </g>
+          );
+        }}
+      </Drag>
+    </g>
   );
 };
 
 TimeSeriesDragToZoom.displayName = 'TimeSeriesDragToZoom';
 
 TimeSeriesDragToZoom.propTypes = {
-
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
   from: PropTypes.number.isRequired,
@@ -98,6 +184,7 @@ TimeSeriesDragToZoom.propTypes = {
   upperBound: PropTypes.number.isRequired,
   minZoomRange: PropTypes.number.isRequired,
   onZoom: PropTypes.func.isRequired,
+  timezone: PropTypes.string.isRequired,
 };
 
 export default TimeSeriesDragToZoom;
--- tuktuktwo/src/components/TimeSeriesZoomable/TimeSeriesZoomButtons.jsx
+++ tuktuktwo/src/components/TimeSeriesZoomable/TimeSeriesZoomButtons.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './timeSeriesZoomable.css';
-
-export const checkZoomInBound = (from, to, minZoomRange) => {
-  const boundedTimeRange = {
-    from: from,
-    to: to,
-  };
-  if (to - from < minZoomRange) {
-    const timeRangeOverflow = minZoomRange - (to - from);
-    const fromCompensation = Math.round(timeRangeOverflow / 2);
-    boundedTimeRange.from = from - fromCompensation;
-    boundedTimeRange.to = to + (timeRangeOverflow - fromCompensation);
-  }
-  return boundedTimeRange;
-};
+import { checkZoomInBound, minZoomRangeReached, zoomBoundReached } from './ZoomUtil';
 
 const _onZoomIn = (props) => {
   const zoomedInTimeRange = (props.to - props.from) * 0.25;
@@ -28,31 +16,36 @@ const _onZoomIn = (props) => {
 };
 
 const _onZoomOut = (props) => {
-  const zoomedInTimeRange = (props.to - props.from) * 0.25;
-  const zoomDiff = Math.floor(zoomedInTimeRange / 2);
+  const zoomedOutTimeRange = (props.to - props.from) / 0.75;
+  const zoomDiff = (zoomedOutTimeRange - (props.to - props.from)) / 2;
 
   // Make sure zoom does not exceed bounds
-  const newFrom = (props.from - zoomDiff >= props.lowerBound) ? (props.from - zoomDiff) : props.lowerBound;
-  const newTo = (props.to + zoomDiff <= props.upperBound) ? (props.to + zoomDiff) : props.upperBound;
+  const newFrom = (Math.floor(props.from - zoomDiff) >= props.lowerBound) ? Math.floor(props.from - zoomDiff) : props.lowerBound;
+  const newTo = (Math.ceil(props.to + zoomDiff) <= props.upperBound) ? Math.ceil(props.to + zoomDiff) : props.upperBound;
 
   // Execute callback
   props.onZoom(newFrom, newTo);
 };
 
-const TimeSeriesZoomButtons = props => (
-  <div styleName="zoomControls">
-    <div styleName="zoomBtn" onClick={() => _onZoomIn(props)}>
-      <div styleName="leftIconContainer">
-        {props.renderZoomInBtn()}
+const TimeSeriesZoomButtons = (props) => {
+  // Very complicated check here
+  const zoomInBtnState = minZoomRangeReached(props) ? '' : 'activeBtn';
+  const zoomOutBtnState = zoomBoundReached(props) ? '' : 'activeBtn';
+  return (
+    <div styleName="zoomControls">
+      <div styleName={classNames('zoomBtn', zoomInBtnState)} onClick={() => _onZoomIn(props)}>
+        <div styleName="leftIconContainer">
+          {props.renderZoomInBtn()}
+        </div>
+      </div>
+      <div styleName={classNames('zoomBtn', zoomOutBtnState)} onClick={() => _onZoomOut(props)}>
+        <div styleName="rightIconContainer">
+          {props.renderZoomOutBtn()}
+        </div>
       </div>
     </div>
-    <div styleName="zoomBtn" onClick={() => _onZoomOut(props)}>
-      <div styleName="rightIconContainer">
-        {props.renderZoomOutBtn()}
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 TimeSeriesZoomButtons.displayName = 'TimeSeriesZoomButtons';
 
