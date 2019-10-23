3c53f89358ddeaeb8e521418d54df2ed00a913c8
sladomic <michael.sladoje@outlook.com>
2019-10-23 11:36:11 +0200

    Rearrange BarChart examples and add tickFormatNumerical prop

--- kitchensink/src/app/static/howToPages/BarChart.jsx
+++ kitchensink/src/app/static/howToPages/BarChart.jsx
@@ -4,6 +4,22 @@ import { BarChart } from 'pyrene-graphs/dist/pyrene-graphs.dev';
 import { exampleData } from 'pyrene-graphs/dist/pyrene-graphs.examples';
 
 const barChartHowTo = [{
+  title: 'Vertical Bar Chart',
+  description: 'Vertical Bar Chart with one value and formatted tick labels',
+  component: () => (
+    <BarChart
+      data={exampleData.applications.map(d => ({
+        label: d.label,
+        values: d.values.map(e => e * 100),
+      }))}
+      header="Applications"
+      description="Optional description and explanation on how to read the chart"
+      legend={['Volume']}
+      direction="vertical"
+      tickFormatNumerical={d => (parseFloat(d) >= 100000 ? `${parseFloat(d) / 1000}k` : d)}
+    />
+  ),
+}, {
   title: 'Horizontal Bar Chart',
   description: 'Horizontal Bar Chart with one value',
   component: () => (
@@ -12,29 +28,7 @@ const barChartHowTo = [{
       header="Applications"
       description="Optional description and explanation on how to read the chart"
       legend={['Volume']}
-    />
-  ),
-}, {
-  title: 'Vertical Bar Chart',
-  description: 'Vertical Bar Chart with one value',
-  component: () => (
-    <BarChart
-      data={exampleData.applications}
-      header="Applications"
-      description="Optional description and explanation on how to read the chart"
-      legend={['Volume']}
-      direction="vertical"
-    />
-  ),
-}, {
-  title: 'Stacked Horizontal Bar Chart',
-  description: 'Horizontal Bar Chart with multiple values',
-  component: () => (
-    <BarChart
-      data={exampleData.nsmSensors}
-      header="Network Security Monitoring"
-      description="Optional description and explanation on how to read the chart"
-      legend={['high', 'moderate', 'low']}
+      direction="horizontal"
     />
   ),
 }, {
@@ -47,6 +41,18 @@ const barChartHowTo = [{
       description="Optional description and explanation on how to read the chart"
       legend={['high', 'moderate', 'low']}
       direction="vertical"
+    />
+  ),
+}, {
+  title: 'Stacked Horizontal Bar Chart',
+  description: 'Horizontal Bar Chart with multiple values',
+  component: () => (
+    <BarChart
+      data={exampleData.nsmSensors}
+      header="Network Security Monitoring"
+      description="Optional description and explanation on how to read the chart"
+      legend={['high', 'moderate', 'low']}
+      direction="horizontal"
     />
   ),
 }];
