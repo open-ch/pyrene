c2105ff6f712446fba98cfc212035c09993ebce1
xuz <xuz@open.ch>
2019-10-24 11:13:36 +0200

    fixed broken tests

--- tuktuktwo/src/components/TimeXAxis/TimeXAxis.spec.jsx
+++ tuktuktwo/src/components/TimeXAxis/TimeXAxis.spec.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import moment from 'moment';
+import { scaleTime } from '@vx/scale';
 import TimeXAxis from './TimeXAxis';
 import { getTickValues } from './TimeXUtil';
 
@@ -17,96 +18,186 @@ const props = {
 const testTimeRanges = [
   {
     id: '0.5h',
-    from: 1569944820000, // 2019-10-01 23:47
-    to: 1569946320000, // 2019-10-01 00:12
+    from: moment.tz('2019-10-01 23:47', 'Asia/Shanghai').valueOf(),
+    to: moment.tz('2019-10-02 00:12', 'Asia/Shanghai').valueOf(),
     timezone: 'Asia/Shanghai',
-    expectedValues: [1569945000000, 1569945600000, 1569946200000], // 23:50, 00:00, 00:10
+    expectedValues: [
+      moment.tz('2019-10-01 23:50', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-02 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-02 00:10', 'Asia/Shanghai').valueOf()
+    ],
+    minMarginRight: 100,
   },
   {
     id: '1h',
-    from: 1569908640000, // 2019-10-01 13:44
-    to: 1569911580000, // 2019-10-01 14:33
+    from: moment.tz('2019-10-01 13:44', 'Europe/Zurich').valueOf(),
+    to: moment.tz('2019-10-01 14:33', 'Europe/Zurich').valueOf(),
     timezone: 'Europe/Zurich',
-    expectedValues: [1569909600000, 1569910800000], // 14:00, 14:20
+    expectedValues: [
+      moment.tz('2019-10-01 14:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-10-01 14:20', 'Europe/Zurich').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '6h',
-    from: 1569902100000, // 2019-10-01 11:55
-    to: 1569918000000, // 2019-10-01 16:20
+    from: moment.tz('2019-10-01 11:55', 'Asia/Shanghai').valueOf(),
+    to: moment.tz('2019-10-01 16:20', 'Asia/Shanghai').valueOf(),
     timezone: 'Asia/Shanghai',
-    expectedValues: [1569902400000, 1569906000000, 1569909600000, 1569913200000, 1569916800000], // 12:00, 13:00, 14:00, 15:00, 16:00
+    expectedValues: [
+      moment.tz('2019-10-01 12:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-01 13:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-01 14:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-01 15:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-01 16:00', 'Asia/Shanghai').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '12h',
-    from: 1569925980000, // 2019-10-01 18:33
-    to: 1569960840000, // 2019-10-02 04:14
+    from: moment.tz('2019-10-01 18:33', 'Asia/Shanghai').valueOf(),
+    to: moment.tz('2019-10-02 04:14', 'Asia/Shanghai').valueOf(),
     timezone: 'Asia/Shanghai',
-    expectedValues: [1569931200000, 1569938400000, 1569945600000, 1569952800000, 1569960000000], // 20:00, 22:00, 00:00, 02:00, 04:00
+    expectedValues: [
+      moment.tz('2019-10-01 20:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-01 22:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-02 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-02 02:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-02 04:00', 'Asia/Shanghai').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '1d',
-    from: 1601569980000, // 2019-10-01 18:33
-    to: 1601644440000, // 2019-10-02 15:14
+    from: moment.tz('2019-10-01 18:33', 'Europe/Zurich').valueOf(),
+    to: moment.tz('2019-10-02 15:14', 'Europe/Zurich').valueOf(),
     timezone: 'Europe/Zurich',
-    expectedValues: [1601575200000, 1601589600000, 1601604000000, 1601618400000, 1601632800000], // 20:00, 00:00, 04:00, 08:00, 12:00
+    expectedValues: [
+      moment.tz('2019-10-01 20:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-10-02 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-10-02 04:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-10-02 08:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-10-02 12:00', 'Europe/Zurich').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '2d',
-    from: 1601569980000, // 2020-10-01 18:33
-    to: 1601730840000, // 2020-10-03 15:14
+    from: moment.tz('2020-10-01 18:33', 'Europe/Zurich').valueOf(),
+    to: moment.tz('2020-10-03 15:14', 'Europe/Zurich').valueOf(),
     timezone: 'Europe/Zurich',
-    expectedValues: [1601589600000, 1601618400000, 1601647200000, 1601676000000, 1601704800000], // 00:00, 08:00, 16:00, 00:00, 08:00
+    expectedValues: [
+      moment.tz('2020-10-02 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2020-10-02 08:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2020-10-02 16:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2020-10-03 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2020-10-03 08:00', 'Europe/Zurich').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '1w',
-    from: 1569925980000, // 2019-10-01 18:33
-    to: 1570432440000, // 2019-10-07 15:14
+    from: moment.tz('2019-10-01 18:33', 'Asia/Shanghai').valueOf(),
+    to: moment.tz('2019-10-07 15:14', 'Asia/Shanghai').valueOf(),
     timezone: 'Asia/Shanghai',
-    expectedValues: [1569945600000, 1570032000000, 1570118400000, 1570204800000, 1570291200000, 1570377600000], // 00:00 of each day
+    expectedValues: [
+      moment.tz('2019-10-02 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-03 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-04 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-05 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-06 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-07 00:00', 'Asia/Shanghai').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '1m',
-    from: 1569925980000, // 2019-10-01 18:33
-    to: 1572246840000, // 2019-10-28 15:14
+    from: moment.tz('2019-10-01 18:33', 'Asia/Shanghai').valueOf(),
+    to: moment.tz('2019-10-28 15:14', 'Asia/Shanghai').valueOf(),
     timezone: 'Asia/Shanghai',
-    expectedValues: [1570377600000, 1570982400000, 1571587200000, 1572192000000], // 00:00 of each Monday
+    expectedValues: [
+      moment.tz('2019-10-07 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-14 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-21 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2019-10-28 00:00', 'Asia/Shanghai').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '3m',
-    from: 1569947580000, // 2019-10-01 18:33
-    to: 1576246440000, // 2019-12-13 15:14
+    from: moment.tz('2019-10-01 18:33', 'Europe/Zurich').valueOf(),
+    to: moment.tz('2019-12-13 15:14', 'Europe/Zurich').valueOf(),
     timezone: 'Europe/Zurich',
-    expectedValues: [1571004000000, 1572217200000, 1573426800000, 1574636400000, 1575846000000], // 00:00 of every other Monday
+    expectedValues: [
+      moment.tz('2019-10-14 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-10-28 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-11-11 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-11-25 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-12-09 00:00', 'Europe/Zurich').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '6m',
-    from: 1569947580000, // 2019-10-01 18:33
-    to: 1582467240000, // 2020-02-23 15:14
+    from: moment.tz('2019-10-01 18:33', 'Europe/Zurich').valueOf(),
+    to: moment.tz('2020-02-23 15:14', 'Europe/Zurich').valueOf(),
     timezone: 'Europe/Zurich',
-    expectedValues: [1572562800000, 1575154800000, 1577833200000, 1580511600000], // 00:00 of first day of a month
+    expectedValues: [
+      moment.tz('2019-11-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2019-12-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2020-01-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2020-02-01 00:00', 'Europe/Zurich').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '1y',
-    from: 1569925980000, // 2019-10-01 18:33
-    to: 1595488440000, // 2020-07-23 15:14
+    from: moment.tz('2019-10-01 18:33', 'Asia/Shanghai').valueOf(),
+    to: moment.tz('2020-07-23 15:14', 'Asia/Shanghai').valueOf(),
     timezone: 'Asia/Shanghai',
-    expectedValues: [1572537600000, 1577808000000, 1582992000000, 1588262400000, 1593532800000], // 00:00 of first day of every other month
+    expectedValues: [
+      moment.tz('2019-11-01 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2020-01-01 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2020-03-01 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2020-05-01 00:00', 'Asia/Shanghai').valueOf(),
+      moment.tz('2020-07-01 00:00', 'Asia/Shanghai').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '2y',
-    from: 1569925980000, // 2019-10-01 18:33
-    to: 1629724440000, // 2021-03-23 15:14
+    from: moment.tz('2019-10-01 18:33', 'Europe/Zurich').valueOf(),
+    to: moment.tz('2021-03-23 15:14', 'Europe/Zurich').valueOf(),
     timezone: 'Europe/Zurich',
-    expectedValues: [1577833200000, 1588284000000, 1598911200000, 1609455600000, 1619820000000], // 00:00 of first day of every 4th month
+    expectedValues: [
+      moment.tz('2020-01-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2020-05-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2020-09-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2021-01-01 00:00', 'Europe/Zurich').valueOf(),
+    ],
+    minMarginRight: 100,
   },
   {
     id: '>2y',
-    from: 1569925980000, // 2019-10-01 18:33
-    to: 1661260440000, // 2022-03-23 15:14
+    from: moment.tz('2019-10-01 18:33', 'Europe/Zurich').valueOf(),
+    to: moment.tz('2022-03-23 15:14', 'Europe/Zurich').valueOf(),
     timezone: 'Europe/Zurich',
-    expectedValues: [1577833200000, 1593554400000, 1609455600000, 1625090400000, 1640991600000, 1656626400000], // 00:00 of first day of every 6th month
+    expectedValues: [
+      moment.tz('2020-01-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2020-07-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2021-01-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2021-07-01 00:00', 'Europe/Zurich').valueOf(),
+      moment.tz('2022-01-01 00:00', 'Europe/Zurich').valueOf(),
+    ],
+    minMarginRight: 100,
   },
 ];
+
+const svgWrapper = bar => (
+  <svg width="1334" height="320">
+    {bar}
+  </svg>
+);
 
 describe('<TimeXAxis />', () => {
   it('renders without crashing', () => {
@@ -114,25 +205,26 @@ describe('<TimeXAxis />', () => {
   });
 
   it('displays the content', () => {
-    const rendered = shallow(<TimeXAxis {...props} />);
-    const renderedAxis = rendered.find('g').children().at(0);
-    expect(renderedAxis.props().tickValues.length).toBeGreaterThan(0);
-    expect(typeof renderedAxis.props().tickComponent).toBe('function');
-    expect(typeof renderedAxis.props().tickFormat).toBe('function');
-    expect(typeof renderedAxis.props().tickLabelProps).toBe('function');
+    const rendered = mount(svgWrapper(<TimeXAxis {...props} />));
+    expect(rendered.find('.vx-group').length).toBeGreaterThan(0);
+    expect(rendered.find('.vx-axis').length).toBeGreaterThan(0);
+    expect(rendered.find('.vx-columns').length).toBeGreaterThan(0);
   });
 
   it('does not render grid', () => {
-    const rendered = shallow(<TimeXAxis showGrid={false} {...props} />);
-    const renderedGrid = rendered.find('g').children().at(1);
-    expect(renderedGrid).toHaveLength(0);
+    const rendered = mount(svgWrapper(<TimeXAxis showGrid={false} {...props} />));
+    expect(rendered.find('.vx-columns')).toHaveLength(0);
   });
 });
 
 describe('TickValues', () => {
   testTimeRanges.forEach((testTimeRange) => {
     it('calculates the correct tick values', () => {
-      const result = getTickValues(testTimeRange.from, testTimeRange.to, testTimeRange.timezone);
+      const xScale = scaleTime({
+        range: [0, 1334],
+        domain: [testTimeRange.from, testTimeRange.to],
+      });
+      const result = getTickValues(testTimeRange.from, testTimeRange.to, testTimeRange.timezone, xScale);
       expect(result).toStrictEqual(testTimeRange.expectedValues);
     });
   });
