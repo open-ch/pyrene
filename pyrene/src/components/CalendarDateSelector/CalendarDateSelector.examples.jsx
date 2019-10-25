e43b659c70bc6869eadc26cca52a92ee179ea8de
lga <lga@open.ch>
2019-10-24 18:36:25 -0700

    made timeUnit mandatory (since we have to update it with onChange)
    Former-commit-id: c75d36d15f676a76217b7111c27ff4fa563463ab

--- pyrene/src/components/CalendarDateSelector/CalendarDateSelector.examples.jsx
+++ pyrene/src/components/CalendarDateSelector/CalendarDateSelector.examples.jsx
@@ -2,7 +2,7 @@ const examples = {
   props: {
     onChange: stateProvider => (value, timeUnit) => stateProvider.setState({ value: value, timeUnit: timeUnit }),
     value: stateProvider => stateProvider.state.value,
-    timeUnit: stateProvider => stateProvider.state.timeUnit,
+    timeUnit: stateProvider => (stateProvider.state.timeUnit === undefined ? 'month' : stateProvider.state.timeUnit),
   },
 };
 
