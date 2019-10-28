234088dc64c3ec0062482bb51fc6d591c1d39bbd
uvi <uvi@open.ch>
2019-10-28 15:36:21 +0100

    don't export unused parts

--- pyrene-graphs/src/index.js
+++ pyrene-graphs/src/index.js
@@ -4,24 +4,18 @@
 // Sort alphabetically to find stuff easily
 import { default as BarChart } from './components/BarChart/BarChart';
 import { default as BarChartTable } from './components/BarChartTable/BarChartTable';
-import { default as TimeSeriesPlayground } from './components/TimeSeriesPlayground/TimeSeriesPlayground';
 import { default as TimeSeriesPlaygroundWithTooltip } from './components/TimeSeriesPlayground/TimeSeriesPlaygroundWithTooltip';
-import { default as Tooltip } from './components/TimeSeries/Tooltip';
 
 // Sort alphabetically to find stuff easily
 const Components = {
   BarChart,
   BarChartTable,
-  TimeSeriesPlayground,
   TimeSeriesPlaygroundWithTooltip,
-  Tooltip,
 };
 
 // Sort alphabetically to find stuff easily
 export { BarChart };
 export { BarChartTable };
 export { TimeSeriesPlaygroundWithTooltip }
-export { TimeSeriesPlayground };
-export { Tooltip };
 
 export default Components;
