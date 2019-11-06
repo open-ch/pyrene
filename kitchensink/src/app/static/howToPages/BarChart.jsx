743aef167b61b341c7b23d29f01fd0ada21f4577
sladomic <michael.sladoje@outlook.com>
2019-11-06 17:27:57 +0100

    Move examples legends to pyrene-graphs

--- kitchensink/src/app/static/howToPages/BarChart.jsx
+++ kitchensink/src/app/static/howToPages/BarChart.jsx
@@ -3,20 +3,18 @@ import React from 'react';
 import { BarChart } from 'pyrene-graphs/dist/pyrene-graphs.dev';
 import { exampleData } from 'pyrene-graphs/dist/pyrene-graphs.examples';
 
-const legendStacked = ['Incident', 'Emergency', 'Maintenance', 'Change', 'Installation'];
-
 const barChartHowTo = [{
   title: 'Vertical Bar Chart',
   description: 'Vertical Bar Chart with one value and formatted tick labels',
   component: () => (
     <BarChart
-      data={exampleData.applications.map((d) => ({
+      data={exampleData.applications.data.map((d) => ({
         label: d.label,
         values: d.values.map((e) => e * 100),
       }))}
       title="Applications"
       description="Optional description and explanation on how to read the chart"
-      legend={['Volume']}
+      legend={exampleData.applications.legend}
       direction="vertical"
       tickFormatNumerical={(d) => (parseFloat(d) >= 100000 ? `${parseFloat(d) / 1000}k` : d)}
     />
@@ -26,10 +24,10 @@ const barChartHowTo = [{
   description: 'Horizontal Bar Chart with one value',
   component: () => (
     <BarChart
-      data={exampleData.applications}
+      data={exampleData.applications.data}
       title="Applications"
       description="Optional description and explanation on how to read the chart"
-      legend={['Volume']}
+      legend={exampleData.applications.legend}
       direction="horizontal"
     />
   ),
@@ -38,10 +36,10 @@ const barChartHowTo = [{
   description: 'Vertical Bar Chart with multiple values',
   component: () => (
     <BarChart
-      data={exampleData.stacked}
+      data={exampleData.stacked.data}
       title="Vertical Stacked Bar Chart"
       description="Optional description and explanation on how to read the chart"
-      legend={legendStacked}
+      legend={exampleData.stacked.legend}
       direction="vertical"
     />
   ),
@@ -50,10 +48,10 @@ const barChartHowTo = [{
   description: 'Horizontal Bar Chart with multiple values',
   component: () => (
     <BarChart
-      data={exampleData.stacked}
+      data={exampleData.stacked.data}
       title="Horizontal Stacked Bar Chart"
       description="Optional description and explanation on how to read the chart"
-      legend={legendStacked}
+      legend={exampleData.stacked.legend}
       direction="horizontal"
     />
   ),
