11e366d50b7abf9f62c1d942a2f1f3ecc97f67e4
sladomic <michael.sladoje@outlook.com>
2019-11-06 17:27:43 +0100

    Move examples legends to pyrene-graphs

--- pyrene-graphs/src/examples/barData.js
+++ pyrene-graphs/src/examples/barData.js
@@ -1,56 +1,63 @@
 /* eslint-disable import/prefer-default-export */
-export const applications = [
-  {
-    label: 'Dropbox',
-    values: [1147.4],
-  },
-  {
-    label: 'Youtube',
-    values: [849.9],
-  },
-  {
-    label: 'Google',
-    values: [558.1],
-  },
-  {
-    label: 'Facebook',
-    values: [345.4],
-  },
-  {
-    label: 'Qlik',
-    values: [200.2],
-  },
-  {
-    label: 'We Transfer',
-    values: [192.1],
-  },
-  {
-    label: 'Asana',
-    values: [145.3],
-  },
-];
 
-export const stacked = [
-  {
-    label: '2012',
-    values: [89, 71, 85, 151, 212],
-  }, {
-    label: '2013',
-    values: [99, 73, 86, 170, 217],
-  }, {
-    label: '2014',
-    values: [90, 71, 80, 165, 200],
-  }, {
-    label: '2015',
-    values: [45, 53, 199, 124, 110],
-  }, {
-    label: '2016',
-    values: [63, 78, 212, 111, 110],
-  }, {
-    label: '2017',
-    values: [43, 44, 244, 109, 99],
-  }, {
-    label: '2018',
-    values: [66, 69, 270, 115, 108],
-  },
-];
+export const applications = {
+  data: [
+    {
+      label: 'Dropbox',
+      values: [1147.4],
+    },
+    {
+      label: 'Youtube',
+      values: [849.9],
+    },
+    {
+      label: 'Google',
+      values: [558.1],
+    },
+    {
+      label: 'Facebook',
+      values: [345.4],
+    },
+    {
+      label: 'Qlik',
+      values: [200.2],
+    },
+    {
+      label: 'We Transfer',
+      values: [192.1],
+    },
+    {
+      label: 'Asana',
+      values: [145.3],
+    },
+  ],
+  legend: ['Volume'],
+};
+
+export const stacked = {
+  data: [
+    {
+      label: '2012',
+      values: [89, 71, 85, 151, 212],
+    }, {
+      label: '2013',
+      values: [99, 73, 86, 170, 217],
+    }, {
+      label: '2014',
+      values: [90, 71, 80, 165, 200],
+    }, {
+      label: '2015',
+      values: [45, 53, 199, 124, 110],
+    }, {
+      label: '2016',
+      values: [63, 78, 212, 111, 110],
+    }, {
+      label: '2017',
+      values: [43, 44, 244, 109, 99],
+    }, {
+      label: '2018',
+      values: [66, 69, 270, 115, 108],
+    },
+  ],
+  legend: ['Incident', 'Emergency', 'Maintenance', 'Change', 'Installation'],
+};
