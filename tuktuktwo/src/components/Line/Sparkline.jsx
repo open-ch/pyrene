5e72aebfe48dfdae6370785fe68935e2946a11d6
sladomic <michael.sladoje@outlook.com>
2019-11-20 09:17:02 +0100

    Add gradient to SparkLine

--- tuktuktwo/src/components/Line/Sparkline.jsx
+++ tuktuktwo/src/components/Line/Sparkline.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { LinePath } from '@vx/shape';
+import { AreaClosed, LinePath } from '@vx/shape';
 import { scaleTime, scaleLinear } from '@vx/scale';
+import { LinearGradient } from '@vx/gradient';
 
 const SparkLine = (props) => {
   const x = (d) => d[0];
@@ -17,12 +18,30 @@ const SparkLine = (props) => {
   });
 
   return (
-    <LinePath
-      data={props.dataSeries.data}
-      x={(d) => xScale(x(d))}
-      y={(d) => yScale(y(d))}
-      stroke={props.colors[0]}
-    />
+    // shapeRendering="auto" to have nicer lines
+    <svg shapeRendering="auto">
+      <LinearGradient
+        id="gradient"
+        fromOpacity={props.gradient.fromOpacity}
+        toOpacity={props.gradient.toOpacity}
+        from={props.gradient.fromColor}
+        to={props.gradient.toColor}
+      />
+      <AreaClosed
+        data={props.dataSeries.data}
+        x={(d) => xScale(x(d))}
+        y={(d) => yScale(y(d))}
+        yScale={yScale}
+        stroke="transparent"
+        fill="url(#gradient)"
+      />
+      <LinePath
+        data={props.dataSeries.data}
+        x={(d) => xScale(x(d))}
+        y={(d) => yScale(y(d))}
+        stroke={props.gradient.fromColor}
+      />
+    </svg>
   );
 };
 
@@ -36,7 +55,6 @@ SparkLine.defaultProps = {
 };
 
 SparkLine.propTypes = {
-  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
   /**
    * Sets the data series. A data series consists of a label and an array of data. Each data item contains a timestamp and a value.
    */
@@ -44,6 +62,12 @@ SparkLine.propTypes = {
     data: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
     label: PropTypes.string.isRequired,
   }),
+  gradient: PropTypes.shape({
+    fromColor: PropTypes.string,
+    fromOpacity: PropTypes.number,
+    toColor: PropTypes.string,
+    toOpacity: PropTypes.number,
+  }).isRequired,
   /**
    * Sets the height of the graph canvas.
    * Type: number (required)
