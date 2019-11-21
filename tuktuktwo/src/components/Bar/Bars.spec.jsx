4d10b24f9ed8f0f6c4c3555ce7b3b7d9387e3413
sladomic <michael.sladoje@outlook.com>
2019-11-21 18:20:54 +0100

    Fix tests

--- tuktuktwo/src/components/Bar/Bars.spec.jsx
+++ tuktuktwo/src/components/Bar/Bars.spec.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Bars from './Bars';
 import chartConstants from '../../common/chartConstants';
+import ScaleUtils from '../../common/ScaleUtils';
 
 const parentSize = { width: 500, height: 404 };
+const labels = [1, 2];
 const values = [12, 37];
 const maxValue = 100;
 const barWeight = 5;
+
+const labelScaleVertical = ScaleUtils.scaleOrdinal(0, parentSize.height - chartConstants.marginBottom, labels);
+const labelScaleHorizontal = ScaleUtils.scaleOrdinal(0, parentSize.width - chartConstants.marginLeftCategorical, labels);
 
 const props = {
   barWeight: barWeight,
@@ -14,6 +19,11 @@ const props = {
   maxValue: maxValue,
   values: values,
   width: parentSize.width,
+  labelScale: labelScaleVertical,
+  labels: labels,
+  direction: 'vertical',
+  left: chartConstants.marginLeftNumerical,
+  labelOffset: labelScaleVertical.bandwidth() / 2,
 };
 
 const svgWrapper = (bars) => (
@@ -28,7 +38,7 @@ describe('<Bars />', () => {
   });
 
   it('renders its content vertically with fixed barWeight', () => {
-    const rendered = mount(svgWrapper(<Bars {...props} direction="vertical" categorical />));
+    const rendered = mount(svgWrapper(<Bars {...props} />));
     const bars = rendered.find('.vx-bar');
     bars.forEach((bar, index) => {
       expect(bar.prop('height')).toBeCloseTo((values[index] / maxValue) * (props.height - chartConstants.marginBottom - chartConstants.marginMaxValueToBorder));
@@ -38,7 +48,13 @@ describe('<Bars />', () => {
   });
 
   it('renders its content horizontally with fixed barWeight', () => {
-    const rendered = mount(svgWrapper(<Bars {...props} direction="horizontal" categorical />));
+    const rendered = mount(svgWrapper(<Bars
+      {...props}
+      direction="horizontal"
+      left={chartConstants.marginLeftCategorical}
+      labelScale={labelScaleHorizontal}
+      labelOffset={labelScaleHorizontal.bandwidth() / 2}
+    />));
     const bars = rendered.find('.vx-bar');
     bars.forEach((bar, index) => {
       expect(bar.prop('height')).toBeCloseTo(barWeight);
@@ -47,24 +63,14 @@ describe('<Bars />', () => {
     });
   });
 
-  it('renders its content vertically with dynamic barWeight', () => {
-    const rendered = mount(svgWrapper(<Bars {...props} direction="vertical" />));
-    const bars = rendered.find('.vx-bar');
-    bars.forEach((bar, index) => {
-      expect(bar.prop('height')).toBeCloseTo((values[index] / maxValue) * (props.height - chartConstants.marginBottom - chartConstants.marginMaxValueToBorder));
-      expect(bar.prop('width')).toBeCloseTo((parentSize.width - chartConstants.marginLeftNumerical - chartConstants.barSpacing * (values.length - 1)) / values.length, 1);
-      expect(bar.prop('fill')).toBe('blue');
-    });
-  });
-
-  it('renders its content horizontally with dynamic barWeight', () => {
-    const rendered = mount(svgWrapper(<Bars {...props} direction="horizontal" />));
-    const bars = rendered.find('.vx-bar');
-    bars.forEach((bar, index) => {
-      expect(bar.prop('height')).toBeCloseTo((parentSize.height - chartConstants.marginBottom - chartConstants.barSpacing * (values.length - 1)) / values.length, 1);
-      expect(bar.prop('width')).toBeCloseTo((values[index] / maxValue) * (parentSize.width - chartConstants.marginLeftCategorical - chartConstants.marginMaxValueToBorder));
-      expect(bar.prop('fill')).toBe('blue');
-    });
-  });
+  // it('does not render bar if label is outside of label axis', () => {
+  //   const rendered = mount(svgWrapper(<Bars {...props} labels={[-1, 1]} />));
+  //   const bars = rendered.find('.vx-bar');
+  //   bars.forEach((bar, index) => {
+  //     expect(bar.prop('height')).toBeCloseTo((values[index] / maxValue) * (props.height - chartConstants.marginBottom - chartConstants.marginMaxValueToBorder));
+  //     expect(bar.prop('width')).toBeCloseTo(barWeight);
+  //     expect(bar.prop('fill')).toBe('blue');
+  //   });
+  // });
 
 });
