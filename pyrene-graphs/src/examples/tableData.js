2bf23c44bf739b13cd31b98446e507950983052d
sladomic <michael.sladoje@outlook.com>
2019-11-28 14:50:42 +0100

    Add secondaryValue to secondaryLabel example

--- pyrene-graphs/src/examples/tableData.js
+++ pyrene-graphs/src/examples/tableData.js
@@ -365,46 +365,13 @@ export const tableColumnsUpDown = {
   },
 };
 
-export const tableDataSecLabel = [
-  {
-    location: 'Altdorf',
-    LQI: 24,
-    up: 100.0,
-  },
-  {
-    location: 'Zürich',
-    LQI: 44,
-    up: 14.0,
-  },
-  {
-    location: 'Lit',
-    LQI: 76,
-    up: 100.0,
-  },
-  {
-    location: 'Chorugh',
-    LQI: 120,
-    up: 100.0,
-  },
-  {
-    location: 'Los Angeles',
-    LQI: 0.7,
-    up: 100.0,
-  },
-];
+const CCI = [81, 64, 46, 78, 96, 11, 39, 42, 15, 8];
+export const tableDataSecLabel = CCI.map((d, i) => ({ CCI: d, ...tableData[i] }));
 
 export const tableColumnsSecLabel = {
-  label: {
-    accessor: (d) => d.location,
-    title: 'Location',
-  },
   secondaryLabel: {
-    accessor: (d) => d.LQI,
-    title: 'LQI',
+    accessor: (d) => d.CCI,
+    title: 'CCI',
   },
-  primaryValue: {
-    accessor: (d) => d.up,
-    title: 'Volume Up',
-    formatter: (d) => `${d.toFixed(1)} %`,
-  },
+  ...tableColumns,
 };
