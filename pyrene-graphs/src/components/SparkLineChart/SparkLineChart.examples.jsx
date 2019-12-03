c3e203bd7fbd9000e4576b30da68ec9b986cfa83
sladomic <michael.sladoje@outlook.com>
2019-12-03 16:38:04 +0100

    Change bigNumber to keyFigure

--- pyrene-graphs/src/components/SparkLineChart/SparkLineChart.examples.jsx
+++ pyrene-graphs/src/components/SparkLineChart/SparkLineChart.examples.jsx
@@ -1,26 +1,15 @@
 import moment from 'moment-timezone';
-import { format } from 'd3-format';
 import timeSeriesData from '../../examples/timeSeriesData';
 
 const examples = {};
 
-const yUnit = 'B';
 const timezone = 'Asia/Shanghai';
 const initialFrom = moment.tz('2019-10-01 00:00', timezone).valueOf();
 const initialTo = moment.tz('2019-10-08 00:00', timezone).valueOf();
 const dataSeries = timeSeriesData.genDownloadedVolumes(initialFrom, initialTo, 42).data;
 
-const dataFormat = (num) => {
-  const formattedNum = `${format('~s')(num)}`;
-  if (num > 0.001 && num < 1000) {
-    return `${parseFloat(formattedNum).toFixed(2)} ${yUnit}`;
-  }
-  return `${parseFloat(formattedNum.substring(0, formattedNum.length - 2)).toFixed(2)} ${formattedNum.substring(formattedNum.length - 1, formattedNum.length)}${yUnit}`;
-};
-
 examples.props = {
-  bigNumber: dataSeries.map((d) => d[1]).reduce((a, b) => a + b),
-  dataFormat: (d) => dataFormat(d * 100000000),
+  keyFigure: `${(dataSeries.map((d) => d[1]).reduce((a, b) => a + b) / 10000).toFixed(2)} TB`,
   dataSeries: dataSeries,
   axisLabel: 'DOWNLOADED VOLUME LAST 7 DAYS',
   timezone: timezone,
