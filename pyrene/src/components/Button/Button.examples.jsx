caa0e4711b472f67b430af890b7372e25edd9746
alo <alo@open.ch>
2020-01-28 09:55:34 +0100

    capitalized example labels, remove redundant font definition

--- pyrene/src/components/Button/Button.examples.jsx
+++ pyrene/src/components/Button/Button.examples.jsx
@@ -8,27 +8,26 @@ examples.props = {
 };
 
 examples.examples = [{
-  props: { label: 'primary' },
+  props: { label: 'Primary' },
   description: 'For all principle actions on a page. Used to highlight the most important actions. Avoid overwhelming usage of primary buttons.',
 }, {
-  props: { label: 'secondary', type: 'secondary' },
+  props: { label: 'Secondary', type: 'secondary' },
   description: 'For secondary actions such as ‘Discard’ in combination with a primary button.',
 }, {
   props: { label: 'Ghost', type: 'ghost' },
   description: 'Ghost button description',
 }, {
-  props: { label: 'danger', type: 'danger', icon: 'errorOutline' },
+  props: { label: 'Danger', type: 'danger', icon: 'errorOutline' },
   description: 'When an action has harmful intentions to the users data (delete, remove, etc). To draw more attention on what the button does add and icon. Icons are always paired with a label.',
 }, {
   props: { label: 'Delete', type: 'action', icon: 'errorOutline' },
   description: 'Used for table actions. They are paired with icon and label.',
-},
-{
-  props: { label: 'Label', type: 'admin', icon: 'trash' },
-  description: 'admin ;)',
 }, {
-  props: { label: 'admin', type: 'admin' },
+  props: { label: 'Admin', type: 'admin', icon: 'trash' },
   description: 'Used when an action is accessible for MC Engineers only.',
+}, {
+  props: { label: 'disabled', type: 'admin', disabled: true },
+  description: 'Capitalization is not done automatically!',
 }];
 
 examples.category = 'Interaction';
