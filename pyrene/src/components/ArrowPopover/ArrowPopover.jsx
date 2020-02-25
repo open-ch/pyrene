1feeefe2f914e95f9f7116fd8e618ec5fc4c5226
jvi <jvi@open.ch>
2020-02-25 07:03:28 +0100

    move arrow with item

--- pyrene/src/components/ArrowPopover/ArrowPopover.jsx
+++ pyrene/src/components/ArrowPopover/ArrowPopover.jsx
@@ -36,19 +36,60 @@ class ArrowPopover extends React.Component {
         <Popover
           align="center"
           distanceToTarget={20}
+          preferredPosition={['top', 'left']}
           onClickOutside={this.closePopover}
-          preferredPosition={['top']}
-          renderPopoverContent={() => (
-            <div styleName={classNames('popover')}>
-              <div>{this.props.popoverContent}</div>
-              <div styleName={classNames('triangle')} />
-            </div>
-          )}
+          renderPopoverContent={(position, nudgedLeft, nudgedTop, targetRect, popoverRect) => {
+            // Square
+            const lengthSide = 20;
+            const arrowWidth = (lengthSide * Math.sqrt(2)) / 2;
+
+            let top = (targetRect.top - popoverRect.top) + (targetRect.height / 2) - (arrowWidth / 2);
+            let left = (targetRect.left - popoverRect.left) + (targetRect.width / 2) - (arrowWidth / 2);
+
+            let popoverRectHeight = popoverRect.height - arrowWidth;
+            let popoverRectWidth = popoverRect.width - arrowWidth;
+
+            switch (position) {
+              case 'top':
+                popoverRectWidth -= arrowWidth;
+                break;
+              case 'bottom':
+                popoverRectWidth += arrowWidth;
+                break;
+              case 'left':
+                popoverRectHeight -= arrowWidth;
+                break;
+              case 'right':
+                popoverRectHeight -= arrowWidth;
+                break;
+              default:
+                break;
+            }
+
+            left = left < 0 ? 0 : left;
+            left = left + arrowWidth > popoverRectWidth ? popoverRectWidth : left;
+            top = top < 0 ? 0 : top;
+            top = top + arrowWidth > popoverRectHeight ? popoverRectHeight : top;
+
+            return (
+              <div styleName={classNames('popover')}>
+                <div>{this.props.popoverContent}</div>
+                <div
+                  styleName={classNames('triangle')}
+                  style={{
+                    left,
+                    top,
+                    width: lengthSide,
+                    height: lengthSide,
+                  }}
+                />
+              </div>
+            );
+          }}
           displayPopover={this.state.displayPopover}
           autoReposition
         >
           <div>
-            {console.log(this.props.children)}
             {React.cloneElement(
               this.props.children,
               { onClick: this.openPopover },
