fb15ddeb4c9c583bafdb33fa8ab1b3bc76d6c54b
jvi <jvi@open.ch>
2020-02-26 12:08:28 +0100

    made functional

--- pyrene/src/components/ArrowPopover/ArrowPopover.jsx
+++ pyrene/src/components/ArrowPopover/ArrowPopover.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -9,98 +9,78 @@ import Popover from '../Popover/Popover';
 /**
  *  Popover with Arrow
  */
-class ArrowPopover extends React.Component {
+const ArrowPopover = ({ children, popoverContent }) => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayPopover: false,
-    };
-  }
+  const [displayPopover, setDisplayPopover] = useState(false);
 
-  closePopover = () => {
-    this.setState(() => ({
-      displayPopover: false,
-    }));
-  };
+  return (
+    <div styleName={classNames('arrowPopover')}>
+      <Popover
+        align="center"
+        distanceToTarget={20}
+        preferredPosition={['top', 'left']}
+        onClickOutside={() => setDisplayPopover(false)}
+        renderPopoverContent={(position, nudgedLeft, nudgedTop, targetRect, popoverRect) => {
+          // Square
+          const lengthSide = 20;
+          const arrowWidth = (lengthSide * Math.sqrt(2)) / 2;
 
-  openPopover = () => {
-    this.setState(() => ({
-      displayPopover: true,
-    }));
-  };
+          let top = (targetRect.top - popoverRect.top) + (targetRect.height / 2) - (arrowWidth / 2);
+          let left = (targetRect.left - popoverRect.left) + (targetRect.width / 2) - (arrowWidth / 2);
 
-  render() {
-    return (
-      <div styleName={classNames('arrowPopover')}>
-        <Popover
-          align="center"
-          distanceToTarget={20}
-          preferredPosition={['top', 'left']}
-          onClickOutside={this.closePopover}
-          renderPopoverContent={(position, nudgedLeft, nudgedTop, targetRect, popoverRect) => {
-            // Square
-            const lengthSide = 20;
-            const arrowWidth = (lengthSide * Math.sqrt(2)) / 2;
+          let popoverRectHeight = popoverRect.height - arrowWidth;
+          let popoverRectWidth = popoverRect.width - arrowWidth;
 
-            let top = (targetRect.top - popoverRect.top) + (targetRect.height / 2) - (arrowWidth / 2);
-            let left = (targetRect.left - popoverRect.left) + (targetRect.width / 2) - (arrowWidth / 2);
+          switch (position) {
+            case 'top':
+              popoverRectWidth -= arrowWidth;
+              break;
+            case 'bottom':
+              popoverRectWidth += arrowWidth;
+              break;
+            case 'left':
+              popoverRectHeight -= arrowWidth;
+              break;
+            case 'right':
+              popoverRectHeight -= arrowWidth;
+              break;
+            default:
+              break;
+          }
 
-            let popoverRectHeight = popoverRect.height - arrowWidth;
-            let popoverRectWidth = popoverRect.width - arrowWidth;
+          left = left < 0 ? 0 : left;
+          left = left + arrowWidth > popoverRectWidth ? popoverRectWidth : left;
+          top = top < 0 ? 0 : top;
+          top = top + arrowWidth > popoverRectHeight ? popoverRectHeight : top;
 
-            switch (position) {
-              case 'top':
-                popoverRectWidth -= arrowWidth;
-                break;
-              case 'bottom':
-                popoverRectWidth += arrowWidth;
-                break;
-              case 'left':
-                popoverRectHeight -= arrowWidth;
-                break;
-              case 'right':
-                popoverRectHeight -= arrowWidth;
-                break;
-              default:
-                break;
-            }
-
-            left = left < 0 ? 0 : left;
-            left = left + arrowWidth > popoverRectWidth ? popoverRectWidth : left;
-            top = top < 0 ? 0 : top;
-            top = top + arrowWidth > popoverRectHeight ? popoverRectHeight : top;
-
-            return (
-              <div styleName={classNames('popover')}>
-                <div>{this.props.popoverContent}</div>
-                <div
-                  styleName={classNames('triangle')}
-                  style={{
-                    left,
-                    top,
-                    width: lengthSide,
-                    height: lengthSide,
-                  }}
-                />
-              </div>
-            );
-          }}
-          displayPopover={this.state.displayPopover}
-          autoReposition
-        >
-          <div>
-            {React.cloneElement(
-              this.props.children,
-              { onClick: this.openPopover },
-            )}
-          </div>
-        </Popover>
-      </div>
-    );
-  }
-
-}
+          return (
+            <div styleName={classNames('popover')}>
+              <div>{popoverContent}</div>
+              <div
+                styleName={classNames('triangle')}
+                style={{
+                  left,
+                  top,
+                  width: lengthSide,
+                  height: lengthSide,
+                }}
+              />
+            </div>
+          );
+        }}
+        displayPopover={displayPopover}
+        autoReposition
+      >
+        <div>
+          {React.cloneElement(
+            children,
+            { onClick: () => setDisplayPopover(true) },
+          )}
+        </div>
+      </Popover>
+    </div>
+  );
+};
 
 ArrowPopover.displayName = 'Arrow Popover';
 
