72c2cf676c811a863b50131db7ab4fb137758267
lga <lga@open.ch>
2020-02-27 14:34:01 +0100

    Merge pull request #279 pyrene from feature/ to develop
    * commit '3d000952c6d72c22198ceb3071954e83174f0e20 [formerly 4c09b2515ce6b8d53efe6526a3a67ae3533eedc0]':
      Solved major sonarqube issue
      Removed unused negated parameter in method
      Passing negated values as part of the filter properties
      Made the api consistent, just the query parameters are strings
      Added support for negated filters for tags and popup
      Added support for list of negated filter keys in the filter tags (no popup yet)
    
    
    Former-commit-id: b28d57b8185408d61474f3ac569542ed72ef3c0f

--- pyrene/src/components/Filter/FilterComponents/FilterTag.jsx
+++ pyrene/src/components/Filter/FilterComponents/FilterTag.jsx
@@ -3,19 +3,22 @@ import PropTypes from 'prop-types';
 
 import './FilterTag.css';
 
-const FilterTag = (props) => (
-  <div styleName="wrapper" title={props.filterText}>
-    <div styleName="label">
-      {props.filterLabel}
+const FilterTag = (props) => {
+  const displayText = props.negated ? `!${props.filterText}` : props.filterText;
+  return (
+    <div styleName="wrapper" title={displayText}>
+      <div styleName="label">
+        {props.filterLabel}
+      </div>
+      <div styleName="text">
+        {displayText}
+      </div>
+      <div onClick={() => props.onClose()}>
+        <div styleName="clearIcon" className="pyreneIcon-delete" />
+      </div>
     </div>
-    <div styleName="text">
-      {props.filterText}
-    </div>
-    <div onClick={() => props.onClose()}>
-      <div styleName="clearIcon" className="pyreneIcon-delete" />
-    </div>
-  </div>
-);
+  );
+};
 
 
 FilterTag.displayName = 'FilterTag';
@@ -23,6 +26,7 @@ FilterTag.displayName = 'FilterTag';
 FilterTag.propTypes = {
   filterLabel: PropTypes.string.isRequired,
   filterText: PropTypes.string.isRequired,
+  negated: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
 };
 
