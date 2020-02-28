2fbb8036f729a8692a79af5022b15fce80ea27b2
alo <alo@open.ch>
2020-02-28 16:01:23 +0100

    tests added for ActionBar

--- pyrene/src/components/ActionBar/ActionBar.spec.jsx
+++ pyrene/src/components/ActionBar/ActionBar.spec.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import ActionBar from './ActionBar';
+import Icon from '../Icon/Icon';
+import ArrowPopover from '../ArrowPopover/ArrowPopover';
+import Tooltip from '../Tooltip/Tooltip';
 
 const props = {
   styling: 'shadow',
@@ -42,11 +45,201 @@ describe('<ActionBar />', () => {
     expect(rendered.find('Loader')).toHaveLength(1);
   });
 
+  describe('disabled container: ', () => {
+    it('renders disabled container when disabled but not loading', () => {
+      const rendered = shallow(<ActionBar {...props} loading={false} disabled />);
+
+      // find the wrapper -> container; this container should be disabled
+      expect(rendered.find('.container').props().className).toContain('disabled');
+
+    });
+
+    it('doesn\'t render disabled container when disabled but it is loading', () => {
+      const rendered = shallow(<ActionBar {...props} loading disabled />);
+
+      // find the wrapper -> container; this container should be disabled
+      expect(rendered.find('.container').props().className).not.toContain('disabled');
+
+    });
+  });
+
   it('renders no box', () => {
     props.styling = 'none';
     const rendered = mount(<ActionBar {...props} />);
     expect(rendered.find('.box-shadow')).toHaveLength(0);
     expect(rendered.find('.box-box')).toHaveLength(0);
+  });
+
+  describe('<Icon />', () => {
+    it('doesn\' render svg if there is none, but renders an iconName', () => {
+      const actionWithEmptytSvg = [
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: true,
+          onClick: () => {},
+        },
+      ];
+
+      const rendered = shallow(<ActionBar actions={actionWithEmptytSvg} />);
+      const icon = rendered.find(Icon).props();
+
+      expect(icon.svg).toBeFalsy();
+      expect(icon.name).toEqual(actionWithEmptytSvg[0].iconName);
+
+    });
+
+    it('doesn\' render svg if lenght is 0, but renders an iconName', () => {
+      const actionWithoutSvg = [
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: true,
+          svg: '',
+          onClick: () => {},
+        },
+      ];
+
+      const rendered = shallow(<ActionBar actions={actionWithoutSvg} />);
+      const icon = rendered.find(Icon).props();
+
+      expect(icon.svg).toBeFalsy();
+      expect(icon.name).toEqual(actionWithoutSvg[0].iconName);
+
+    });
+
+    it('renders svg if defined instead of an iconName', () => {
+      const actionWithSvg = [
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: true,
+          svg: 'pathToSvg',
+          onClick: () => {},
+        },
+      ];
+
+      const rendered = shallow(<ActionBar actions={actionWithSvg} />);
+      const icon = rendered.find(Icon).props();
+
+      expect(icon.name).toBeFalsy();
+      expect(icon.svg).toEqual(actionWithSvg[0].svg);
+
+    });
+  });
+
+  describe('<ArrowPopover />', () => {
+
+    const FakePopover = () => (
+      <div />
+    );
+
+    it('renders no arrow popover if action doesn\'t contain one', () => {
+
+      const actionWithoutPopover = [
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: true,
+          svg: 'pathToSvg',
+          onClick: () => {},
+        },
+      ];
+
+      const rendered = shallow(<ActionBar actions={actionWithoutPopover} />);
+
+      expect(rendered.find(ArrowPopover)).toHaveLength(0);
+
+    });
+
+    it('renders no arrow popover if action does contain one but its not active', () => {
+
+      const inactiveAction = [
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: false,
+          svg: 'pathToSvg',
+          onClick: () => {},
+          popover: <FakePopover />,
+        },
+      ];
+
+      const rendered = shallow(<ActionBar actions={inactiveAction} />);
+
+      expect(rendered.find(ArrowPopover)).toHaveLength(0);
+
+    });
+
+    it('renders arrow popover if action contains one', () => {
+
+      const actionWithPopover = [
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: true,
+          svg: 'pathToSvg',
+          onClick: () => {},
+          popover: <FakePopover />,
+        },
+      ];
+
+      const rendered = shallow(<ActionBar actions={actionWithPopover} />);
+
+      expect(rendered.find(ArrowPopover)).toHaveLength(1);
+      expect(rendered.find(ArrowPopover).props().popoverContent).toEqual(<FakePopover />);
+
+    });
+
+  });
+
+  describe('<Tooltip />', () => {
+
+    it('doesn\'t render tooltip if there is none', () => {
+
+      const actionsWithoutTooltips = [
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: true,
+          onClick: () => {},
+        },
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: true,
+          onClick: () => {},
+        },
+      ];
+
+      const rendered = shallow(<ActionBar actions={actionsWithoutTooltips} />);
+      expect(rendered.find(Tooltip)).toHaveLength(0);
+
+    });
+
+    it('renders one tooltip if one action has one', () => {
+
+      const actionsWithoutTooltips = [
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: true,
+          onClick: () => {},
+        },
+        {
+          iconName: 'chevronLeft',
+          color: 'neutral300',
+          active: true,
+          onClick: () => {},
+          tooltip: 'Im am tooltip!',
+        },
+      ];
+
+      const rendered = shallow(<ActionBar actions={actionsWithoutTooltips} />);
+      expect(rendered.find(Tooltip)).toHaveLength(1);
+
+    });
+
   });
 
   it('can be interacted with correctly', () => {
