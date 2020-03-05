db33963409f50689502ab0f787833ab2d2508192
jvi <jvi@open.ch>
2020-03-05 10:54:18 +0100

    moved arrowPosition out so can be tested

--- pyrene/src/components/ArrowPopover/ArrowPopover.jsx
+++ pyrene/src/components/ArrowPopover/ArrowPopover.jsx
@@ -6,83 +6,79 @@ import './arrowPopover.css';
 import Popover from '../Popover/Popover';
 
 
+export const arrowPosition = (position, targetRect, popoverRect) => {
+  // Square
+  const lengthSide = 20;
+  const arrowWidth = (lengthSide * Math.sqrt(2)) / 2;
+
+  let top = (targetRect.top - popoverRect.top) + (targetRect.height / 2) - (arrowWidth / 2);
+  let left = (targetRect.left - popoverRect.left) + (targetRect.width / 2) - (arrowWidth / 2);
+
+  let popoverRectHeight = popoverRect.height - arrowWidth;
+  let popoverRectWidth = popoverRect.width - arrowWidth;
+
+  switch (position) {
+    case 'top':
+      popoverRectWidth -= arrowWidth;
+      break;
+    case 'bottom':
+      popoverRectWidth += arrowWidth;
+      break;
+    case 'left':
+      popoverRectHeight -= arrowWidth;
+      break;
+    case 'right':
+      popoverRectHeight -= arrowWidth;
+      break;
+    default:
+      break;
+  }
+
+  left = left < 0 ? 0 : left;
+  left = left + arrowWidth > popoverRectWidth ? popoverRectWidth : left;
+  top = top < 0 ? 0 : top;
+  top = top + arrowWidth > popoverRectHeight ? popoverRectHeight : top;
+
+  return { top, left, lengthSide };
+};
+
 /**
  *  Popover with Arrow
  */
-const ArrowPopover = ({ children, popoverContent, displayPopover }) => {
+const ArrowPopover = ({ children, popoverContent, displayPopover }) => (
+  <div styleName={classNames('arrowPopover')}>
+    <Popover
+      align="center"
+      distanceToTarget={20}
+      preferredPosition={['top', 'left']}
+      renderPopoverContent={(position, nudgedLeft, nudgedTop, targetRect, popoverRect) => {
 
-  const arrowPosition = (position, targetRect, popoverRect) => {
-    // Square
-    const lengthSide = 20;
-    const arrowWidth = (lengthSide * Math.sqrt(2)) / 2;
+        const { top, left, lengthSide } = arrowPosition(position, targetRect, popoverRect);
 
-    let top = (targetRect.top - popoverRect.top) + (targetRect.height / 2) - (arrowWidth / 2);
-    let left = (targetRect.left - popoverRect.left) + (targetRect.width / 2) - (arrowWidth / 2);
-
-    let popoverRectHeight = popoverRect.height - arrowWidth;
-    let popoverRectWidth = popoverRect.width - arrowWidth;
-
-    switch (position) {
-      case 'top':
-        popoverRectWidth -= arrowWidth;
-        break;
-      case 'bottom':
-        popoverRectWidth += arrowWidth;
-        break;
-      case 'left':
-        popoverRectHeight -= arrowWidth;
-        break;
-      case 'right':
-        popoverRectHeight -= arrowWidth;
-        break;
-      default:
-        break;
-    }
-
-    left = left < 0 ? 0 : left;
-    left = left + arrowWidth > popoverRectWidth ? popoverRectWidth : left;
-    top = top < 0 ? 0 : top;
-    top = top + arrowWidth > popoverRectHeight ? popoverRectHeight : top;
-
-    return { top, left, lengthSide };
-  };
-
-  return (
-    <div styleName={classNames('arrowPopover')}>
-      <Popover
-        align="center"
-        distanceToTarget={20}
-        preferredPosition={['top', 'left']}
-        renderPopoverContent={(position, nudgedLeft, nudgedTop, targetRect, popoverRect) => {
-
-
-          const { top, left, lengthSide } = arrowPosition(position, targetRect, popoverRect);
-
-          return (
-            <div styleName={classNames('popover')}>
-              <div>{popoverContent}</div>
-              <div
-                styleName={classNames('triangle')}
-                style={{
-                  left,
-                  top,
-                  width: lengthSide,
-                  height: lengthSide,
-                }}
-              />
-            </div>
-          );
-        }}
-        displayPopover={displayPopover}
-        autoReposition
-      >
-        <div>
-          {children}
-        </div>
-      </Popover>
-    </div>
-  );
-};
+        return (
+          <div styleName={classNames('popover')}>
+            <div>{popoverContent}</div>
+            <div
+              styleName={classNames('triangle')}
+              style={{
+                left,
+                top,
+                width: lengthSide,
+                height: lengthSide,
+              }}
+            />
+          </div>
+        );
+      }}
+      displayPopover={displayPopover}
+      autoReposition
+    >
+      <div>
+        {children}
+      </div>
+    </Popover>
+  </div>
+);
 
 ArrowPopover.displayName = 'Arrow Popover';
 
