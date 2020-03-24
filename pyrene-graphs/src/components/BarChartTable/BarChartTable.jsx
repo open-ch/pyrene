618c85022270176c53135993523fa10c18815313
xuz <xuz@open.ch>
2020-03-24 14:10:33 +0100

    fixed barchart table container height

--- pyrene-graphs/src/components/BarChartTable/BarChartTable.jsx
+++ pyrene-graphs/src/components/BarChartTable/BarChartTable.jsx
@@ -21,92 +21,102 @@ export default class BarChartTable extends React.Component {
     };
   }
 
-   togglePopover = () => {
-     this.setState((prevState) => ({
-       showPopover: !prevState.showPopover,
-     }));
-   };
+  getDisplayRows = (dataLength) => {
+    if (this.props.displayedRows < 0) {
+      return dataLength;
+    }
+    if (dataLength < this.props.displayedRows) {
+      return dataLength;
+    }
+    return this.props.displayedRows;
+  };
 
-   render() {
-     const dataAvailable = this.props.data && this.props.data.length > 0;
+  togglePopover = () => {
+    this.setState((prevState) => ({
+      showPopover: !prevState.showPopover,
+    }));
+  };
 
-     const colors = (this.props.type === 'comparison' ? this.props.colorScheme.comparison : this.props.colorScheme.valueGround);
-     const description = this.props.type === 'bar' ? '' : this.props.description;
-     const sortedData = dataAvailable && this.props.data.sort((a, b) => {
-       const sortPrimaryValue = getValueWithAccessor(b, this.props.columns.primaryValue.accessor) - getValueWithAccessor(a, this.props.columns.primaryValue.accessor);
-       return this.props.columns.secondaryValue ? sortPrimaryValue || (getValueWithAccessor(b, this.props.columns.secondaryValue.accessor) - getValueWithAccessor(a, this.props.columns.secondaryValue.accessor)) : sortPrimaryValue;
-     });
-     const displayedRows = dataAvailable && this.props.displayedRows < 0 ? this.props.data.length : this.props.displayedRows;
-     const popOverAdditionalRows = 5;
-     const rowHeight = 32;
-     return (
-       <div styleName="container">
-         <Header
-           title={this.props.title}
-           description={description}
-           legend={getLegend(this.props.type, this.props.columns, this.props.colorScheme)}
-           colors={colors}
-         />
-         {/* table height: displayedRows + 1 header row + conditional showMoreLink div */}
-         <div style={{ height: dataAvailable ? `${displayedRows * rowHeight + rowHeight + (this.props.data.length > displayedRows && this.props.loading ? 26 : 0)}px` : '90px' }}>
-           <Responsive>
-             {(parent) => (
-               <SimpleTable
-                 actions={this.props.actions}
-                 columns={getColumns({
-                   dataAvailable: dataAvailable,
-                   props: this.props,
-                   colors: colors,
-                   width: parent.width,
-                 })}
-                 data={dataAvailable ? sortedData.slice(0, displayedRows) : []}
-                 onRowDoubleClick={this.props.onRowDoubleClick}
-                 loading={this.props.loading}
-               />
-             )}
-           </Responsive>
-         </div>
-         {dataAvailable && (this.props.data.length > displayedRows) && !this.props.loading && (
-           <div styleName="showMoreLink" onClick={this.togglePopover}>
-             {`Show more (${sortedData.length})`}
-             {this.state.showPopover && (
-               <Popover
-                 align="center"
-                 children={<div styleName="popOverPlaceholder"></div>} // eslint-disable-line
-                 distanceToTarget={-((popOverAdditionalRows - 2) * rowHeight) - 1.5} // to center the popover vertically, so that 3 rows of the popover table are under and 2 rows over the bar chart table, - 1.5 to align borders
-                 renderPopoverContent={() => (
-                   <div styleName="popOverContainer" style={{ height: `${(displayedRows + popOverAdditionalRows) * rowHeight + rowHeight + rowHeight}px` }}>
-                     {/* popover height: (displayedRows + 5 more rows) * 32px + 32px table header + 32px popover header */}
-                     <div styleName="popOverHeader">
-                       {this.props.title}
-                     </div>
-                     <div styleName="popOverBody" style={{ height: `${(displayedRows + 5) * 32 + 32}px` }}>
-                       <div styleName="tableContainer">
-                         <SimpleTable
-                           actions={this.props.actions}
-                           columns={getColumns({ props: this.props, colors: colors })}
-                           data={sortedData}
-                           onRowDoubleClick={this.props.onRowDoubleClick}
-                         />
-                       </div>
-                       {this.props.popoverFooter && (
-                         <div styleName="footerContainer">
-                           {this.props.popoverFooter}
-                         </div>
-                       )}
-                     </div>
-                   </div>
-                 )}
-                 displayPopover={this.state.showPopover}
-                 onClickOutside={this.togglePopover}
-               />
-             )}
-           </div>
-         )}
+  render() {
+    const dataAvailable = this.props.data && this.props.data.length > 0;
 
-       </div>
-     );
-   }
+    const colors = (this.props.type === 'comparison' ? this.props.colorScheme.comparison : this.props.colorScheme.valueGround);
+    const description = this.props.type === 'bar' ? '' : this.props.description;
+    const sortedData = dataAvailable && this.props.data.sort((a, b) => {
+      const sortPrimaryValue = getValueWithAccessor(b, this.props.columns.primaryValue.accessor) - getValueWithAccessor(a, this.props.columns.primaryValue.accessor);
+      return this.props.columns.secondaryValue ? sortPrimaryValue || (getValueWithAccessor(b, this.props.columns.secondaryValue.accessor) - getValueWithAccessor(a, this.props.columns.secondaryValue.accessor)) : sortPrimaryValue;
+    });
+    const displayedRows = dataAvailable ? this.getDisplayRows(this.props.data.length) : 0;
+    const popOverAdditionalRows = 5;
+    const rowHeight = 32;
+    return (
+      <div styleName="container">
+        <Header
+          title={this.props.title}
+          description={description}
+          legend={getLegend(this.props.type, this.props.columns, this.props.colorScheme)}
+          colors={colors}
+        />
+        {/* table height: displayedRows + 1 header row + conditional showMoreLink div */}
+        <div style={{ height: dataAvailable ? `${displayedRows * rowHeight + rowHeight + (this.props.data.length > displayedRows && this.props.loading ? 26 : 0)}px` : '90px' }}>
+          <Responsive>
+            {(parent) => (
+              <SimpleTable
+                actions={this.props.actions}
+                columns={getColumns({
+                  dataAvailable: dataAvailable,
+                  props: this.props,
+                  colors: colors,
+                  width: parent.width,
+                })}
+                data={dataAvailable ? sortedData.slice(0, displayedRows) : []}
+                onRowDoubleClick={this.props.onRowDoubleClick}
+                loading={this.props.loading}
+              />
+            )}
+          </Responsive>
+        </div>
+        {dataAvailable && (this.props.data.length > displayedRows) && !this.props.loading && (
+          <div styleName="showMoreLink" onClick={this.togglePopover}>
+            {`Show more (${sortedData.length})`}
+            {this.state.showPopover && (
+              <Popover
+                align="center"
+                children={<div styleName="popOverPlaceholder"></div>} // eslint-disable-line
+                distanceToTarget={-((popOverAdditionalRows - 2) * rowHeight) - 1.5} // to center the popover vertically, so that 3 rows of the popover table are under and 2 rows over the bar chart table, - 1.5 to align borders
+                renderPopoverContent={() => (
+                  <div styleName="popOverContainer" style={{ height: `${(displayedRows + popOverAdditionalRows) * rowHeight + rowHeight + rowHeight}px` }}>
+                    {/* popover height: (displayedRows + 5 more rows) * 32px + 32px table header + 32px popover header */}
+                    <div styleName="popOverHeader">
+                      {this.props.title}
+                    </div>
+                    <div styleName="popOverBody" style={{ height: `${(displayedRows + 5) * 32 + 32}px` }}>
+                      <div styleName="tableContainer">
+                        <SimpleTable
+                          actions={this.props.actions}
+                          columns={getColumns({ props: this.props, colors: colors })}
+                          data={sortedData}
+                          onRowDoubleClick={this.props.onRowDoubleClick}
+                        />
+                      </div>
+                      {this.props.popoverFooter && (
+                        <div styleName="footerContainer">
+                          {this.props.popoverFooter}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                )}
+                displayPopover={this.state.showPopover}
+                onClickOutside={this.togglePopover}
+              />
+            )}
+          </div>
+        )}
+
+      </div>
+    );
+  }
 
 }
 
@@ -187,12 +197,12 @@ BarChartTable.propTypes = {
    */
   description: PropTypes.string,
   /**
-  * Sets the number of displayed rows.
-  */
+   * Sets the number of displayed rows.
+   */
   displayedRows: PropTypes.number,
   /**
-  * If set, a loader is shown.
-  */
+   * If set, a loader is shown.
+   */
   loading: PropTypes.bool,
   /**
    * Called when the user double clicks on a row.
