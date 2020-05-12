aa0491dbc6120c5a63324e3e812e42fa684d2bb8
jvi <jvi@open.ch>
2020-05-12 12:42:29 +0200

    added tiny

--- pyrene/src/components/LabelAndValue/LabelAndValue.examples.jsx
+++ pyrene/src/components/LabelAndValue/LabelAndValue.examples.jsx
@@ -8,10 +8,13 @@ LabelAndValue.props = {
 
 LabelAndValue.examples = [
   {
-    props: { label: 'bandwidth', value: '461.2 kb/s', size: 'small' },
+    props: { label: 'Punch card', value: '12/80', size: 'tiny' },
+    description: 'Tiny size',
+  }, {
+    props: { label: 'Bandwidth', value: '461.2 kb/s', size: 'small' },
     description: 'Small size',
   }, {
-    props: { label: 'volume', value: '89 GB', size: 'large' },
+    props: { label: 'Volume', value: '89 GB', size: 'large' },
     description: 'Large size',
   },
 ];
