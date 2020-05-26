54a94ef362a3a8d28386e35bc5117313fa635592
otz <otz@open.ch>
2020-05-26 09:34:51 +0200

    Merge pull request #384 pyrene from feature/-calendar-date-selector to develop
    * commit '2246639ee6ac454d6e834181d6d3db59e4807e24 [formerly fe1ac5a6dbfbb3ab08fc1c4a5b711101e1f49e78]':
      remove innerwidth
      adjust tests
      reuse horizontal switch and arrow selector
      init
    
    
    Former-commit-id: 2c88fff69cc7c363cfbd507931596871d0ccff5c

---
+++ pyrene/src/components/TimeRangeSelector/PresetTimeRanges/HorizontalSwitch/HorizontalSwitch.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import './horizontalSwitch.css';
+
+const HorizontalSwitch = (props) => (
+  <div styleName="horizontalSwitch">
+    {props.values.map((value) => (
+      <button
+        id={value.id}
+        key={value.id}
+        type="button"
+        styleName={
+          classNames(
+            { disabled: props.disabled },
+            { active: props.selected === value.id },
+          )
+        }
+        disabled={props.disabled}
+        onClick={() => {
+          if (props.selected !== value.id) {
+            props.onClick(value);
+          }
+        }}
+      >
+        <span>
+          {value.label}
+        </span>
+      </button>
+    ))}
+  </div>
+);
+
+HorizontalSwitch.defaultProps = {
+  disabled: false,
+};
+
+HorizontalSwitch.propTypes = {
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  selected: PropTypes.string.isRequired,
+  values: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default HorizontalSwitch;
