6f5888f9096d23a29a665a4fd9c117effa7e5112
xuz <xuz@open.ch>
2020-06-08 09:35:46 +0200

    Revert "added processing prop"
    This reverts commit 48b9e76458a2a26486a171fa1cb43ccf14c22524 [formerly acf3436dbcb43438f633f4a30c5dbde591e3f215].
    
    
    Former-commit-id: acc5d6fbf626c57dae60cb7e356f53cd34204317

--- pyrene/src/components/Modal/Modal.jsx
+++ pyrene/src/components/Modal/Modal.jsx
@@ -76,7 +76,7 @@ export default class Modal extends React.Component {
         </div>
       </div>
       <div styleName={classNames('contentContainer', { contentScrolling: this.props.contentScrolling })}>
-        <div styleName={classNames('content', { contentPadding: this.props.contentPadding }, { contentScrolling: this.props.contentScrolling }, { overlay: this.props.processing })}>
+        <div styleName={classNames('content', { contentPadding: this.props.contentPadding }, { contentScrolling: this.props.contentScrolling })}>
           {this.props.renderCallback()}
         </div>
       </div>
@@ -91,7 +91,6 @@ export default class Modal extends React.Component {
 
   createButtonArray = (buttonInfo) => buttonInfo.map((buttonProps) => (
     <Button
-      loading={buttonProps.loading}
       icon={buttonProps.icon}
       type={buttonProps.type}
       label={buttonProps.label}
@@ -128,7 +127,6 @@ Modal.displayName = 'Modal';
 
 Modal.defaultProps = {
   loading: false,
-  processing: false,
   rightButtonBarElements: [],
   leftButtonBarElements: [],
   onPreviousArrowClick: () => null,
@@ -196,10 +194,6 @@ Modal.propTypes = {
    * Called when the user clicks on the previous button.
    */
   onPreviousArrowClick: PropTypes.func,
-  /**
-   * Sets the processing state.
-   */
-  processing: PropTypes.bool,
   /**
    * Sets the content to be rendered inside the component.
    */
