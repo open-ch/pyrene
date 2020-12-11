0b975754d4fe70d6e7bf7e368ba5ebc8cb07106f
Sanmi Awi <ola@open.ch>
2020-12-11 12:04:29 +0100

    Merge ArrowPopover and Popover. Removed unused imports and unnecessary spaces.

--- pyrene/src/components/ArrowPopover/ArrowPopover.examples.tsx
+++ pyrene/src/components/ArrowPopover/ArrowPopover.examples.tsx
@@ -36,18 +36,18 @@ ArrowPopover.examples = [
       preferredPosition: ['bottom' as PreferredPos, 'right' as PreferredPos],
       displayPopover: false,
       popoverContent:
-          <div style={{
-            width: 250, backgroundColor: '#9BB7D4', border: '1px solid #efefef', padding: '2em 1em 2em 1em', color: 'white',
-          }}
-          >
-            This is a child component
-            <div style={{ position: 'relative', display: 'inline-block' }}>
-              <div style={{ marginRight: '16px', display: 'inline-block' }}>
-                <Button onClick={() => null} label="Cancel" type="secondary" />
-              </div>
-              <Button onClick={() => null} label="Test" type="primary" />
-            </div>
-          </div>,
+  <div style={{
+    width: 250, backgroundColor: '#9BB7D4', border: '1px solid #efefef', padding: '2em 1em 2em 1em', color: 'white',
+  }}
+  >
+    This is a child component
+    <div style={{ position: 'relative', display: 'inline-block' }}>
+      <div style={{ marginRight: '16px', display: 'inline-block' }}>
+        <Button onClick={() => null} label="Cancel" type="secondary" />
+      </div>
+      <Button onClick={() => null} label="Test" type="primary" />
+    </div>
+  </div>,
       children: <Placeholder width={400} />,
     },
   },
