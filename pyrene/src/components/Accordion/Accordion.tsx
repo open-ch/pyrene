80f431851fc4158bc0222c95714a947cf0799587
mie <mie@open.ch>
2020-12-22 10:41:20 +0100

    added accordion description

--- pyrene/src/components/Accordion/Accordion.tsx
+++ pyrene/src/components/Accordion/Accordion.tsx
@@ -14,6 +14,14 @@ export interface AccordionProps {
   type?: 'regular' | 'custom'
 }
 
+/**
+ * The accordion component delivers large amounts of content in a small space through progressive disclosure. The header title gives the user a high level overview of the content allowing the user to decide which sections to read.
+ * Accordions can make information processing and discovering more effective. However, it does hide content from users and it’s important to account for a user not noticing or reading all of the included content.
+ *
+ * Accordions can be grouped or function independently. If icons are used in the header title, all header titles should have an icon. The default state for accordions is closed, but in specific situations it may make sense to have one already expanded.
+ *
+ * There is a regular type for the accordion component that is used in most cases. However, the accordion function can also be used for a custom implementation.
+ */
 const Accordion: React.FC<AccordionProps> = ({
   sections = [],
   type = 'regular',
