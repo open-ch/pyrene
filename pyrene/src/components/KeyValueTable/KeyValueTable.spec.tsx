2e838aee4ed8869d69d5a6d943223c376f05f7c5
Otto Zemzans <otz@open.ch>
2021-01-04 16:52:03 +0100

    remove import extension

--- pyrene/src/components/KeyValueTable/KeyValueTable.spec.tsx
+++ pyrene/src/components/KeyValueTable/KeyValueTable.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import KeyValueTable, { KeyValueTableProps } from './KeyValueTable.tsx';
+import KeyValueTable, { KeyValueTableProps } from './KeyValueTable';
 
 
 const props: KeyValueTableProps = {
