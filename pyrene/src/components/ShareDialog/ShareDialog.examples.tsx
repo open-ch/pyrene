3d094efedb456db789754dfb5cb98adbfbf35ad2
Xujia Zhou <xuz@open.ch>
2021-01-08 09:30:39 +0100

    Merge pull request #8 in PORTAL/pyrene-new from feature/share-dialog-ts-convert to main
    * commit 'd3db0b966c8f5908ec0591ea0b316158ac42e78f [formerly fb9d65e18564f5269482bf39f3bd4ef4cee72a4e]':
      converted ShareDialog to ts
    
    
    Former-commit-id: 2b3af49c5e341b68364168cbeca8c7140148001c

---
+++ pyrene/src/components/ShareDialog/ShareDialog.examples.tsx
@@ -0,0 +1,14 @@
+import { Example } from '../../examples/Example';
+import { ShareDialogProps } from './ShareDialog';
+
+const examples: Example<ShareDialogProps> = {};
+
+examples.props = {
+  position: 'top',
+  align: 'start',
+  link: 'http://www.veryveryverylonglinkonanydomainintheinternet.com',
+};
+
+examples.category = 'Other';
+
+export default examples;
