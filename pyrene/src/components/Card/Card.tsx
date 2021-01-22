4834c7dae0da2ecc9d41539420f69abcb10d1756
Alena Oblukova <alo@open.ch>
2021-01-22 17:22:43 +0100

    Merge pull request #22 in PORTAL/pyrene from feature/CardToTS to main
    * commit '10678d889017778b454e391e5d3e367feaeeb09e [formerly 5e01c80d5c3bf44b802576d33bcdbd6a9c9597ce]':
      render multiple children tested
      added card tests as well ;)
      Card to tsx
    
    
    Former-commit-id: 05cb9304514cbfeb11b097f5f003fd2a8196f075

--- pyrene/src/components/Card/Card.tsx
+++ pyrene/src/components/Card/Card.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { ReactElement } from 'react';
 import classNames from 'classnames';
 
 import Loader from '../Loader/Loader';
 import Banner from '../Banner/Banner';
 import './card.css';
 
+export interface CardProps {
+  /**
+   The content enclosed in the card.
+   */
+  children: ReactElement[] | ReactElement,
+  /**
+       * An error to be shown rather than the content.
+       */
+  error?: string,
+  /**
+       * An optional footer component.
+       */
+  footer?: ReactElement,
+  /**
+       * An optional header component, e.g., a time range selector.
+       */
+  header?: ReactElement,
+  /**
+       * Indicates whether the card is loading. Displays an overlay loader.
+       */
+  loading?: boolean,
+}
+
 /**
  * Cards are containers for content.
  *
  * They take the full width of the enclosing box and the height they need to fit the content.
 */
-const Card = ({
-  header, footer, children, loading, error,
-}) => (
+const Card:React.FC<CardProps> = ({
+  header,
+  footer,
+  children,
+  loading = false,
+  error,
+}: CardProps) => (
 
   <div styleName="container">
     {header && <div styleName="header">{header}</div>}
@@ -33,38 +59,5 @@ const Card = ({
 );
 
 Card.displayName = 'Card';
-
-Card.defaultProps = {
-  header: null,
-  error: null,
-  footer: null,
-  loading: false,
-};
-
-Card.propTypes = {
-  /**
- The content enclosed in the card.
- */
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]).isRequired,
-  /**
-  * An error to be shown rather than the content.
-  */
-  error: PropTypes.string,
-  /**
-  * An optional footer component.
-  */
-  footer: PropTypes.node,
-  /**
-  * An optional header component, e.g., a time range selector.
-  */
-  header: PropTypes.node,
-  /**
-  * Indicates whether the card is loading. Displays an overlay loader.
-  */
-  loading: PropTypes.bool,
-};
 
 export default Card;
