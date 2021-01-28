1c69527e34521692ec1bc437e03b6812e5c926d3
Julian Visser <jvi@open.ch>
2021-01-28 17:34:26 +0100

    MC-20639: Migrated CalendarDateSelectorUtils to date-fns

--- pyrene/src/components/CalendarDateSelector/CalendarDateSelectorUtils.js
+++ pyrene/src/components/CalendarDateSelector/CalendarDateSelectorUtils.js
@@ -1,4 +1,6 @@
-import moment from 'moment-timezone';
+import {
+  isAfter, isBefore, set, add,
+} from 'date-fns';
 
 const DAY = 'day';
 const MONTH = 'month';
@@ -11,30 +13,21 @@ export const DATE_TYPES = {
 };
 
 /**
- * Converts our custom date object to a moment instance
- * Because the momth of the internal object is 0-indexed and
- * externally the dates are passed in as 1-indexed, we need to convert them
- *
+ * Converts our custom date object to JavaScript Date
  * @param {Object} value
- * @returns {moment} moment instance
+ * @returns {Date}
  */
-export const convertToInternalMomentJs = (value) => moment().tz('UTC').set({ year: value.year, month: value.month - 1, date: value.day });
+export const convertToJsDate = (value) => new Date(value.year, value.month - 1, value.day);
 
 /**
- * Converts a momentjs instance to our custom date object format
- * It is passed a date instance and the type of object which is expected to be returned
- * 1. Year - { year }
- * 2. Month - { year, month }
- * 3. Day - { year, month, day }
- * It also increases the month number by 1 so that it is 1-indexed
- *
- * @param {moment} date
+ * Converts a JavaScript Date object to our custom date object format
+ * @param {Date} date
  * @returns {Object}
  */
-const convertToExternalObject = (date) => ({
-  year: date.year(),
-  month: date.month() + 1,
-  day: date.date(),
+const convertToExternalDateObject = (date) => ({
+  year: date.getFullYear(),
+  month: date.getMonth() + 1,
+  day: date.getDate(),
 });
 
 
@@ -45,48 +38,47 @@ const convertToExternalObject = (date) => ({
  * @param timeUnit which timeUnit we are currently changing: YEAR, MONTH or DAY
  */
 export const handleDateChange = (value, change, timeUnit) => {
-  let conversionDate = value;
+  const tempDate = value;
+
   // If we are changing Month or Year, set the date to the first of the month.
   if (timeUnit === MONTH || timeUnit === YEAR) {
-    conversionDate.day = 1;
+    tempDate.day = 1;
   }
   if (timeUnit === YEAR) {
-    conversionDate.month = 1;
+    tempDate.month = 1;
   }
-  conversionDate = convertToInternalMomentJs(conversionDate).add(change, `${timeUnit}s`);
-  return convertToExternalObject(conversionDate);
+
+  const conversionDate = add(convertToJsDate(tempDate), { [`${timeUnit}s`]: change });
+  return convertToExternalDateObject(conversionDate);
 };
 
 /**
  * Provides the current date object in the `value` prop format
  */
-export const getCurrentDate = () => {
-  const date = moment().utc();
-  return convertToExternalObject(date);
-};
+export const getCurrentDate = () => convertToExternalDateObject(new Date());
 
 export const canNavigateForward = (value, upperBound, timeRange) => {
-  const upperBoundDate = convertToInternalMomentJs(upperBound);
-  const valueDate = convertToInternalMomentJs(value);
+  const upperBoundDate = convertToJsDate(upperBound);
+  const valueDate = convertToJsDate(value);
   switch (timeRange) {
     case DATE_TYPES.YEAR:
       return value.year < upperBound.year;
     case DATE_TYPES.MONTH:
-      return valueDate.isBefore(upperBoundDate.set('date', 1));
+      return isBefore(valueDate, set(upperBoundDate, { date: 1 }));
     default:
-      return valueDate.isBefore(upperBoundDate);
+      return isBefore(valueDate, upperBoundDate);
   }
 };
 
 export const canNavigateBackward = (value, lowerBound, timeRange) => {
-  const lowerBoundDate = convertToInternalMomentJs(lowerBound);
-  const valueDate = convertToInternalMomentJs(value);
+  const lowerBoundDate = convertToJsDate(lowerBound);
+  const valueDate = convertToJsDate(value);
   switch (timeRange) {
     case DATE_TYPES.YEAR:
       return value.year > lowerBound.year;
     case DATE_TYPES.MONTH:
-      return valueDate.isAfter(lowerBoundDate.set('date', 1));
+      return isAfter(valueDate, set(lowerBoundDate, { date: 1 }));
     default:
-      return valueDate.isAfter(lowerBoundDate);
+      return isAfter(valueDate, lowerBoundDate);
   }
 };
