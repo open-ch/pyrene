6796aa227570d668b871c0c30c39c80fec74c02a
Sanmi Awi <ola@open.ch>
2021-02-12 13:38:18 +0100

    Return a date if input is valid.

--- pyrene/src/components/DateTimeInput/DateTimeInput.tsx
+++ pyrene/src/components/DateTimeInput/DateTimeInput.tsx
@@ -1,20 +1,92 @@
-import React, { RefObject, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import Icon from '../Icon/Icon';
 import './dateTimeInput.css';
 
 export interface DateTimeInputProps{
   name?: string,
   timeStamp?: number,
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  onChange?: (value: number | null) => void,
 }
+
+type DateType = {
+  day: number,
+  month: number,
+  year: number,
+} | undefined;
+
+type TimeType = {
+  minutes: number,
+  hours: number,
+} | undefined;
+
+export const getTimeStamp = (date:DateType, time:TimeType): number | null => {
+  if (time === undefined || date === undefined) {
+    return null;
+  }
+
+  const timeStamp = new Date(date.year, date.month, date.day, time.hours, time.minutes);
+  return timeStamp.valueOf();
+};
+
+export const getDate = (date:DateType): number | null => {
+  if (date === undefined) {
+    return null;
+  }
+  return new Date(date.year, date.month, date.day).valueOf();
+};
 
 const DateTimeInput: React.FC<DateTimeInputProps> = ({
   name,
   onChange,
 } : DateTimeInputProps) => {
 
+  let date: DateType;
+  let time: TimeType;
+
   const [dValue, setDateValue] = useState('');
   const [tValue, setTimeValue] = useState('');
+
+  const allowedSeparatorCheck = (valueToCheck:string) : boolean => (/[/.:]$/.test(valueToCheck));
+
+  const ddmmyyyyWithSep = (str:string): DateType => {
+    if (allowedSeparatorCheck(str.charAt(2)) && allowedSeparatorCheck(str.charAt(5))) {
+      return { day: +dValue.substr(0, 2), month: +dValue.substr(3, 2), year: +dValue.substr(6) };
+    }
+    return undefined;
+  };
+
+  const hhmmWithSep = (str:string): TimeType => {
+    if (allowedSeparatorCheck(str.charAt(2))) {
+      return { hours: +tValue.substr(0, 2), minutes: +tValue.substr(3) };
+    }
+    return undefined;
+  };
+
+  const dateChecker = () => {
+    let timestamp = null;
+    if (dValue.length === 10) {
+      timestamp = getDate(ddmmyyyyWithSep(dValue));
+    }
+    console.log('Date', timestamp);
+  };
+
+  const timeChecker = () => {
+    let timestamp = null;
+    if (dValue.length === 10 && tValue.length === 5) {
+      date = ddmmyyyyWithSep(dValue);
+      time = hhmmWithSep(tValue);
+
+      timestamp = getTimeStamp(date, time);
+
+
+      if (onChange) {
+        onChange(timestamp);
+        console.log('Parent Called with, ', timestamp);
+      }
+    }
+
+    console.log('Time', timestamp);
+  };
 
   const defOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const node = event.target as HTMLInputElement;
@@ -39,8 +111,9 @@ const DateTimeInput: React.FC<DateTimeInputProps> = ({
             placeholder="DD.MM.YYYY"
             styleName="dateInput"
             maxLength={10}
-            onChange={onChange || defOnChange}
+            onChange={defOnChange}
             value={dValue}
+            onKeyUp={dateChecker}
           />
         </div>
         <div styleName="iconInputContainer">
@@ -50,8 +123,9 @@ const DateTimeInput: React.FC<DateTimeInputProps> = ({
             placeholder="HH:MM"
             styleName="timeInput"
             maxLength={5}
-            onChange={onChange || defOnChange}
+            onChange={defOnChange}
             value={tValue}
+            onKeyUp={timeChecker}
           />
         </div>
 
