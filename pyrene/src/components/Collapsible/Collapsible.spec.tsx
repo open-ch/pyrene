73db071d2fae02484bcb8afdc41b1fb7c3872399
Eleni Mandilara <elm@open.ch>
2021-03-11 12:03:35 +0100

    Test if content is properly expanded/collapsed

--- pyrene/src/components/Collapsible/Collapsible.spec.tsx
+++ pyrene/src/components/Collapsible/Collapsible.spec.tsx
@@ -4,8 +4,11 @@ import { shallow } from 'enzyme';
 import Collapsible from './Collapsible';
 
 const props = {
-  renderCallback: () => (<div>ContentDiv</div>), // eslint-disable-line react/display-name
-  labelCollapsed: 'Show More',
+  renderCallback: function displayPlaceholder(): React.ReactElement {
+    return (<div>ContentDiv</div>);
+  },
+  labelCollapsed: 'More information',
+  labelExpanded: 'Less information',
 };
 
 describe('<Collapsible />', () => {
@@ -16,8 +19,34 @@ describe('<Collapsible />', () => {
   it('renders the content', () => {
     const rendered = shallow(<Collapsible {...props} />);
 
-    expect(rendered.contains('Show More')).toBe(true);
+    expect(rendered.contains('More information')).toBe(true);
     expect(rendered.contains('ContentDiv')).toBe(true);
+  });
+
+  it('renders the content when the `expanded` option is selected by default', () => {
+    const defaultExpandedProps = { ...props, defaultExpanded: true };
+    const rendered = shallow(<Collapsible {...defaultExpandedProps} />);
+
+    expect(rendered.contains('Less information')).toBe(true);
+    expect(rendered.contains('ContentDiv')).toBe(true);
+  });
+
+  it('renders expanded content when label is clicked on', () => {
+    const rendered = shallow(<Collapsible {...props} />);
+    rendered.find('.unSelectable').simulate('click');
+
+    expect(rendered.contains('Less information')).toBe(true);
+    expect(rendered.contains('ContentDiv')).toBe(true);
+  });
+
+  it('expands and collapses the content when the respective label is clicked on', () => {
+    const rendered = shallow(<Collapsible {...props} />);
+
+    rendered.find('.unSelectable').simulate('click');
+    expect(rendered.contains('Less information')).toBe(true);
+
+    rendered.find('.unSelectable').simulate('click');
+    expect(rendered.contains('More information')).toBe(true);
   });
 
 });
