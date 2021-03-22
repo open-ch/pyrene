0d71cd55cd36aec1b17382c41f90a88e6ba44a0a
Sanmi Awi <ola@open.ch>
2021-03-22 10:09:53 +0000

    MC-20654: Kitchensink error

--- pyrene/src/components/DateTimeInput/DateTimeInput.examples.tsx
+++ pyrene/src/components/DateTimeInput/DateTimeInput.examples.tsx
@@ -8,11 +8,7 @@ export interface State {
 const DateTimeInput: Example<DateTimeInputProps, State> = {};
 
 DateTimeInput.props = {
-  onChange: (stateProvider: StateProvider<State>) => (value?: number) => {
-    if (value) {
-      stateProvider.setState({ value });
-    }
-  },
+  onChange: (stateProvider: StateProvider<State>) => (value?: number) => { stateProvider.setState({ value }); },
   timeStamp: (stateProvider) => stateProvider.state.value,
 };
 
@@ -21,11 +17,7 @@ DateTimeInput.examples = [
     props: {
       minDateTime: 946684740000,
       maxDateTime: 1809631860000,
-      onChange: (stateProvider: StateProvider<State>) => (value?: number) => {
-        if (value) {
-          stateProvider.setState({ value });
-        }
-      },
+      onChange: (stateProvider: StateProvider<State>) => (value?: number) => { stateProvider.setState({ value }); },
       timeStamp: (stateProvider) => stateProvider.state.value,
     },
     description: `Maximum date: ${new Date(1809631860000).toString()},  Minimum date: ${new Date(946684740000).toString()}`,
@@ -33,22 +25,14 @@ DateTimeInput.examples = [
   {
     props: {
       maxDateTime: 1809631860000,
-      onChange: (stateProvider: StateProvider<State>) => (value?: number) => {
-        if (value) {
-          stateProvider.setState({ value });
-        }
-      },
+      onChange: (stateProvider: StateProvider<State>) => (value?: number) => { stateProvider.setState({ value }); },
       timeStamp: (stateProvider) => stateProvider.state.value,
     },
     description: `Maximum date: ${new Date(1809631860000).toString()}`,
   },
   {
     props: {
-      onChange: (stateProvider: StateProvider<State>) => (value?: number) => {
-        if (value) {
-          stateProvider.setState({ value });
-        }
-      },
+      onChange: (stateProvider: StateProvider<State>) => (value?: number) => { stateProvider.setState({ value }); },
       timeStamp: (stateProvider) => (stateProvider.state.value ? stateProvider.state.value : 1809631860000),
     },
     description: `Timestamp date: ${new Date(1809631860000).toString()}`,
