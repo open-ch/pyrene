379fda2b0044fcf8e835a3fa475a1dc15cbae156
Sakai-san <o_edo@hotmail.com>
2021-04-13 09:22:58 +0200

    Code formatting.

--- pyrene/src/components/Accordion/Accordion.tsx
+++ pyrene/src/components/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React , { FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react';
 import Section, { SectionProps } from './Section';
 import './accordion.css';
 
