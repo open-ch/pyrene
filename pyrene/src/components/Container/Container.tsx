279f9fdf40f5823ee9b782b22731a0eca6c0893a
Sakai-san <o_edo@hotmail.com>
2021-04-20 23:27:03 +0200

    Container component in TS.

--- pyrene/src/components/Container/Container.tsx
+++ pyrene/src/components/Container/Container.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {
+  FunctionComponent, useEffect, useState, useRef, MouseEvent,
+} from 'react';
 import clsx from 'clsx';
 
 import styles from './container.css';
@@ -7,85 +8,86 @@ import styles from './container.css';
 /**
  * Container contain content and actions about a single subject.
  */
-export default class Container extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      contentHeight: null,
-      expanded: props.defaultExpanded,
-    };
-  }
-
-  componentDidMount() {
-    if (this.contentRef) {
-      this.setState(() => ({
-        contentHeight: this.contentRef.clientHeight,
-      }));
-    }
-  }
-
-  toggleCollapse = (event) => {
-    event.persist();
-    if (this.props.collapsible) {
-      this.setState((prevState) => ({
-        expanded: !prevState.expanded,
-      }),
-      () => this.props.onChange(event));
-    }
-  };
-
-  render() {
-    return (
-      <div className={clsx(styles.container, { [styles.expanded]: this.state.expanded || !this.props.collapsible })}>
-        <div className={clsx(styles.header, { [styles.collapsible]: this.props.collapsible })} onClick={this.toggleCollapse} role="button" aria-label="Show or hide container">
-          <span className={clsx(styles.title, 'unSelectable')}>
-            {' '}
-            {this.props.title}
-          </span>
-          <div className={styles.arrowContainer}>
-            {this.props.collapsible && <span className={clsx('pyreneIcon-chevronDown', styles.collapsArrow)} />}
-          </div>
-        </div>
-        <div className={styles.contentContainer} style={{ height: (this.state.expanded || !this.props.collapsible) && this.state.contentHeight ? this.state.contentHeight : null }}>
-          <div className={styles.innerContentContainer} ref={(contentRef) => { this.contentRef = contentRef; }}>
-            {this.props.renderCallback()}
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-}
-
-
-Container.displayName = 'Container';
-
-Container.defaultProps = {
-  defaultExpanded: false,
-  onChange: () => null,
-  collapsible: false,
-};
-
-Container.propTypes = {
+interface ContainerProps {
   /**
    * Whether the container is collapsible when the user clicks on the header.
    */
-  collapsible: PropTypes.bool,
+  collapsible: boolean,
   /**
    * Whether to display the content when the component is first mounted.
    */
-  defaultExpanded: PropTypes.bool,
+  defaultExpanded: boolean,
   /**
    * Javascript event handler.
    */
-  onChange: PropTypes.func,
+  onChange: (event: MouseEvent<HTMLDivElement>) => void,
   /**
    * Sets the content to be rendered inside the component.
    */
-  renderCallback: PropTypes.func.isRequired,
+  renderCallback: () => JSX.Element,
   /**
    * Sets the title displayed in the header of the component.
    */
-  title: PropTypes.string.isRequired,
+  title: string,
+}
+
+const Container: FunctionComponent<ContainerProps> = ({
+  collapsible = false,
+  defaultExpanded = false,
+  onChange = () => null,
+  renderCallback,
+  title,
+}: ContainerProps) => {
+  const [contentHeight, setContentHeight] = useState<number|null>(null);
+  const [expanded, setExpanded] = useState<boolean>(defaultExpanded);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const clickedEvent = useRef<MouseEvent<HTMLDivElement>|null>(null);
+
+  useEffect(() => {
+    if (contentRef?.current?.clientHeight) {
+      setContentHeight(contentRef.current.clientHeight);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (contentRef?.current) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      onChange(clickedEvent.current);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [expanded]);
+
+
+  const toggleCollapse = (event: MouseEvent<HTMLDivElement>) => {
+    event.persist();
+    clickedEvent.current = event;
+    if (collapsible) {
+      setExpanded((prevState) => !prevState);
+    }
+  };
+
+  return (
+    <div className={clsx(styles.container, { [styles.expanded]: expanded || !collapsible })}>
+      <div className={clsx(styles.header, { [styles.collapsible]: collapsible })} onClick={toggleCollapse} role="button" aria-label="Show or hide container">
+        <span className={clsx(styles.title, 'unSelectable')}>
+          {' '}
+          {title}
+        </span>
+        <div className={styles.arrowContainer}>
+          {collapsible && <span className={clsx('pyreneIcon-chevronDown', styles.collapsArrow)} />}
+        </div>
+      </div>
+      <div className={styles.contentContainer} style={{ height: (expanded || !collapsible) && contentHeight ? contentHeight : 0 }}>
+        <div className={styles.innerContentContainer} ref={contentRef}>
+          {renderCallback()}
+        </div>
+      </div>
+    </div>
+  );
 };
+
+Container.displayName = 'Container';
+
+export default Container;
