4433acedc7f647d74683acd37f7b65fe802ea7cf
Sakai-san <o_edo@hotmail.com>
2021-04-22 02:28:33 +0200

    Typo in variable name.

--- pyrene/src/components/TimeRangeSelector/TimeRangeNavigationBar/TimeRangeNavigationBar.jsx
+++ pyrene/src/components/TimeRangeSelector/TimeRangeNavigationBar/TimeRangeNavigationBar.jsx
@@ -6,7 +6,7 @@ import { differenceInMinutes } from 'date-fns';
 
 import ArrowSelector from './ArrowSelector/ArrowSelector';
 
-const convertTZ = (date, tzString) => new Date((typeof date === 'string' ? new Date(date) : date).toLocaleString('en-US', { timeZone: tzString }));
+const convertTZ = (date, timeZone) => new Date((typeof date === 'string' ? new Date(date) : date).toLocaleString('en-US', { timeZone }));
 
 // eslint-disable-next-line prefer-template
 const formatDate = (date) => ('0' + date.getDate()).slice(-2) + '.' + ('0' + (date.getMonth() + 1)).slice(-2) + '.' + date.getFullYear() + ', ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
@@ -33,12 +33,12 @@ const TimeRangeNavigationBar = (props) => {
 
 TimeRangeNavigationBar.renderCurrentTimeRange = (from, to, timezone) => {
   const localFrom = convertTZ(new Date(from), timezone);
-  const localFormFormatted = formatDate(localFrom);
+  const localFromFormatted = formatDate(localFrom);
 
   const localTo = convertTZ(new Date(to), timezone);
   const localToFormatted = formatDate(localTo);
 
-  return `${localFormFormatted} - ${localToFormatted}`;
+  return `${localFromFormatted} - ${localToFormatted}`;
 };
 
 TimeRangeNavigationBar.defaultProps = {
