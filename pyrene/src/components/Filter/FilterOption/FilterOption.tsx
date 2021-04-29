1a880d576c9cbcc2d3070d3ff2b35c4eeccc51ba
Sakai-san <o_edo@hotmail.com>
2021-04-29 23:53:04 +0200

    TypeScript.

--- pyrene/src/components/Filter/FilterOption/FilterOption.tsx
+++ pyrene/src/components/Filter/FilterOption/FilterOption.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { FunctionComponent } from 'react';
 
 import './filterOption.css';
 import SingleSelect from '../../SingleSelect/SingleSelect';
@@ -7,111 +6,111 @@ import TextField from '../../TextField/TextField';
 import MultiSelect from '../../MultiSelect/MultiSelect';
 import Checkbox from '../../Checkbox/Checkbox';
 
-export default class FilterOption extends React.Component {
+interface FilterOptionsProps {
+  handleFilterChange: (value: any, inputValue: string | boolean, id: string) => void,
+  id: string,
+  label: string,
+  negatable: boolean,
+  negated?: boolean,
+  options?: any[],
+  sorted?: boolean,
+  type: string,
+  value?: any,
+}
 
-    doesInterfaceSupportNegate = () => {
-      switch (this.props.type) {
-        case 'text':
-        case 'singleSelect':
-        case 'multiSelect': return true;
-        default: return false;
-      }
-    };
+const FilterOption: FunctionComponent<FilterOptionsProps> = ({
+  handleFilterChange,
+  id,
+  label,
+  negatable,
+  type,
+  sorted = true,
+  negated = false,
+  options = [],
+  value = null
+}: FilterOptionsProps) => {
 
-    getFilterInterface = () => {
+  const doesInterfaceSupportNegate = (inputType: string) => {
+    switch (inputType) {
+      case 'text':
+      case 'singleSelect':
+      case 'multiSelect': return true;
+      default: return false;
+    }
+  };
 
-      const isValue = !!this.props.value;
+  const getFilterInterface = () => {
 
-      switch (this.props.type) {
-        case 'singleSelect':
-          return (
-            <SingleSelect
-              name={this.props.id}
-              options={this.props.options}
-              onChange={(value) => this.props.handleFilterChange(value, this.props.negated, this.props.id)}
-              value={isValue ? this.props.value : null}
-              sorted={this.props.sorted}
-              clearable
-              searchable
+    const isValue = !!value;
+
+    switch (type) {
+      case 'singleSelect':
+        return (
+          <SingleSelect
+            name={id}
+            options={options}
+            onChange={(value) => handleFilterChange(value, negated, id)}
+            value={isValue ? value : null}
+            sorted={sorted}
+            clearable
+            searchable
+          />
+        );
+      case 'multiSelect':
+        return (
+          <MultiSelect
+            name={id}
+            options={options}
+            // If multiSelect is empty (empty array) return null to filter instead of []
+            onChange={(value) => handleFilterChange(value && value.length === 0 ? null : value, negated, id)}
+            // Pass empty array instead of null to multiSelect component if filterValues are null
+            value={isValue ? value : []}
+            sorted={sorted}
+            selectedOptionsInDropdown
+            keepMenuOnSelect
+            clearable
+          />
+        );
+      case 'text':
+        return (
+          <TextField
+            name={id}
+            // If textField is empty (empty string) return null instead of ''
+            onChange={(value) => handleFilterChange(value === '' ? null : value, false, id)}
+            // Pass empty string instead of null to textField component if filterValues are null
+            value={isValue ? value : ''}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div styleName="filterOption">
+      <div styleName="filterOptionWrapper">
+        <div styleName="label">
+          {label}
+        </div>
+        <div styleName="interface">
+          {getFilterInterface()}
+        </div>
+      </div>
+      {negatable && (
+        <div styleName="negatedCheckbox">
+          {doesInterfaceSupportNegate(type) && (
+            <Checkbox
+              value={negated}
+              disabled={false}
+              onChange={(inputValue) => handleFilterChange(value, inputValue, id)}
+              tooltip="To negate the filter, check the box."
             />
-          );
-        case 'multiSelect':
-          return (
-            <MultiSelect
-              name={this.props.id}
-              options={this.props.options}
-              // If multiSelect is empty (empty array) return null to filter instead of []
-              onChange={(value) => this.props.handleFilterChange(value && value.length === 0 ? null : value, this.props.negated, this.props.id)}
-              // Pass empty array instead of null to multiSelect component if filterValues are null
-              value={isValue ? this.props.value : []}
-              sorted={this.props.sorted}
-              selectedOptionsInDropdown
-              keepMenuOnSelect
-              clearable
-            />
-          );
-        case 'text':
-          return (
-            <TextField
-              name={this.props.id}
-              // If textField is empty (empty string) return null instead of ''
-              onChange={(value) => this.props.handleFilterChange(value === '' ? null : value, false, this.props.id)}
-              // Pass empty string instead of null to textField component if filterValues are null
-              value={isValue ? this.props.value : ''}
-            />
-          );
-        default:
-          return null;
-      }
-    };
-
-    render() {
-      return (
-        <div styleName="filterOption">
-          <div styleName="filterOptionWrapper">
-            <div styleName="label">
-              {this.props.label}
-            </div>
-            <div styleName="interface">
-              {this.getFilterInterface()}
-            </div>
-          </div>
-          {this.props.negatable && (
-            <div styleName="negatedCheckbox">
-              {this.doesInterfaceSupportNegate() && (
-                <Checkbox
-                  value={this.props.negated}
-                  disabled={false}
-                  onChange={(value) => this.props.handleFilterChange(this.props.value, value, this.props.id)}
-                  tooltip="To negate the filter, check the box."
-                />
-              )}
-            </div>
           )}
         </div>
-      );
-    }
-
-}
+      )}
+    </div>
+  );
+};
 
 
 FilterOption.displayName = 'FilterOption';
-
-FilterOption.defaultProps = {
-  negated: false,
-  options: [],
-  value: null,
-  sorted: true,
-};
-
-FilterOption.propTypes = {
-  handleFilterChange: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  negatable: PropTypes.bool.isRequired,
-  negated: PropTypes.bool,
-  options: PropTypes.array, // eslint-disable-line react/forbid-prop-types
-  sorted: PropTypes.bool,
-  type: PropTypes.string.isRequired,
-  value: PropTypes.oneOfType([PropTypes.array, PropTypes.string, PropTypes.object]),
-};
