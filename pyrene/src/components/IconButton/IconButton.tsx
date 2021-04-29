564b2fa03c809d8fcfd67083e9de0d019219cf14
Sanmi Awi <ola@open.ch>
2021-04-29 10:10:05 +0000

    MC-21322: change styleName to className in IconButton component

--- pyrene/src/components/IconButton/IconButton.tsx
+++ pyrene/src/components/IconButton/IconButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import './iconbutton.css';
+import styles from './iconbutton.css';
 
 export interface IconButtonProps {
   /**
@@ -35,7 +35,7 @@ const IconButton: React.FC<IconButtonProps> = ({
   path = '#',
   type = 'neutral',
 }: IconButtonProps) => (
-  <a styleName={clsx('iconButton', { disabled: disabled }, { [`type-${type}`]: true })}
+  <a className={clsx(styles.iconButton, { [styles.disabled]: disabled }, { [styles[`type-${type}`]]: true })}
     href={path}
     onClick={
       onClick ? ((event) => {
