59e0b7622f7a72ac07c625e3dffa96d8be479545
Sakai-san <o_edo@hotmail.com>
2021-04-30 21:30:42 +0200

    Example to tsx.

--- pyrene/src/components/DropdownButton/DropdownButton.examples.tsx
+++ pyrene/src/components/DropdownButton/DropdownButton.examples.tsx
@@ -1,12 +1,15 @@
-const examples = {
-  props: {
-    label: 'Actions',
-    icon: 'errorOutline',
-    actions: [
-      { label: 'action 1', onClick: () => {} },
-      { label: 'action 2', onClick: () => {} },
-    ],
-  },
+import { Example } from '../../examples/Example';
+import { DropdownButtonProps } from './DropdownButton';
+
+const examples: Example<DropdownButtonProps> = {};
+
+examples.props = {
+  label: 'Actions',
+  icon: 'errorOutline',
+  actions: [
+    { label: 'action 1', onClick: () => {} },
+    { label: 'action 2', onClick: () => {} },
+  ],
 };
 
 examples.category = 'Interaction';
