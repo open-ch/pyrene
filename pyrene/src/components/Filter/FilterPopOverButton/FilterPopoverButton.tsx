0e9792a688b7307b57867e8df0b6428533924231
Sakai-san <o_edo@hotmail.com>
2021-04-30 16:19:42 +0200

    Task done.

--- pyrene/src/components/Filter/FilterPopOverButton/FilterPopoverButton.tsx
+++ pyrene/src/components/Filter/FilterPopOverButton/FilterPopoverButton.tsx
@@ -1,60 +1,70 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { FunctionComponent, MouseEvent } from 'react';
 import Popover from '../../Popover/Popover';
-
+import { SingleSelectProps } from '../../SingleSelect/SingleSelect';
 import FilterPopover from '../FilterPopover/FilterPopover';
 import FilterButton from '../FilterComponents/FilterButton';
+import { IconProps } from '../../Icon/Icon';
 
-const FilterPopoverButton = (props) => (
+type SingleSelectValue = SingleSelectProps<unknown>['value'];
+type MultiselectValue = Array<{iconProps?: IconProps, label: string, value?: string | number | boolean}>;
+type TextFieldValue = string;
+
+export interface FilterPopoverButtonProps {
+  displayPopover?: boolean,
+  filterNegatedKeys: string[],
+  filters:Array<{
+    id?: string,
+    label?: string,
+    negated?: boolean,
+    options?: any[],
+    sorted?: boolean,
+    type?: string,
+  }>,
+  filterValues: { [key: string]: MultiselectValue | SingleSelectValue | TextFieldValue },
+  handleFilterChange: (value: any, negated: boolean, id: string) => void,
+  label: string,
+  negatable: boolean,
+  noBorder?: boolean,
+  onClick?: (e: MouseEvent) => void,
+  onFilterApply: (e: MouseEvent) => void,
+  onFilterClear: (e: MouseEvent) => void,
+}
+
+const FilterPopoverButton: FunctionComponent<FilterPopoverButtonProps> = ({
+  handleFilterChange,
+  filterValues,
+  negatable,
+  filters,
+  filterNegatedKeys,
+  onFilterClear,
+  onFilterApply,
+  label,
+  onClick = () => null,
+  displayPopover = false,
+  noBorder = false,
+}: FilterPopoverButtonProps) => (
   <Popover
     align="start"
     distanceToTarget={8}
-    displayPopover={props.displayPopover}
+    displayPopover={displayPopover}
     preferredPosition={['bottom']}
     renderPopoverContent={() => (
       <FilterPopover
-        filters={props.filters}
-        handleFilterChange={props.handleFilterChange}
-        filterValues={props.filterValues}
-        filterNegatedKeys={props.filterNegatedKeys}
-        negatable={props.negatable}
-        onFilterClear={props.onFilterClear}
-        onClose={props.onClick}
-        onFilterApply={props.onFilterApply}
+        filters={filters}
+        handleFilterChange={handleFilterChange}
+        filterValues={filterValues}
+        filterNegatedKeys={filterNegatedKeys}
+        negatable={negatable}
+        onFilterClear={onFilterClear}
+        onClose={onClick}
+        onFilterApply={onFilterApply}
       />
     )}
   >
-    <FilterButton disabled={false} displayPopover={props.displayPopover} label={props.label} noBorder={props.noBorder} onClick={props.onClick} />
+    <FilterButton disabled={false} displayPopover={displayPopover} label={label} noBorder={noBorder} onClick={onClick} />
   </Popover>
 );
 
 FilterPopoverButton.displayName = 'FilterPopoverButton';
-
-FilterPopoverButton.defaultProps = {
-  noBorder: false,
-  displayPopover: false,
-  onClick: () => null,
-};
-
-FilterPopoverButton.propTypes = {
-  displayPopover: PropTypes.bool,
-  filterNegatedKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
-  filters: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string,
-    label: PropTypes.string,
-    negated: PropTypes.bool,
-    options: PropTypes.array,
-    sorted: PropTypes.bool,
-    type: PropTypes.string,
-  })).isRequired,
-  filterValues: PropTypes.shape({}).isRequired,
-  handleFilterChange: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired,
-  negatable: PropTypes.bool.isRequired,
-  noBorder: PropTypes.bool,
-  onClick: PropTypes.func,
-  onFilterApply: PropTypes.func.isRequired,
-  onFilterClear: PropTypes.func.isRequired,
-};
 
 export default FilterPopoverButton;
