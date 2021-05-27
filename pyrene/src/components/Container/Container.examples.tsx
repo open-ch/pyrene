2619267bcba0836e5e4365d205f32c255399ae82
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2021-05-27 17:57:29 +0200

    Container component in TypeScript (#26)
    * File renaming.
    
    * Container component in TS.
    
    * Conditional style.
    
    * Remove import.
    
    * Default when props are optional.
    
    * Conversion done.
    
    * defaultProps for retro-compatibility.
    
    * Export.
    
    * Export type.
    
    * Remove disabled linter.
    
    * Linting.
    
    * useReducer.
    
    * Fix issue in kitchensink for loading component.
    
    * Clean up.
    
    * Clean up.
    
    * Formatting.
    
    * jsx to tsx.
    
    * Typing.
    
    * jsx to tsx.
    
    * Code readibility.
    
    * Simplify the reducer.
    
    * Remove unused imported function.
    
    * Enhancement following code review.

--- pyrene/src/components/Container/Container.examples.tsx
+++ pyrene/src/components/Container/Container.examples.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { ContainerProps } from './Container';
+import { Example } from '../../examples/Example';
 import Placeholder from '../../examples/Placeholder';
 
-const examples = {
+const examples: Example<ContainerProps> = {
   props: {
     title: 'Container',
     defaultExpanded: true,
     renderCallback: () => <Placeholder label="Content" width={320} />, // eslint-disable-line react/display-name
   },
+  category: 'Layout',
 };
-
-examples.category = 'Layout';
 
 export default examples;
