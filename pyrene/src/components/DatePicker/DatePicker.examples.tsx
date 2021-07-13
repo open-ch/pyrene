a76a193041b2a90f2a4108a2c14618d7d06a2fe2
Sakai-san <o_edo@hotmail.com>
2021-07-13 13:22:57 +0200

    Datepicker POC new component.

---
+++ pyrene/src/components/DatePicker/DatePicker.examples.tsx
@@ -0,0 +1,12 @@
+import { Example } from '../../examples/Example';
+import { DatePickerProps } from './DatePicker';
+
+
+const ReactDatePickerWrapper: Example<DatePickerProps> = {
+    props : {
+        shouldDisplayTimeColumn: true,
+    },
+    category: "Form",
+};
+
+export default ReactDatePickerWrapper;
