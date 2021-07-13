fabeeaf7b164fe85e32d963e1f1acbe4a660d354
Sakai-san <o_edo@hotmail.com>
2021-07-13 13:22:57 +0200

    Datepicker POC new component.

---
+++ pyrene/src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,78 @@
+import React, {
+    useState,
+    FunctionComponent,
+  } from 'react';
+  import ReactDatepicker from 'react-datepicker';
+  
+  import 'react-datepicker/dist/react-datepicker.css';
+  import styles from '../datePicker.css';
+  import setHours from "date-fns/setHours";
+  import setMinutes from "date-fns/setMinutes";
+  
+  export interface DatePickerProps {
+    shouldDisplayTimeColumn?: boolean,
+  }
+  
+  const DatePicker: FunctionComponent<DatePickerProps> = ({
+    shouldDisplayTimeColumn = true,
+  }: DatePickerProps) => {
+  
+    const nextIcon = <span className="pyreneIcon-chevronRight" />;
+    const prevIcon = <span className="pyreneIcon-chevronLeft" />;
+  
+    const [startDate, setStartDate] = useState( new Date() );
+  
+    return (
+      <div className={styles.wrapper}>
+        <ReactDatepicker
+          renderCustomHeader={({
+            date,
+            decreaseMonth,
+            increaseMonth,
+          }) => (
+            <div>
+              {/* eslint-disable-next-line react/button-has-type */}
+              <button
+                aria-label="Previous Month"
+                className="react-datepicker__navigation react-datepicker__navigation--previous"
+                onClick={decreaseMonth}
+              >
+                {prevIcon}
+              </button>
+              <span className="react-datepicker__current-month">
+                {date.toLocaleString('en-US', {
+                  month: 'long',
+                })}
+                &nbsp;
+                <span className="pyrene__current-year">
+                  {date.toLocaleString('en-US', {
+                    year: 'numeric',
+                  })}
+                </span>
+              </span>
+              {/* eslint-disable-next-line react/button-has-type */}
+              <button
+                aria-label="Next Month"
+                className="react-datepicker__navigation react-datepicker__navigation--next"
+                onClick={increaseMonth}
+              >
+                {nextIcon}
+              </button>
+            </div>
+          )}
+          calendarStartDay={1}
+          selected={startDate}
+          showPopperArrow={false}
+          showTimeSelect={shouldDisplayTimeColumn}
+          onChange={(date: Date) => setStartDate(date) }
+          dateFormat={shouldDisplayTimeColumn ? 'dd.MM.yyyy hh:mm aa' : 'dd.MM.yyyy'}
+          formatWeekDay={(nameOfDay) => nameOfDay.substr(0, 1)}
+          placeholderText="dd.MM.yyyy hh:mm"
+        />
+      </div>
+    );
+  };
+  
+  DatePicker.displayName = 'DatePicker';
+  
+  export default DatePicker;
