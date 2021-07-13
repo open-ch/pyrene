4616c722bab2c1c32cdb18affb82115c2fc69002
Sakai-san <o_edo@hotmail.com>
2021-07-13 16:28:58 +0200

    Debugging.

--- pyrene/src/components/DatePicker/DatePicker.tsx
+++ pyrene/src/components/DatePicker/DatePicker.tsx
@@ -2,37 +2,74 @@ import React, {
   useState,
   FunctionComponent,
   useEffect,
+  forwardRef,
 } from 'react';
 import ReactDatepicker from 'react-datepicker';
 import clsx from 'clsx';
 
+import {isExists} from 'date-fns';
 import isValid from 'date-fns/isValid';
 import setHours from 'date-fns/setHours';
 import setMinutes from 'date-fns/setMinutes';
 import styles from './datePicker.css';
 import 'react-datepicker/dist/react-datepicker.css';
 
-
-const ExampleCustomTimeInput: FunctionComponent<any> = ({ date, value, onChange }) => {
-
-  console.log('rendering of ExampleCustomTimeInput');
-
-  return (
-    <input
-      value={value}
-      onChange={(e) => {
-        const value = e.target.value;
-        console.log('new value', value);
-        onChange(value);
-      }}
-      style={{ border: "solid 1px pink" }}
-    />
-  );
-};
-
 export interface DatePickerProps {
   dateOnly?: boolean,
 }
+
+export const isValidDate = (date: string): boolean => {
+  return date.length === 10;
+};
+
+const allowedSeparatorCheck = (valueToCheck: string) => (/[/.:]$/.test(valueToCheck));
+
+export const getDateTypeFromddmmyyyyWithSep = (str: string): any | undefined => {
+  if (str.length === 10 && allowedSeparatorCheck(str.charAt(2)) && allowedSeparatorCheck(str.charAt(5))) {
+    const date = { day: +str.substr(0, 2), month: +str.substr(3, 2), year: +str.substr(6) };
+    if (!Number.isNaN(date.day) && !Number.isNaN(date.month) && !Number.isNaN(date.year)) {
+      return date;
+    }
+  }
+  return undefined;
+};
+
+const ExampleCustomTimeInput: FunctionComponent<any> = forwardRef(({ value, onChange, onClick, setStartDate }, ref) => {
+
+  const [internalValue, setInternalValue] = useState(value);
+  const [hasError, setHasError] = useState(false);
+
+  console.log('internal', internalValue);
+
+  return (
+    <div>
+      <input
+        ref={ref as any}
+        onClick={onClick}
+        value={internalValue}
+        onChange={(e) => {
+          const enterredDate = e.target.value;
+          console.log('new value', enterredDate);
+          setInternalValue(enterredDate);
+
+          if(!isValidDate(enterredDate)){
+            console.log('NOT valid', enterredDate );
+            setHasError(true);
+          }
+          else{
+            console.log('IS valid', enterredDate );
+            const dateFormatted = getDateTypeFromddmmyyyyWithSep(enterredDate);
+           // setStartDate(new Date( dateFormatted.year + '-' + dateFormatted.month + '-' + dateFormatted.day));
+            onChange(e);
+            setHasError(false);
+          }
+        }}
+        style={{ border: "solid 10px pink !important" }}
+      />
+      {hasError && <div>issue in the formatting</div>}
+    </div>
+  )
+});
 
 const DatePicker: FunctionComponent<DatePickerProps> = ({
   dateOnly,
@@ -42,7 +79,8 @@ const DatePicker: FunctionComponent<DatePickerProps> = ({
   const prevIcon = <span className="pyreneIcon-chevronLeft" />;
 
   const [startDate, setStartDate] = useState(new Date());
-
+  
+  console.log('external', startDate);
   return (
     <div className={styles.wrapper}>
       <ReactDatepicker
@@ -85,12 +123,12 @@ const DatePicker: FunctionComponent<DatePickerProps> = ({
         selected={startDate}
         showPopperArrow={false}
         showTimeSelect={!dateOnly}
-        onChange={(date: Date) => setStartDate(date)}
+        onChange={(date: Date) => useState(date)}
         timeFormat='hh:mm'
         dateFormat={dateOnly ? 'dd.MM.yyyy' : 'dd.MM.yyyy hh:mm aa'}
         formatWeekDay={(nameOfDay) => nameOfDay.substr(0, 1)}
         placeholderText="dd.MM.yyyy hh:mm"
-        customTimeInput={<ExampleCustomTimeInput />}
+        customInput={<ExampleCustomTimeInput setStartDate={setStartDate}/>}
       />
     </div>
   );
