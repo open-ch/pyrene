1a785bb39d44468f72e5833c859dd4034f5f09e4
Sakai-san <o_edo@hotmail.com>
2021-08-05 09:04:32 +0200

    label link to input.

--- pyrene/src/components/DatePickerMultiple/DatePickerMultiple.tsx
+++ pyrene/src/components/DatePickerMultiple/DatePickerMultiple.tsx
@@ -19,37 +19,49 @@ const DatePickerMultiple: FunctionComponent<DatePickerMultipleProps> = ({
   const [endDate, setEndDate] = useState<null | Date>(null);
   
   return (  
-    <div className={styles.wrapper}>
-      <ReactDatepicker
-        calendarStartDay={1}
-        selected={startDate}
-        showPopperArrow={false}
-        showTimeSelect={!dateOnly}
-        onChange={(date: Date) => setStartDate(date)}
-        selectsStart
-        timeFormat="hh:mm"
-        dateFormat={dateOnly ? DATE_FORMAT : DATETIME_FORMAT}
-        formatWeekDay={(nameOfDay) => nameOfDay.substr(0, 1)}
-        placeholderText={dateOnly ? DATE_FORMAT.toLowerCase() : DATETIME_FORMAT.toLowerCase()}
-        customInput={<CustomTimeInput dateOnly={dateOnly}/>}
-      />
-      <ReactDatepicker
-        className={styles.DatePickerTo}
-        calendarStartDay={1}
-        selected={endDate}
-        showPopperArrow={false}
-        showTimeSelect={!dateOnly}
-        onChange={(date: Date) => setEndDate(date)}
-        selectsEnd
-        startDate={startDate}
-        endDate={endDate}
-        minDate={startDate}
-        timeFormat="hh:mm"
-        dateFormat={dateOnly ? DATE_FORMAT : DATETIME_FORMAT}
-        formatWeekDay={(nameOfDay) => nameOfDay.substr(0, 1)}
-        placeholderText={dateOnly ? DATE_FORMAT.toLowerCase() : DATETIME_FORMAT.toLowerCase()}
-        customInput={<CustomTimeInput dateOnly={dateOnly}/>}
-      />
+    <div className={styles.Wrapper}>
+      <div className={styles.DatePickerFromWrapper}>
+        <label>
+          From
+          <br />
+          <ReactDatepicker
+            calendarStartDay={1}
+            selected={startDate}
+            showPopperArrow={false}
+            showTimeSelect={!dateOnly}
+            onChange={(date: Date) => setStartDate(date)}
+            selectsStart
+            timeFormat="hh:mm"
+            dateFormat={dateOnly ? DATE_FORMAT : DATETIME_FORMAT}
+            formatWeekDay={(nameOfDay) => nameOfDay.substr(0, 1)}
+            placeholderText={dateOnly ? DATE_FORMAT.toLowerCase() : DATETIME_FORMAT.toLowerCase()}
+            customInput={<CustomTimeInput dateOnly={dateOnly}/>}
+          />
+        </label>
+      </div>
+      <div className={styles.DatePickerToWrapper}>
+        <label>
+          To
+          <br />
+          <ReactDatepicker
+            className={styles.DatePickerTo}
+            calendarStartDay={1}
+            selected={endDate}
+            showPopperArrow={false}
+            showTimeSelect={!dateOnly}
+            onChange={(date: Date) => setEndDate(date)}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            timeFormat="hh:mm"
+            dateFormat={dateOnly ? DATE_FORMAT : DATETIME_FORMAT}
+            formatWeekDay={(nameOfDay) => nameOfDay.substr(0, 1)}
+            placeholderText={dateOnly ? DATE_FORMAT.toLowerCase() : DATETIME_FORMAT.toLowerCase()}
+            customInput={<CustomTimeInput dateOnly={dateOnly}/>}
+          />
+        </label>
+      </div>
     </div>
   );
 };
