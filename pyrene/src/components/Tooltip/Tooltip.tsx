e9f149698f5d99a85a1f28e55c0eb7a86be36ea4
Cyril Wanner <cwa@open.ch>
2021-08-12 15:53:50 +0200

    Migrate tooltip component to typescript

--- pyrene/src/components/Tooltip/Tooltip.tsx
+++ pyrene/src/components/Tooltip/Tooltip.tsx
@@ -1,92 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Popover from 'react-tiny-popover';
 
 import styles from './tooltip.css';
 
-/**
- * A Tooltip is a small piece of contextual information about an element on the screen, which is displayed when a user hovers or focuses on the element it is describing.
- */
-export default class Tooltip extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPopoverOpen: false,
-    };
-  }
-
-  render() {
-    return (
-      <Popover
-        isOpen={this.state.isPopoverOpen}
-        position={this.props.preferredPosition}
-        align={this.props.align}
-        padding={this.props.distanceToTarget}
-        content={({
-          position, nudgedLeft, nudgedTop, targetRect, popoverRect, // eslint-disable-line no-unused-vars
-        }) => (
-          <div className={styles.tooltip} style={{ maxWidth: this.props.maxWidth }}>
-            {this.props.label}
-          </div>
-        )}
-        containerStyle={{
-          borderRadius: '2px',
-          boxShadow: '0 4px 8px -2px rgba(0, 21, 44, 0.2), 0 0 1px 0 rgba(0, 21, 44, 0.3)',
-          // @ts-ignore
-          zIndex: 9999,
-        }}
-        disableReposition={!this.props.autoReposition}
-      >
-        <div
-          onMouseOver={() => this.setState({ isPopoverOpen: true })}
-          onMouseLeave={() => this.setState({ isPopoverOpen: false })}
-        >
-          {this.props.children}
-        </div>
-      </Popover>
-    );
-  }
-
-}
-
-Tooltip.displayName = 'Tooltip';
-
-Tooltip.defaultProps = {
-  preferredPosition: [],
-  align: 'center',
-  autoReposition: true,
-  maxWidth: 344,
-  distanceToTarget: 8,
-};
-
-Tooltip.propTypes = {
+export interface TooltipProps {
   /**
    * Sets the alignment of the tooltip.
    */
-  align: PropTypes.oneOf(['start', 'center', 'end']),
+  align?: 'start' | 'center' | 'end';
   /**
    * Whether automatic repositioning is enabled.
    */
-  autoReposition: PropTypes.bool,
+  autoReposition?: boolean;
   /**
    * Wrapped component(s) that the tooltip is using for its positioning.
    */
-  children: PropTypes.element.isRequired,
+  children: React.ReactNode;
   /**
    * Sets the distance of the tooltip to its target.
    */
-  distanceToTarget: PropTypes.number,
+  distanceToTarget?: number;
   /**
    * Sets the label displayed inside of the tooltip.
    */
-  label: PropTypes.node.isRequired,
+  label: React.ReactNode;
   /**
    * Sets the Tooltip width. Only to be used to enforce line breaks.
    */
-  maxWidth: PropTypes.number,
+  maxWidth?: number;
   /**
-    * Sets the preferred position array ordered by priority for auto repositioning.
-    */
-  preferredPosition: PropTypes.arrayOf(PropTypes.oneOf(['top', 'right', 'bottom', 'left'])),
+   * Sets the preferred position array ordered by priority for auto repositioning.
+   */
+  preferredPosition?: ('top' | 'right' | 'bottom' | 'left')[];
+}
+
+/**
+ * A Tooltip is a small piece of contextual information about an element on the screen, which is displayed when a user hovers or focuses on the element it is describing.
+ */
+const Tooltip: React.FC<TooltipProps> = ({
+  preferredPosition = [],
+  align = 'center',
+  autoReposition = true,
+  maxWidth = 344,
+  distanceToTarget = 8,
+  label,
+  children,
+}: TooltipProps) => {
+  const [isPopoverOpen, setPopoverOpen] = useState(false);
+
+  return (
+    <Popover
+      isOpen={isPopoverOpen}
+      position={preferredPosition}
+      align={align}
+      padding={distanceToTarget}
+      content={({
+        position, nudgedLeft, nudgedTop, targetRect, popoverRect, // eslint-disable-line no-unused-vars
+      }) => (
+        <div className={styles.tooltip} style={{ maxWidth }}>
+          {label}
+        </div>
+      )}
+      containerStyle={{
+        borderRadius: '2px',
+        boxShadow: '0 4px 8px -2px rgba(0, 21, 44, 0.2), 0 0 1px 0 rgba(0, 21, 44, 0.3)',
+        zIndex: '9999',
+      }}
+      disableReposition={!autoReposition}
+    >
+      <div
+        onMouseOver={() => setPopoverOpen(true)}
+        onMouseLeave={() => setPopoverOpen(false)}
+      >
+        {children}
+      </div>
+    </Popover>
+  );
 };
+
+Tooltip.displayName = 'Tooltip';
+
+export default Tooltip;
