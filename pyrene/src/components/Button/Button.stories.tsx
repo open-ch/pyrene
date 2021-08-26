a5f36abcbab6a2b31c1b773e3e6c8b15ab36084e
Cyril Wanner <cwa@open.ch>
2021-08-26 16:04:20 +0200

    Initial storybook set up

---
+++ pyrene/src/components/Button/Button.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Story, Meta } from '@storybook/react';
+
+import Button, {ButtonProps} from './Button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+// Primary button
+export const Primary = Template.bind({});
+
+Primary.args = {
+  label: 'Button',
+};
+
+// Danger button
+export const Danger = Template.bind({});
+
+Danger.args = {
+  label: 'Button',
+  type: 'danger',
+};
