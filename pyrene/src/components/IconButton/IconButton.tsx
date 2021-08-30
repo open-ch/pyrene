f92da07d7621c568031de2243492bc39071bb822
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2021-08-30 21:13:06 +0200

    storybook story button (#157)
    * Icon name typing.
    
    * Stories button component.
    
    * Import.

--- pyrene/src/components/IconButton/IconButton.tsx
+++ pyrene/src/components/IconButton/IconButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './iconbutton.css';
+import { IconNames } from '../types';
 
 export interface IconButtonProps {
   /**
@@ -10,7 +11,7 @@ export interface IconButtonProps {
   /**
    * Sets the icon shown
    */
-  icon: string,
+  icon: keyof IconNames,
   /**
    * Javascript event handler. Overrides the redirect functionality (path prop ignored).
    */
