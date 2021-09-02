5615bcc411b96eb7288f7eef0d9142d370d47582
Sakai-san <o_edo@hotmail.com>
2021-09-02 22:20:18 +0200

    Component providing state.

--- pyrene/src/components/TabView/TabView.stories.tsx
+++ pyrene/src/components/TabView/TabView.stories.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
+// @ts-nocheck
+import React, { Component } from 'react';
 
 import { Story, Meta } from '@storybook/react';
 import Icon from '../Icon/Icon';
 import TabViewComponent, { TabViewProps } from './TabView';
+import StateProvider from '../../examples/StateProvider';
 import Placeholder from '../../examples/Placeholder';
-import { Example, StateProvider } from '../../examples/Example';
 
 export default {
   title: 'Components/Layout/TabView',
   component: TabViewComponent,
 } as Meta;
-
-interface State {
-  tabName: string
-}
 
 const renderAuxiliaryIcon = (name: string) => (
   <div style={{ marginLeft: 4 }}>
@@ -21,23 +18,35 @@ const renderAuxiliaryIcon = (name: string) => (
   </div>
 );
 
-const Template: Story<Example<TabViewProps, State>> = (args) => <TabViewComponent {...args as any} />;
+type ProviderState = {
+  tabName: undefined | string;
+};
+
+const Template: Story<TabViewProps> = (args) => {
+  console.log('args', args);
+  return (
+    <StateProvider<TabViewProps, ProviderState> initState={{ tabName: undefined }}>
+      {
+        (state, setState) => ({
+          component: TabViewComponent,
+          props: {
+            initialTabName: 'Tab 1',
+            directAccessTabs: 3,
+            tabChanged: () => setState((prevState: ProviderState) => ({ tabName: prevState.tabName ? `${prevState.tabName} ${1}` : '1' })),
+            tabs: [
+              { name: 'Tab 1', renderAuxiliaryInfo: () => renderAuxiliaryIcon('home'), renderCallback: () => <Placeholder label="tab 1" /> }, // eslint-disable-line react/display-name
+              { name: 'Tab 2', renderCallback: () => <Placeholder label={`Tab ${state.tabName || ''}`} />, disabled: false }, // eslint-disable-line react/display-name
+              { name: 'Tab 3', renderCallback: () => <Placeholder label="tab 3" />, disabled: true }, // eslint-disable-line react/display-name
+              { name: 'Looooooooooooooooooooooooooooooooooooooong Name', renderCallback: () => <Placeholder label="tab 4" /> }, // eslint-disable-line react/display-name
+              {
+                name: 'Tab 5', renderAuxiliaryInfo: () => renderAuxiliaryIcon('info'), renderCallback: () => <Placeholder label="tab 5" />, disabled: true, // eslint-disable-line react/display-name
+              },
+            ],
+          },
+        })
+      }
+    </StateProvider>
+  );
+};
 
 export const TabView = Template.bind({});
-
-TabView.args = {
-  props: {
-    initialTabName: 'Tab 1',
-    directAccessTabs: 3,
-    tabChanged: (stateProvider: StateProvider<State>) => () => stateProvider.setState((prevState) => ({ tabName: prevState.tabName ? `${prevState.tabName} ${1}` : '1' })),
-    tabs: (stateProvider: StateProvider<State>) => [
-      { name: 'Tab 1', renderAuxiliaryInfo: () => renderAuxiliaryIcon('home'), renderCallback: () => <Placeholder label="tab 1" /> }, // eslint-disable-line react/display-name
-      { name: 'Tab 2', renderCallback: () => <Placeholder label={`Tab ${stateProvider.state.tabName}`} />, disabled: false }, // eslint-disable-line react/display-name
-      { name: 'Tab 3', renderCallback: () => <Placeholder label="tab 3" />, disabled: true }, // eslint-disable-line react/display-name
-      { name: 'Looooooooooooooooooooooooooooooooooooooong Name', renderCallback: () => <Placeholder label="tab 4" /> }, // eslint-disable-line react/display-name
-      {
-        name: 'Tab 5', renderAuxiliaryInfo: () => renderAuxiliaryIcon('info'), renderCallback: () => <Placeholder label="tab 5" />, disabled: true, // eslint-disable-line react/display-name
-      },
-    ],
-  },
-};
---
+++ pyrene/src/examples/StateProvider.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface RenderPropsReturnValue<P> {
+  component: React.ComponentType<P>,
+  props: P,
+}
+
+interface Props<P, S> {
+  initState: S;
+  children: (state: S, setState: React.Component['setState']) => RenderPropsReturnValue<P>;
+}
+
+
+class StateProvider<P, S> extends React.Component<Props<P, S>, S> {
+
+  constructor(props: Props<P, S>) {
+    super(props);
+    this.state = props.initState;
+  }
+
+  render() {
+    const setState = this.setState.bind(this);
+    const { component: Component, props } = this.props.children(this.state, setState);
+    return <Component {...props} />;
+  }
+
+}
+
+export default StateProvider;
