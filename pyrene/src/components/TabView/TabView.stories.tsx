64b2c5054866561239ebc6da1e5144ca229ac310
Sakai-san <o_edo@hotmail.com>
2021-09-02 09:58:18 +0200

    Story

--- pyrene/src/components/TabView/TabView.stories.tsx
+++ pyrene/src/components/TabView/TabView.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Story, Meta } from '@storybook/react';
 import Icon from '../Icon/Icon';
 import TabViewComponent, { TabViewProps } from './TabView';
@@ -20,21 +21,23 @@ const renderAuxiliaryIcon = (name: string) => (
   </div>
 );
 
-const Template: Story<Example<TabViewProps, State>> = (args) => <TabViewComponent {...args} />;
+const Template: Story<Example<TabViewProps, State>> = (args) => <TabViewComponent {...args as any} />;
 
 export const TabView = Template.bind({});
 
 TabView.args = {
-  initialTabName: 'Tab 1',
-  directAccessTabs: 3,
-  tabChanged: (stateProvider: StateProvider<State>) => () => stateProvider.setState((prevState) => ({ tabName: prevState.tabName ? `${prevState.tabName} ${1}` : '1' })),
-  tabs: (stateProvider: StateProvider<State>) => [
-    { name: 'Tab 1', renderAuxiliaryInfo: () => renderAuxiliaryIcon('home'), renderCallback: () => <Placeholder label="tab 1" /> }, // eslint-disable-line react/display-name
-    { name: 'Tab 2', renderCallback: () => <Placeholder label={`Tab ${stateProvider.state.tabName}`} />, disabled: false }, // eslint-disable-line react/display-name
-    { name: 'Tab 3', renderCallback: () => <Placeholder label="tab 3" />, disabled: true }, // eslint-disable-line react/display-name
-    { name: 'Looooooooooooooooooooooooooooooooooooooong Name', renderCallback: () => <Placeholder label="tab 4" /> }, // eslint-disable-line react/display-name
-    {
-      name: 'Tab 5', renderAuxiliaryInfo: () => renderAuxiliaryIcon('info'), renderCallback: () => <Placeholder label="tab 5" />, disabled: true, // eslint-disable-line react/display-name
-    },
-  ],
+  props: {
+    initialTabName: 'Tab 1',
+    directAccessTabs: 3,
+    tabChanged: (stateProvider: StateProvider<State>) => () => stateProvider.setState((prevState) => ({ tabName: prevState.tabName ? `${prevState.tabName} ${1}` : '1' })),
+    tabs: (stateProvider: StateProvider<State>) => [
+      { name: 'Tab 1', renderAuxiliaryInfo: () => renderAuxiliaryIcon('home'), renderCallback: () => <Placeholder label="tab 1" /> }, // eslint-disable-line react/display-name
+      { name: 'Tab 2', renderCallback: () => <Placeholder label={`Tab ${stateProvider.state.tabName}`} />, disabled: false }, // eslint-disable-line react/display-name
+      { name: 'Tab 3', renderCallback: () => <Placeholder label="tab 3" />, disabled: true }, // eslint-disable-line react/display-name
+      { name: 'Looooooooooooooooooooooooooooooooooooooong Name', renderCallback: () => <Placeholder label="tab 4" /> }, // eslint-disable-line react/display-name
+      {
+        name: 'Tab 5', renderAuxiliaryInfo: () => renderAuxiliaryIcon('info'), renderCallback: () => <Placeholder label="tab 5" />, disabled: true, // eslint-disable-line react/display-name
+      },
+    ],
+  },
 };
