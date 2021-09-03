92bbd708aa000ef41e23363fdc7da4ebfcb5bf8c
Sakai-san <o_edo@hotmail.com>
2021-09-03 07:49:15 +0200

    Code comment.

--- pyrene/src/examples/StateProvider.tsx
+++ pyrene/src/examples/StateProvider.tsx
@@ -10,6 +10,12 @@ interface Props<P, S> {
   children: (state: S, setState: React.Component['setState']) => RenderPropsReturnValue<P>;
 }
 
+
+/**
+ * StateProvider is a react component acting as a container passing its state as props to other components.
+ *
+ * StateProvider is needed to demo a component which needs state from parent. For example in Storybook.
+ */
 class StateProvider<P, S> extends React.Component<Props<P, S>, S> {
 
   constructor(props: Props<P, S>) {
@@ -22,7 +28,6 @@ class StateProvider<P, S> extends React.Component<Props<P, S>, S> {
     const { component: Component, props } = this.props.children(this.state, setState);
     return <Component {...props} />;
   }
-
 }
 
 export default StateProvider;
