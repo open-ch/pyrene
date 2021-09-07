9f054a5948b736029d97c270b2430b7ef3275b01
Sakai-san <o_edo@hotmail.com>
2021-09-07 08:02:17 +0200

    Destructuring.

--- pyrene/src/components/SimpleTable/SimpleTable.tsx
+++ pyrene/src/components/SimpleTable/SimpleTable.tsx
@@ -1,21 +1,66 @@
-import React from 'react';
+import React,{ FunctionComponent } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import Loader from '../Loader/Loader';
 import SimpleTableActionList from './SimpleTableActionList';
 import styles from './simpleTable.css';
 
+interface SimpleTableProps {
+  /**
+   * Allows the definition of row actions Type: [{ label: [ string ], onClick: [ function ] }, ...]
+   */
+  actions?: Array<{
+    label: string,
+    onClick: () => void,
+  }>,
+  /**
+   * Sets the Table columns.
+   * Type: [{ accessor: ( string | func ) (required), align: string, cellRenderCallback: func, headerName: string, id: string (required), width: number ]
+   */
+  columns: Array<{
+    accessor?: string | ((key: string) => string),
+    align?: string,
+    cellRenderCallback?: () => void,
+    headerName?: string,
+    id?: string,
+    width?: number,
+  }>,
+  /**
+   * Sets the Table data displayed in the rows. Type: [ JSON ]
+   */
+  data: Array<unknown>,
+  /**
+   * Disables the component and displays a loader inside of it.
+   */
+  loading?: boolean,
+  /**
+   * Called when the user clicks on a row.
+   */
+  onRowClick?: () => void,
+  /**
+   * Called when the user double clicks on a row.
+   */
+  onRowDoubleClick?: () => void,
+}
+
 /**
  * Simple Tables are used to display tabular data without the overhead of pagination, sorting and filtering.
  */
-const SimpleTable = (props) => (
+const SimpleTable: FunctionComponent<SimpleTableProps> = ({
+  actions = [],
+  columns,
+  data,
+  loading = false,
+  onRowClick,
+  onRowDoubleClick,
+}: SimpleTableProps) => (
   <div className={styles.container}>
     <table className={styles.table}>
-      {props.columns.length > 0 && props.columns.some((column) => typeof column.headerName !== 'undefined' && column.headerName !== '')
+      {columns.length > 0 && columns.some((column) => typeof column.headerName !== 'undefined' && column.headerName !== '')
         && (
           <thead className={styles.tableHeader}>
             <tr className={styles.tableHeaderRow}>
-              {props.columns.map((column) => (
+              {columns.map((column) => (
                 <th
                   className={styles.tableHeaderCell}
                   style={{ maxWidth: column.width > 0 ? `${column.width}px` : null }}
@@ -26,7 +71,7 @@ const SimpleTable = (props) => (
                   </div>
                 </th>
               ))}
-              {props.actions.length > 0 && (
+              {actions.length > 0 && (
                 <th
                   aria-label="Action"
                   className={clsx(styles.tableHeaderCell, styles.actionCell)}
@@ -37,14 +82,14 @@ const SimpleTable = (props) => (
           </thead>
         )}
       <tbody className={styles.tableBody}>
-        {!props.loading && props.data && props.data.length > 0 && props.data.map((row, rowIndex) => (
+        {!loading && data && data.length > 0 && data.map((row, rowIndex) => (
           <tr
-            className={clsx(styles.tableRow, { [styles.tableRowWithFunction]: (props.onRowClick || props.onRowDoubleClick) })}
+            className={clsx(styles.tableRow, { [styles.tableRowWithFunction]: onRowClick || onRowDoubleClick })}
             key={Object.values(row).join()}
-            onDoubleClick={() => (props.onRowDoubleClick ? props.onRowDoubleClick(row) : null)}
-            onClick={() => (props.onRowClick ? props.onRowClick(row) : null)}
+            onDoubleClick={() => (onRowDoubleClick ? onRowDoubleClick(row) : null)}
+            onClick={() => (onRowClick ? onRowClick(row) : null)}
           >
-            {props.columns.length > 0 && props.columns.map((column, columnIndex) => {
+            {columns.length > 0 && columns.map((column, columnIndex) => {
               const valueRow = row;
               valueRow.value = typeof column.accessor === 'string' ? row[column.accessor] : column.accessor(row, rowIndex, columnIndex);
               return (
@@ -59,14 +104,14 @@ const SimpleTable = (props) => (
                 </td>
               );
             })}
-            {!props.loading && props.data && props.data.length > 0 && props.actions && props.actions.length > 0 && (
+            {!loading && data && data.length > 0 && actions && actions.length > 0 && (
               <td
                 className={clsx(styles.tableCell, styles.actionCell)}
                 key={`action-${Object.values(row).join('-')}`}
               >
                 <SimpleTableActionList
                   row={Object.assign(row, { key: row.key ? row.key.toString() : '' })}
-                  actions={props.actions}
+                  actions={actions}
                 />
               </td>
             )}
@@ -74,12 +119,12 @@ const SimpleTable = (props) => (
         ))}
       </tbody>
     </table>
-    {props.loading && (
+    {loading && (
       <div className={styles.loader}>
         <Loader type="inline" />
       </div>
     )}
-    {!props.loading && (!props.data || !(props.data.length > 0)) && (
+    {!loading && (!data || !(data.length > 0)) && (
       <div className={styles.noData}>
         No data found.
       </div>
