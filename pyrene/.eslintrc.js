76409ed656583dce9a095c7a5db805d5e7845b81
Sakai-san <o_edo@hotmail.com>
2021-09-09 09:39:01 +0200

    Fix linting

--- pyrene/.eslintrc.js
+++ pyrene/.eslintrc.js
@@ -1,19 +1,25 @@
-{
-  "extends": "@osag/eslint-config",
-  "env": {
-    "browser": true
+const path = require('path');
+
+module.exports = {
+  extends: '@osag/eslint-config',
+  env: {
+    browser: true,
   },
-  "parserOptions": {
-    "sourceType": "module"
+  parserOptions: {
+    sourceType: 'module',
+    project: path.resolve(__dirname, 'tsconfig.json'),
+    babelOptions: {
+      configFile: path.resolve(__dirname, '.babelrc'),
+    },
   },
-  "rules": {
-    "no-restricted-imports": [
-      "error",
-      "jquery"
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      'jquery',
     ],
-    "react/jsx-props-no-spreading": "off", 
-    "react/forbid-prop-types": 0, 
-    "no-multiple-empty-lines": 0,
-    "@typescript-eslint/unbound-method": 0
-  } 
-}
+    'react/jsx-props-no-spreading': 'off',
+    'react/forbid-prop-types': 0,
+    'no-multiple-empty-lines': 0,
+    '@typescript-eslint/unbound-method': 0,
+  },
+};
