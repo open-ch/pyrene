8bcefac20482124402c8f1018b444dc6c3bbbe89
Sakai-san <o_edo@hotmail.com>
2021-09-14 13:13:55 +0200

    Typing.

--- pyrene/src/components/TimeRangeSelector/TimeRangeNavigationBar/ArrowSelector/TRSStepper/TRSStepper.tsx
+++ pyrene/src/components/TimeRangeSelector/TimeRangeNavigationBar/ArrowSelector/TRSStepper/TRSStepper.tsx
@@ -1,59 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { FunctionComponent } from 'react';
 import clsx from 'clsx';
-
 import styles from './trsStepper.css';
+
+export interface TRSStepperProps {
+  /**
+   * Sets the direction of the arrow.
+   */
+  direction?: 'right' | 'left',
+  /**
+   * Disables any interaction with the component.
+   */
+  disabled?: boolean,
+  /**
+   * The component is inactive due to constraints.
+   */
+  inactive?: boolean,
+  /**
+   * Javascript event handler.
+   */
+  onClick: () => void,
+}
 
 /**
  * Custom steppers for the TRS used for previous/next navigation.
  */
-const TRSStepper = (props) => {
-  const capitalisedDirection = props.direction.charAt(0).toUpperCase() + props.direction.slice(1);
+const TRSStepper: FunctionComponent<TRSStepperProps> = ({
+  direction = 'left',
+  disabled = false,
+  inactive = false,
+  onClick,
+}: TRSStepperProps) => {
+  const capitalisedDirection = direction.charAt(0).toUpperCase() + direction.slice(1);
   const iconName = `pyreneIcon-chevron${capitalisedDirection}`;
   return (
     <button
       type="button"
       className={clsx('unSelectable',
         styles.stepper,
-        { [styles.disabled]: props.disabled },
-        { [styles.right]: props.direction === 'right' },
-        { [styles.left]: props.direction === 'left' })}
-      onClick={props.onClick}
-      disabled={props.disabled}
+        { [styles.disabled]: disabled },
+        { [styles.right]: direction === 'right' },
+        { [styles.left]: direction === 'left' })}
+      onClick={onClick}
+      disabled={disabled}
     >
       <span className={clsx(iconName,
         styles.stepperIcon,
-        { [styles.disabledIcon]: props.inactive })}
+        { [styles.disabledIcon]: inactive })}
       />
     </button>
   );
-};
-
-TRSStepper.displayName = 'TRSStepper';
-
-TRSStepper.defaultProps = {
-  direction: 'left',
-  disabled: false,
-  inactive: false,
-};
-
-TRSStepper.propTypes = {
-  /**
-   * Sets the direction of the arrow.
-   */
-  direction: PropTypes.oneOf(['right', 'left']),
-  /**
-   * Disables any interaction with the component.
-   */
-  disabled: PropTypes.bool,
-  /**
-   * The component is inactive due to constraints.
-   */
-  inactive: PropTypes.bool,
-  /**
-   * Javascript event handler.
-   */
-  onClick: PropTypes.func.isRequired,
 };
 
 export default TRSStepper;
