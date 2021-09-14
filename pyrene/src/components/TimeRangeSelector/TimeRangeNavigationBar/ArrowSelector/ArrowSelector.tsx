94ae0c83c52de2f03325a9428e5da9c73c8f3be4
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2021-09-14 16:46:16 +0200

    arrow selector ts [improvement] (#166)
    * jsx to tsx
    
    * Typing.
    
    * Typing.
    
    Former-commit-id: bbcee73c7557602713dda297ff47a3122a42dba1

---
+++ pyrene/src/components/TimeRangeSelector/TimeRangeNavigationBar/ArrowSelector/ArrowSelector.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/prop-types */
+import React, { FunctionComponent } from 'react';
+import clsx from 'clsx';
+import TRSStepper from './TRSStepper/TRSStepper';
+import styles from './arrowSelector.css';
+
+interface ArrowSelectorProps {
+  backInactive: boolean,
+  disabled?: boolean,
+  forwardInactive: boolean,
+  innerWidth: number,
+  label: string,
+  onNavigateBack: () => void,
+  onNavigateForward: () => void,
+}
+
+const ArrowSelector: FunctionComponent<ArrowSelectorProps> = ({
+  disabled = false,
+  backInactive,
+  forwardInactive,
+  innerWidth,
+  label,
+  onNavigateBack,
+  onNavigateForward,
+}) => (
+  <div className={styles.arrowSelector}>
+    <TRSStepper
+      direction="left"
+      disabled={disabled}
+      inactive={backInactive}
+      onClick={(disabled || backInactive) ? () => {} : onNavigateBack}
+    />
+    <div className={styles.contentOuter}>
+      <div className={styles.contentInner} style={{ width: innerWidth }}>
+        <div className={clsx(styles.value, { [styles.disabled]: disabled })}>
+          {label}
+        </div>
+      </div>
+    </div>
+    <TRSStepper
+      direction="right"
+      disabled={disabled}
+      inactive={forwardInactive}
+      onClick={(disabled || forwardInactive) ? () => {} : onNavigateForward}
+    />
+  </div>
+);
+
+export default ArrowSelector;
