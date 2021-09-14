63bd794107f3ec7a5e423f3642ba0c65c49a7077
Sakai-san <o_edo@hotmail.com>
2021-09-14 10:45:22 +0200

    Function component

--- pyrene/src/components/TimeRangeSelector/TimeRangeNavigationBar/TimeRangeNavigationBar.tsx
+++ pyrene/src/components/TimeRangeSelector/TimeRangeNavigationBar/TimeRangeNavigationBar.tsx
@@ -1,33 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-import { differenceInMinutes, getTime, format } from 'date-fns';
-import { utcToZonedTime } from 'date-fns-tz';
-
-
+import React, { FunctionComponent } from 'react';
+import differenceInMinutes from 'date-fns/differenceInMinutes';
+import getTime from 'date-fns/getTime';
+import format from 'date-fns/format';
+import utcToZonedTime from 'date-fns-tz/utcToZonedTime';
 import ArrowSelector from './ArrowSelector/ArrowSelector';
 
-const TimeRangeNavigationBar = (props) => {
-  // We should not check for milliseconds but minutes changes
-  const backInactive = differenceInMinutes(props.from, props.lowerBound) <= 0;
+interface TimeRangeNavigationBarProps {
+  disabled?: boolean,
+  from: number,
+  lowerBound: number,
+  onNavigateBack: () => void,
+  onNavigateForward: () => void,
+  timezone: string,
+  to: number,
+  upperBound: number,
+}
 
-  // We should not check for milliseconds but minutes changes
-  const forwardInactive = differenceInMinutes(props.to, props.upperBound) >= 0;
-
-  return (
-    <ArrowSelector
-      label={TimeRangeNavigationBar.renderCurrentTimeRange(props.from, props.to, props.timezone)}
-      onNavigateForward={props.onNavigateForward}
-      backInactive={backInactive}
-      forwardInactive={forwardInactive}
-      onNavigateBack={props.onNavigateBack}
-      disabled={props.disabled}
-      innerWidth={248}
-    />
-  );
-};
-
-TimeRangeNavigationBar.renderCurrentTimeRange = (from, to, timezone) => {
+const renderCurrentTimeRange = (from: number, to: number, timezone: string) => {
   const localFrom = getTime(utcToZonedTime(from, timezone));
   const localTo = getTime(utcToZonedTime(to, timezone));
   const pattern = 'dd.MM.yyyy, HH:mm';
@@ -35,19 +24,33 @@ TimeRangeNavigationBar.renderCurrentTimeRange = (from, to, timezone) => {
   return `${format(localFrom, pattern)} - ${format(localTo, pattern)}`;
 };
 
-TimeRangeNavigationBar.defaultProps = {
-  disabled: false,
-};
+const TimeRangeNavigationBar: FunctionComponent<TimeRangeNavigationBarProps> = ({
+  disabled = false,
+  from,
+  lowerBound,
+  onNavigateBack,
+  onNavigateForward,
+  timezone,
+  to,
+  upperBound,
+}) => {
+  // We should not check for milliseconds but minutes changes
+  const backInactive = differenceInMinutes(from, lowerBound) <= 0;
 
-TimeRangeNavigationBar.propTypes = {
-  disabled: PropTypes.bool,
-  from: PropTypes.number.isRequired,
-  lowerBound: PropTypes.number.isRequired,
-  onNavigateBack: PropTypes.func.isRequired,
-  onNavigateForward: PropTypes.func.isRequired,
-  timezone: PropTypes.string.isRequired,
-  to: PropTypes.number.isRequired,
-  upperBound: PropTypes.number.isRequired,
+  // We should not check for milliseconds but minutes changes
+  const forwardInactive = differenceInMinutes(to, upperBound) >= 0;
+
+  return (
+    <ArrowSelector
+      label={renderCurrentTimeRange(from, to, timezone)}
+      onNavigateForward={onNavigateForward}
+      backInactive={backInactive}
+      forwardInactive={forwardInactive}
+      onNavigateBack={onNavigateBack}
+      disabled={disabled}
+      innerWidth={248}
+    />
+  );
 };
 
 export default TimeRangeNavigationBar;
