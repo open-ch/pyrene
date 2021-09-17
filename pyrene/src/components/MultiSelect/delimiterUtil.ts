c4dee0328c32e71ab51525e33c862f12937b7036
Sakai-san <o_edo@hotmail.com>
2021-09-17 15:25:19 +0200

    Code comment.

--- pyrene/src/components/MultiSelect/delimiterUtil.ts
+++ pyrene/src/components/MultiSelect/delimiterUtil.ts
@@ -3,6 +3,8 @@ const DEFAULT_DELIMITERS = ['\n', '\t'];
 
 /**
  * Creates a unique array of string case insensitively.
+ * @param {string[]} words - array of string values
+ * @returns {string[]}
  */
 export const getCaseInsensitiveDistinctValues = (words: string[]) => words.reduce((result, el) => {
   if (result.every((otherEl) => otherEl.toLowerCase() !== el.toLowerCase())) {
@@ -14,7 +16,7 @@ export const getCaseInsensitiveDistinctValues = (words: string[]) => words.reduc
 
 /**
  * Get delimited string values from a string.
- * @param {string} string - the raw string containing delimiter symbols
+ * @param {string} text - the raw string containing delimiter symbols
  * @returns {string[]}
  */
 export const getDelimitedValues = (text: string) => {
