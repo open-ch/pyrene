bd68e22ffba5db15adcd1a899765fbb4c8cbde31
Sakai-san <o_edo@hotmail.com>
2021-09-17 12:31:55 +0200

    Typing.

--- pyrene/src/components/MultiSelect/MultiSelectMenuWithOptions.tsx
+++ pyrene/src/components/MultiSelect/MultiSelectMenuWithOptions.tsx
@@ -1,33 +1,30 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React, { FunctionComponent } from 'react';
 import clsx from 'clsx';
 import { components, MenuProps } from 'react-select';
 import { Option } from './types';
 import styles from './multiSelectMenuWithOptions.css';
 
-/* eslint-disable react/prop-types, react/jsx-props-no-spreading */
-/* props are controlled by the parent component of react-select */
-const MultiSelectMenuWithOptions: FunctionComponent<MenuProps<Option, boolean>> = (props) => {
-  console.log('props', props);
-  return (
-    <components.Menu {...props}>
-      <>
-        {props.getValue().length > 0 && (
-          <div className={styles.selectMenuWithOptions}>
-            {props.getValue().map((option) => (
-              <div className={clsx(styles.selectOption, { [styles.invalid]: option.invalid })} key={option.value}>
-                <div className={styles.optionLabel}>{option.label}</div>
-                <div className={styles.clearBox} onClick={() => props.setValue(props.getValue().filter((e) => e !== option))}>
-                  <div className={clsx(styles.clearIcon, 'pyreneIcon-delete')} />
-                </div>
+const MultiSelectMenuWithOptions: FunctionComponent<MenuProps<Option, boolean>> = (props) => (
+  <components.Menu {...props}>
+    <>
+      {props.getValue().length > 0 && (
+        <div className={styles.selectMenuWithOptions}>
+          {props.getValue().map((option) => (
+            <div className={clsx(styles.selectOption, { [styles.invalid]: option.invalid })} key={option.value}>
+              <div className={styles.optionLabel}>{option.label}</div>
+              <div className={styles.clearBox} onClick={() => props.setValue(props.getValue().filter((e) => e !== option), 'clear')}>
+                <div className={clsx(styles.clearIcon, 'pyreneIcon-delete')} />
               </div>
-            ))}
-          </div>
-        )}
-        {props.children}
-      </>
-    </components.Menu>
-  );
-}
+            </div>
+          ))}
+        </div>
+      )}
+      {props.children}
+    </>
+  </components.Menu>
+);
 
 
 MultiSelectMenuWithOptions.displayName = 'MultiSelectMenuWithOptions';
