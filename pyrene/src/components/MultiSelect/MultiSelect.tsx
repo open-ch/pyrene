fcd4220a3d693551f0bb215bbc14e5e9a75dacdd
Sakai-san <o_edo@hotmail.com>
2021-09-20 08:58:51 +0200

    Remove proptype.

--- pyrene/src/components/MultiSelect/MultiSelect.tsx
+++ pyrene/src/components/MultiSelect/MultiSelect.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import Select from 'react-select';
 import CreatableSelect from 'react-select/creatable';
