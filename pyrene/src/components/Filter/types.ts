aa873ff9790057f988d0d69c5738ad49f10577a6
Sakai-san <o_edo@hotmail.com>
2021-09-23 13:55:18 +0200

    Cleanup.

--- pyrene/src/components/Filter/types.ts
+++ pyrene/src/components/Filter/types.ts
@@ -2,13 +2,12 @@ import { SingleSelectProps } from '../SingleSelect/SingleSelect';
 import { Option } from '../MultiSelect/types';
 
 export type MultiSelectOption = Option;
+export type SingleSelectOption = SingleSelectProps<unknown>['options'];
 
 export type SingleSelectValue = SingleSelectProps<unknown>['value'];
 export type MultiselectValue = Array<MultiSelectOption>;
 export type TextFieldValue = string;
 export type InputValue = MultiselectValue | SingleSelectValue | TextFieldValue;
-
-export type SingleSelectOption = SingleSelectProps<unknown>['options'];
 
 export type Filter = {
   id: string,
@@ -19,10 +18,10 @@ export type Filter = {
   type: 'singleSelect' | 'multiSelect' | 'text',
 };
 
-export type HandleFilterChange = (option: any, negated: boolean, id: string) => void;
-
 export type FilterValue = Array<MultiSelectOption> | MultiSelectOption | TextFieldValue;
 
 export type FilterValues = {
   [key: string]: FilterValue
 };
+
+export type HandleFilterChange = (option: any, negated: boolean, id: Filter['id']) => void;
