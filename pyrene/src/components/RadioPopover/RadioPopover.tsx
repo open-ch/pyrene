eec928dd5938c75968526bbdab9a57a08890406c
Sakai-san <o_edo@hotmail.com>
2021-09-29 10:58:12 +0200

    Typing.

--- pyrene/src/components/RadioPopover/RadioPopover.tsx
+++ pyrene/src/components/RadioPopover/RadioPopover.tsx
@@ -1,110 +1,95 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+/* eslint-disable react/require-default-props */
+import React, { FunctionComponent, useState } from 'react';
 import clsx from 'clsx';
 
 import styles from './radioPopover.css';
 import Popover from '../Popover/Popover';
 import OptionList from './OptionList';
 
-
-export default class RadioPopover extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayPopover: false,
-    };
-  }
-
-  togglePopover = () => this.setState((prevState) => ({
-    displayPopover: !prevState.displayPopover,
-  }));
-
-  closePopover = () => this.setState({ displayPopover: false });
-
-  render() {
-    const {
-      options,
-      onChange,
-      renderHelpSection,
-      renderLabel,
-      value,
-    } = this.props;
-    const { displayPopover } = this.state;
-
-    const selectedValue = options.find((option) => option.value === value);
-
-    return (
-      <div className={styles.radioPopover}>
-        <Popover
-          preferredPosition={['bottom']}
-          align="end"
-          displayPopover={displayPopover}
-          distanceToTarget={8}
-          onClickOutside={this.closePopover}
-          renderPopoverContent={() => (
-            <OptionList
-              options={options}
-              onChange={(newValue) => {
-                onChange(newValue);
-                this.closePopover();
-              }}
-              renderHelpSection={renderHelpSection}
-              selectedValue={selectedValue}
-            />
-          )}
-        >
-          <div
-            className={clsx(styles.popoverTriggerButton, {
-              [styles.popoverOpen]: this.state.displayPopover,
-            })}
-            onClick={this.togglePopover}
-          >
-            <div className={clsx(styles.buttonLabel, 'unSelectable')}>
-              {renderLabel(selectedValue)}
-            </div>
-            <div
-              className={clsx(styles.arrowIcon, { 'pyreneIcon-chevronUp': this.state.displayPopover, 'pyreneIcon-chevronDown': !this.state.displayPopover })}
-            />
-          </div>
-        </Popover>
-      </div>
-    );
-  }
-
+interface Option {
+  label: string,
+  value: number | string,
 }
 
-RadioPopover.displayName = 'Radio Popover';
-
-RadioPopover.defaultProps = {
-  options: [],
-  renderHelpSection: null,
-  renderLabel: (value) => value && value.label,
-  value: null,
-};
-
-RadioPopover.propTypes = {
+interface RadioPopoverProps {
   /**
    * Sets the selected choice of the user.
    */
-  onChange: PropTypes.func.isRequired,
+  onChange: (option: Option) => void,
   /**
    * Set the values that the user can choose from.
    */
-  options: PropTypes.arrayOf(PropTypes.shape({
-    label: PropTypes.string.isRequired,
-    value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-  })),
+  options?: Array<Option>,
   /**
    * Render callback for the help section above the options
    */
-  renderHelpSection: PropTypes.func,
+  renderHelpSection?: () => string | JSX.Element,
   /**
    * Render callback for the label for custom formatting
    */
-  renderLabel: PropTypes.func,
+  renderLabel?: (option: Option) => string,
   /**
    * selected value - should match the `value` key in one of the `options`
    */
-  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  value?: number | string | null,
+}
+
+const RadioPopover: FunctionComponent<RadioPopoverProps> = ({
+  onChange,
+  renderHelpSection,
+  options = [],
+  renderLabel = (value) => value && value.label,
+  value = null,
+}: RadioPopoverProps) => {
+  const [displayPopover, setDisplayPopover] = useState(false);
+
+  const togglePopover = () => setDisplayPopover((prevDisplayPopover) => !prevDisplayPopover);
+
+  const closePopover = () => setDisplayPopover(false);
+
+  const selectedValue = options.find((option) => option.value === value);
+
+  return (
+    <div className={styles.radioPopover}>
+      <Popover
+        preferredPosition={['bottom']}
+        align="end"
+        displayPopover={displayPopover}
+        distanceToTarget={8}
+        onClickOutside={closePopover}
+        renderPopoverContent={() => (
+          <OptionList
+            options={options}
+            onChange={(newValue) => {
+              onChange(newValue);
+              closePopover();
+            }}
+            renderHelpSection={renderHelpSection}
+            selectedValue={selectedValue}
+          />
+        )}
+      >
+        <div
+          className={clsx(styles.popoverTriggerButton, {
+            [styles.popoverOpen]: displayPopover,
+          })}
+          onClick={togglePopover}
+        >
+          <div className={clsx(styles.buttonLabel, 'unSelectable')}>
+            {selectedValue && renderLabel(selectedValue)}
+          </div>
+          <div
+            className={clsx(styles.arrowIcon, {
+              'pyreneIcon-chevronUp': displayPopover,
+              'pyreneIcon-chevronDown': !displayPopover,
+            })}
+          />
+        </div>
+      </Popover>
+    </div>
+  );
 };
+
+RadioPopover.displayName = 'Radio Popover';
+
+export default RadioPopover;
