da6293ac914e4b8baa0460abeab678defb44510e
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2021-09-30 10:29:48 +0200

    textfield ts [improvement] (#187)
    * jsx to tsx.
    
    * Typing.
    
    * cleanup.
    
    * Code formatting.
    
    * Typing.
    
    * jsx to tsx
    
    * Typing.
    
    * Code formatting.
    
    * Remove useless react fragment.

---
+++ pyrene/src/components/TextField/TextField.examples.tsx
@@ -0,0 +1,20 @@
+import { TextFieldProps } from './TextField';
+import { Example, StateProvider } from '../../examples/Example';
+
+interface State {
+  value: string;
+}
+
+const examples: Example<TextFieldProps, State> = {
+  props: {
+    title: 'Field Label',
+    placeholder: 'Placeholder Text',
+    helperLabel: 'Helper text for instructions',
+    width: 500,
+    value: (stateProvider) => stateProvider.state.value,
+    onChange: (stateProvider: StateProvider<State>): TextFieldProps['onChange'] => (value) => stateProvider.setState({ value }),
+  },
+  category: 'Form',
+};
+
+export default examples;
