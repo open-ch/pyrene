f7a48d169558ee65a898109ca1bd3b71163ea4f0
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2021-09-30 10:29:48 +0200

    textfield ts [improvement] (#187)
    * jsx to tsx.
    
    * Typing.
    
    * cleanup.
    
    * Code formatting.
    
    * Typing.
    
    * jsx to tsx
    
    * Typing.
    
    * Code formatting.
    
    * Remove useless react fragment.
    
    Former-commit-id: 221a86deb54d5f905d591bd14e5b1e3e16a6b5ed

---
+++ pyrene/src/components/TextField/TextField.tsx
@@ -0,0 +1,156 @@
+/* eslint-disable react/require-default-props */
+import React, { FunctionComponent, ChangeEvent } from 'react';
+import clsx from 'clsx';
+import styles from './textField.css';
+
+export interface TextFieldProps {
+  /**
+   * Focus an element when it mounts.
+   */
+  autoFocus?: boolean;
+  /**
+   * Disables any interaction with the component.
+   */
+  disabled?: boolean;
+  /**
+   * Sets a label below the input field to display additional information for the user.
+   */
+  helperLabel?: string;
+  /**
+   * Sets the visual appearance, to signal that the input is invalid.
+   */
+  invalid?: boolean;
+  /**
+   * Sets the label displayed instead of the helperLabel when the input is invalid.
+   */
+  invalidLabel?: string;
+  /**
+   * Sets the html name property of the form element.
+   */
+  name?: string;
+  /**
+   * Javascript event handler.
+   */
+  onBlur?: () => void;
+  /**
+   * Called when hitting Escape.
+   */
+  onCancel?: () => void;
+  /**
+   * Javascript event handler.
+   */
+  onChange?: (value: string, event: ChangeEvent<HTMLInputElement>) => void;
+  /**
+   * Javascript event handler.
+   */
+  onFocus?: () => void;
+  /**
+   * Called when hitting enter
+   */
+  onSubmit?: (value: string) => void;
+  /**
+   * Sets the placeholder label.
+   */
+  placeholder?: string;
+  /**
+   * Adds a visual indication to display that the field is required.
+   */
+  required?: boolean;
+  /**
+   * Applies type 'password' to the input field, i.e. entered text will be obscured.
+   */
+  secret?: boolean;
+  /**
+   * Sets the title above the input field.
+   */
+  title?: string;
+  /**
+   * Sets the value of the input field.
+   */
+  value?: string;
+  /**
+   * Sets a fixed width (px) for the input field.
+   */
+  width?: number;
+}
+
+/**
+ * A text field allows the user to enter and edit text or numeric values in one line.
+ * Use text fields in forms to allow enter, select, and search for text.
+ * Text fields are typically found within a form but can also be part of a modal, dialog, search etc.
+ *
+ * Don’t use the text field to enter dates and times.
+ * In this case, use the date picker, date range selection, or date/time picker. For entering long texts use the textarea component.
+ */
+const TextField: FunctionComponent<TextFieldProps> = ({
+  title = '',
+  value = '',
+  placeholder = '',
+  helperLabel = '',
+  invalidLabel = '',
+  name = '',
+  width = -1,
+  required = false,
+  secret = false,
+  disabled = false,
+  invalid = false,
+  onBlur,
+  onCancel,
+  onChange,
+  onFocus,
+  onSubmit,
+  autoFocus = false,
+}: TextFieldProps) => (
+  <div
+    className={clsx(styles.textFieldContainer, {
+      [styles.disabled]: disabled,
+      [styles.invalid]: invalid && !disabled,
+    })}
+    style={{ width: width >= 0 ? `${width}px` : '100%' }}
+  >
+    {title && (
+      <div
+        className={clsx(styles.textFieldTitle, {
+          [styles.required]: required && !disabled,
+        })}
+      >
+        {title}
+      </div>
+    )}
+    <div className={styles.textFieldIconLayoutContainer}>
+      <input
+        className={clsx(styles.textField, { [styles.filled]: value })}
+        type={secret ? 'password' : 'text'}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={(event) => onChange?.(event.target.value, event)}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter' && onSubmit) {
+            onSubmit(value);
+            return;
+          }
+
+          if (event.keyCode === 27 && onCancel) {
+            onCancel();
+          }
+        }}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        autoFocus={autoFocus} // eslint-disable-line jsx-a11y/no-autofocus
+      />
+      {/* Future use of an api with predefined icons - <span className={clsx(`pyreneIcon-${props.icon}`, styles.textFieldIcon)} /> */}
+    </div>
+
+    {invalid && invalidLabel && !disabled ? (
+      <div className={styles.invalidLabel}>
+        <span className={clsx('pyreneIcon-errorOutline', styles.errorIcon)} />
+        {invalidLabel}
+      </div>
+    ) : helperLabel && <div className={styles.textFieldHelper}>{helperLabel}</div>}
+  </div>
+);
+
+TextField.displayName = 'Textfield';
+
+export default TextField;
