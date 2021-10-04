2add955d169fa967852100b4fc93aef9ba1da23e
Sakai-san <o_edo@hotmail.com>
2021-10-04 11:46:14 +0200

    Typing.

--- pyrene/src/components/Table/TableHeader/TableHeaderCell.tsx
+++ pyrene/src/components/Table/TableHeader/TableHeaderCell.tsx
@@ -1,53 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+/* eslint-disable react/prop-types */
+import React, {
+  ReactNode, FunctionComponent, CSSProperties, MouseEvent
+} from 'react';
 import clsx from 'clsx';
-
-// @ts-ignore
 import DefaultSort from '../images/sort.svg';
-// @ts-ignore
 import AscSort from '../images/sortDown.svg';
-// @ts-ignore
 import DescSort from '../images/sortUp.svg';
 
 import styles from './tableHeaderCell.css';
 
-const stringContains = (haystack, needle) => haystack.indexOf(needle) !== -1;
+interface TableHeaderCellProps {
+  children: ReactNode,
+  className?: string,
+  multiSelect?: boolean,
+  style?: CSSProperties,
+  toggleSort: (e: MouseEvent<HTMLDivElement>) => void,
+}
 
-const getIconComponent = (className) => {
-  if (stringContains(className, 'asc')) {
+const getIconComponent = (className: string) => {
+  if (className.indexOf('asc') !== -1) {
     return <AscSort />;
   }
-  if (stringContains(className, 'desc')) {
+  if (className.indexOf('desc') !== -1) {
     return <DescSort />;
   }
   return <DefaultSort />;
 };
 
-const TableHeaderCell = (props) => (
+const TableHeaderCell: FunctionComponent<TableHeaderCellProps> = ({
+  children,
+  className,
+  multiSelect = false,
+  style,
+  toggleSort,
+}) => (
   <div
-    onClick={(event) => props.toggleSort(event)}
-    className={clsx({ [styles.multiSelect]: props.multiSelect }, styles.tableHeaderCell, props.className, 'unSelectable')}
-    style={props.style}
+    onClick={(event) => toggleSort(event)}
+    className={clsx({ [styles.multiSelect]: multiSelect }, styles.tableHeaderCell, className, 'unSelectable')}
+    style={style}
   >
-    {props.children}
-    {props.className && getIconComponent(props.className)}
+    {children}
+    {className && getIconComponent(className)}
   </div>
 );
-
-TableHeaderCell.displayName = 'TableHeaderCell';
-
-TableHeaderCell.defaultProps = {
-  className: null,
-  style: null,
-  multiSelect: false,
-};
-
-TableHeaderCell.propTypes = {
-  children: PropTypes.node.isRequired,
-  className: PropTypes.string,
-  multiSelect: PropTypes.bool,
-  style: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-  toggleSort: PropTypes.func.isRequired,
-};
 
 export default TableHeaderCell;
