bbbeb7364fd85e0154024512dab252a15a1e9fdd
Sakai-san <o_edo@hotmail.com>
2021-10-04 22:36:30 +0200

    Typing.

--- pyrene/src/components/TreeTable/TreeTableCell/TreeTableCell.tsx
+++ pyrene/src/components/TreeTable/TreeTableCell/TreeTableCell.tsx
@@ -1,85 +1,80 @@
-import React, { FunctionComponent, CSSProperties } from 'react';
-import PropTypes from 'prop-types';
+/* eslint-disable react/require-default-props */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React, { CSSProperties } from 'react';
 import clsx from 'clsx';
-import TreeTablePropTypes from '../TreeTablePropTypes';
 
 import styles from './treeTableCell.css';
 
-type AllowedValues = string | number | Array<any> | boolean | Function;
+type AllowedValues = string | number | boolean;
 
-interface Column {
-  accessor: string,
-  cellStyle?: object,
+type Row<R> = R & {
+  _getParent: () => string,
+  _rowId: string,
+  _treeDepth: number,
+};
+
+type RowData<R> = Row<R> & {
+  children: Array<Row<R>>
+};
+
+interface Column<R> {
+  accessor: keyof R,
+  cellStyle?: any,
   headerName?: string,
-  headerStyle?: object,
+  headerStyle?: any,
   initiallyHidden?: boolean,
   width?: number,
-  renderCallback?: (value: AllowedValues, rowData: Array<AllowedValues>) => JSX.Element,
+  renderCallback?: (value?: AllowedValues, rowData?: RowData<R>) => JSX.Element,
 }
 
-interface TreeTableCellProps {
-  columnProps: Column,
+interface TreeTableCellProps<R> {
+  columnProps: Column<R>,
   firstColumn?: boolean,
   onExpandClick: () => void,
   parent?: boolean,
-  rowData?: Array<AllowedValues>,
+  rowData?: RowData<R>,
   sectionOpen?: boolean,
   style?: CSSProperties,
   value?: AllowedValues,
 }
 
-const TreeTableCell: FunctionComponent<TreeTableCellProps> = ({
+function TreeTableCell <R extends object>({
   onExpandClick,
   value = '',
-  columnProps = {},
+  columnProps,
   parent = false,
   sectionOpen = false,
   firstColumn = false,
-  style = {},
-  rowData = {},
-}) => (
-  <div style={style} className={styles.treeTableCell}>
+  style,
+  rowData,
+}: TreeTableCellProps<R>) {
+  return (
+    <div style={style} className={styles.treeTableCell}>
 
-    {firstColumn && parent
-      ? <div
-          className={clsx(styles.pivotIcon, { [styles.sectionOpen]: sectionOpen }, 'pyreneIcon-chevronDown')}
-          onClick={onExpandClick}
-        />
-      : <div className={styles.iconSpaceholder} />
-    }
+      {firstColumn && parent
+        ? (
+          <div
+            className={clsx(styles.pivotIcon, { [styles.sectionOpen]: sectionOpen }, 'pyreneIcon-chevronDown')}
+            onClick={onExpandClick}
+          />
+        )
+        : <div className={styles.iconSpaceholder} />}
 
-    {/* Use renderCallback if there is one defined for this column */}
+      {/* Use renderCallback if there is one defined for this column */}
 
-    {columnProps.renderCallback
-      ? columnProps.renderCallback(value, rowData)
-      : (
-        <div className={styles.cellDataContainer} title={value}>
-          {value}
-        </div>
-      )}
-  </div>
-);
+      {columnProps.renderCallback
+        ? columnProps.renderCallback(value, rowData)
+        : (
+          <div className={styles.cellDataContainer} title={`${value}`}>
+            {value}
+          </div>
+        )}
+    </div>
+  );
+}
 
+export default TreeTableCell;
 
 TreeTableCell.displayName = 'TreeTableCell';
-
-TreeTableCell.defaultProps = {
-  value: '',
-  columnProps: {},
-  parent: false,
-  sectionOpen: false,
-  firstColumn: false,
-  style: {},
-  rowData: {},
-};
-
-TreeTableCell.propTypes = {
-  columnProps: TreeTablePropTypes.COLUMN,
-  firstColumn: PropTypes.bool,
-  onExpandClick: PropTypes.func.isRequired,
-  parent: PropTypes.bool,
-  rowData: TreeTablePropTypes.DATAOBJECT,
-  sectionOpen: PropTypes.bool,
-  style: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number])),
-  value: PropTypes.oneOfType(TreeTablePropTypes.ALLOWED_VALUES),
-};
