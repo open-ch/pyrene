f08ac5bb8d574e09f470753f0e56688b5555a5f9
Sakai-san <o_edo@hotmail.com>
2021-10-04 10:48:49 +0200

    Typing.

--- pyrene/src/components/Table/TableCell/TableCell.tsx
+++ pyrene/src/components/Table/TableCell/TableCell.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+/* eslint-disable react/prop-types */
+import React, { CSSProperties, ReactNode, FunctionComponent } from 'react';
 import clsx from 'clsx';
 
 import styles from './tableCell.css';
 
-const TableCell = (props) => (
-  <div className={clsx(styles.tableCell, 'rt-td')} role="gridcell" style={props.style} onClick={props.onClick}>
-    <div className={clsx(styles.tableData, { [styles.multiSelect]: props.multiSelect })} style={{ overflow: props.style.overflow }}>{props.children}</div>
+export interface TableCellProps {
+  children: ReactNode,
+  multiSelect: boolean,
+  onClick: () => void,
+  style?: CSSProperties
+}
+
+const TableCell: FunctionComponent<TableCellProps> = ({
+  children,
+  multiSelect = false,
+  onClick,
+  style,
+}) => (
+  <div className={clsx(styles.tableCell, 'rt-td')} role="gridcell" style={style} onClick={onClick}>
+    <div
+      className={clsx(styles.tableData, { [styles.multiSelect]: multiSelect })}
+      style={{ overflow: style?.overflow }}
+    >
+      {children}
+    </div>
   </div>
 );
-
-
-TableCell.displayName = 'TableCell';
-
-TableCell.defaultProps = {
-  children: null,
-  style: null,
-  multiSelect: false,
-};
-
-TableCell.propTypes = {
-  children: PropTypes.node,
-  multiSelect: PropTypes.bool,
-  onClick: PropTypes.func.isRequired,
-  style: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-};
 
 export default TableCell;
