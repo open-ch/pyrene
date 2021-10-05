e8ad5da2c65bede8083291632360503b04b491b0
Sakai-san <o_edo@hotmail.com>
2021-10-05 11:55:06 +0200

    Config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
-    '../pyrene/src/**/*.stories.mdx',
-    '../pyrene/src/**/*.stories.@(js|jsx|ts|tsx)'
+    '../**/src/**/*.stories.mdx',
+    '../**/src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: [
     '@storybook/addon-links',
