c55fb9069cee01fc3cddbcbb1d728168a68da52e
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2021-10-05 11:04:29 +0200

    table ts [improvement] (#194)
    * jsx to tsx.
    
    * Typing.
    
    * jsx to tsx.
    
    * Typing.
    
    * Linting.
    
    * Typing.
    
    * Typing.
    
    Former-commit-id: 574e13102a5955b6336466a38458abdee75b6512

---
+++ pyrene/src/components/Table/TableHeader/TableHeader.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import React, { ReactNode, FunctionComponent } from 'react';
+import clsx from 'clsx';
+
+import styles from './tableHeader.css';
+
+interface TableHeaderProps {
+  children: ReactNode,
+  disabled?: boolean
+}
+
+const TableHeader: FunctionComponent<TableHeaderProps> = ({ children, disabled = false }) => (
+  <div className={clsx(styles.tableHeader, { [styles.disabled]: disabled })}>
+    {children}
+  </div>
+);
+
+export default TableHeader;
