66bb33f2e340c679e0b7ad5300e3811b09e7b872
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2021-10-29 18:23:12 +0200

    tree table action bar ts [improvement] (#210)
    * jsx to tsx
    
    * Typing.
    
    * Typing.
    
    * Typing.
    
    * Typing.
    
    * Export type.
    
    Co-authored-by: thr <thr@open.ch>
    Former-commit-id: ec88e5257a383b0ef53824ab11dbfdb1689581cc

---
+++ pyrene/src/components/TreeTable/TreeTableActionBar/TreeTableActionBar.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/prop-types */
+import React, { FunctionComponent } from 'react';
+
+import styles from './treeTableActionBar.css';
+import ButtonBar from '../../ButtonBar/ButtonBar';
+import Button from '../../Button/Button';
+import CheckboxPopover from '../../CheckboxPopover/CheckboxPopover';
+import { Item, OnItemClick } from '../../CheckboxPopover/CheckboxList';
+
+export interface TreeTableActionBarProps {
+  columnToggleProps: {
+    listItems: Array<Item>,
+    onItemClick: OnItemClick,
+    onRestoreDefault: () => void,
+    toggleColumns: boolean,
+  },
+  disabledExpand: boolean,
+  displayExpandAll: boolean,
+  renderRightItems?: () => JSX.Element,
+  toggleAll: () => void,
+}
+
+const TreeTableActionBar: FunctionComponent<TreeTableActionBarProps> = ({
+  columnToggleProps,
+  disabledExpand,
+  displayExpandAll,
+  renderRightItems,
+  toggleAll,
+}) => (
+  <div className={styles.treeTableActionBar}>
+    <ButtonBar
+      leftButtonSectionElements={[
+        <Button
+          label={displayExpandAll ? 'Expand All' : 'Collapse All'}
+          icon={displayExpandAll ? 'chevronDown' : 'chevronUp'}
+          type="action"
+          onClick={toggleAll}
+          disabled={disabledExpand}
+        />,
+      ]}
+      noPadding
+    />
+    <div className={styles.treeTableRightSideContainer}>
+      {renderRightItems?.()}
+      {columnToggleProps.toggleColumns && (
+        <CheckboxPopover
+          buttonLabel="Columns"
+          listItems={columnToggleProps.listItems}
+          onItemClick={columnToggleProps.onItemClick}
+          onRestoreDefault={columnToggleProps.onRestoreDefault}
+        />
+      )}
+    </div>
+  </div>
+);
+
+export default TreeTableActionBar;
