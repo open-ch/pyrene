cbb988fd37113e6f86172a05a3fcbf413a8aa127
thr <thr@open.ch>
2021-10-29 10:48:48 +0200

    Typing.

--- pyrene/src/components/TreeTable/TreeTableActionBar/TreeTableActionBar.tsx
+++ pyrene/src/components/TreeTable/TreeTableActionBar/TreeTableActionBar.tsx
@@ -1,56 +1,62 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+/* eslint-disable react/prop-types */
+import React, { FunctionComponent } from 'react';
 
 import styles from './treeTableActionBar.css';
 import ButtonBar from '../../ButtonBar/ButtonBar';
 import Button from '../../Button/Button';
 import CheckboxPopover from '../../CheckboxPopover/CheckboxPopover';
 
-const TreeTableActionBar = (props) => (
+interface Item {
+  id: string,
+  label: string,
+  value: boolean
+}
+
+export interface TreeTableActionBarProps {
+  columnToggleProps: {
+    listItems?: Array<Item>,
+    onItemClick?: () => void,
+    onRestoreDefault?: () => void,
+    toggleColumns?: boolean,
+  },
+  disabledExpand: boolean,
+  displayExpandAll: boolean,
+  renderRightItems?: () => void,
+  toggleAll: () => void,
+}
+
+const TreeTableActionBar: FunctionComponent<TreeTableActionBarProps> = ({
+  columnToggleProps,
+  disabledExpand,
+  displayExpandAll,
+  renderRightItems,
+  toggleAll,
+}) => (
   <div className={styles.treeTableActionBar}>
     <ButtonBar
       leftButtonSectionElements={[
         <Button
-          label={props.displayExpandAll ? 'Expand All' : 'Collapse All'}
-          icon={props.displayExpandAll ? 'chevronDown' : 'chevronUp'}
+          label={displayExpandAll ? 'Expand All' : 'Collapse All'}
+          icon={displayExpandAll ? 'chevronDown' : 'chevronUp'}
           type="action"
-          onClick={props.toggleAll}
-          disabled={props.disabledExpand}
+          onClick={toggleAll}
+          disabled={disabledExpand}
         />,
       ]}
       noPadding
     />
     <div className={styles.treeTableRightSideContainer}>
-      {props.renderRightItems && props.renderRightItems()}
-      {props.columnToggleProps.toggleColumns && (
+      {renderRightItems?.()}
+      {columnToggleProps.toggleColumns && (
         <CheckboxPopover
           buttonLabel="Columns"
-          listItems={props.columnToggleProps.listItems}
-          onItemClick={props.columnToggleProps.onItemClick}
-          onRestoreDefault={props.columnToggleProps.onRestoreDefault}
+          listItems={columnToggleProps.listItems}
+          onItemClick={columnToggleProps.onItemClick}
+          onRestoreDefault={columnToggleProps.onRestoreDefault}
         />
       )}
     </div>
   </div>
 );
-
-TreeTableActionBar.displayName = 'TreeTableActionBar';
-
-TreeTableActionBar.defaultProps = {
-  renderRightItems: null,
-};
-
-TreeTableActionBar.propTypes = {
-  columnToggleProps: PropTypes.shape({
-    listItems: PropTypes.array,
-    onItemClick: PropTypes.func,
-    onRestoreDefault: PropTypes.func,
-    toggleColumns: PropTypes.bool,
-  }).isRequired,
-  disabledExpand: PropTypes.bool.isRequired,
-  displayExpandAll: PropTypes.bool.isRequired,
-  renderRightItems: PropTypes.func,
-  toggleAll: PropTypes.func.isRequired,
-};
 
 export default TreeTableActionBar;
