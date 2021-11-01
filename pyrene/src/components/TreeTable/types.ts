9f572d71c504a6e7bf1eb83dd66f24ddc30691ec
Sakai-san <o_edo@hotmail.com>
2021-11-01 14:55:53 +0100

    Code formatting.

--- pyrene/src/components/TreeTable/types.ts
+++ pyrene/src/components/TreeTable/types.ts
@@ -2,14 +2,14 @@ import { CSSProperties } from 'react';
 
 export type CellValue = string | number | boolean;
 
-export type RowData<R> = ExtendedRow<R>;
-
 export type ExtendedRow<R> = R & {
   _getParent: () => ExtendedRow<R>,
   _rowId: string,
   _treeDepth: number,
   children?: Array<ExtendedRow<R>>
 };
+
+export type RowData<R> = ExtendedRow<R>;
 
 export interface Column<R> {
   accessor: keyof R,
