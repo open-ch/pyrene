0e372804a20df7a3a39cdc0fef9d71a3e3278c56
Sakai-san <o_edo@hotmail.com>
2021-11-04 22:28:08 +0100

    Code formatting.

--- pyrene/src/components/Badge/Badge.examples.tsx
+++ pyrene/src/components/Badge/Badge.examples.tsx
@@ -1,61 +1,60 @@
+/* eslint-disable no-alert */
 import { BadgeProps } from './Badge';
 import { Example } from '../../examples/Example';
 
-const examples: Example<BadgeProps> = {};
-
-examples.props = {
-  label: 'Security Alert',
-  maxWidth: 120,
-  type: 'danger',
-  onClick: () => { alert('Dangerous!'); }, // eslint-disable-line no-alert
+const examples: Example<BadgeProps> = {
+  props: {
+    label: 'Security Alert',
+    maxWidth: 120,
+    type: 'danger',
+    onClick: () => alert('Dangerous!'),
+  },
+  examples: [
+    {
+      props: {
+        label: 'Release Notes',
+        maxWidth: 100,
+        type: 'neutral',
+      },
+      description: 'Unclickable badge.',
+    },
+    {
+      props: {
+        label: 'More Information',
+        maxWidth: 80,
+        type: 'info',
+        onClick: () => alert('We want information!'),
+      },
+      description: 'Clickable badge alerting information.',
+    },
+    {
+      props: {
+        label: 'Success',
+        maxWidth: 100,
+        type: 'success',
+        onClick: () => alert('This is a success!'),
+      },
+      description: 'Clickable badge alerting success.',
+    },
+    {
+      props: {
+        label: 'Warning',
+        maxWidth: 100,
+        type: 'warning',
+      },
+      description: 'Unclickable badge.',
+    },
+    {
+      props: {
+        label: 'Danger',
+        maxWidth: 100,
+        type: 'danger',
+        onClick: () => alert('Dangerous!'),
+      },
+      description: 'Clickable badge alerting danger.',
+    },
+  ],
+  category: 'Other',
 };
-
-examples.examples = [
-  {
-    props: {
-      label: 'Release Notes',
-      maxWidth: 100,
-      type: 'neutral',
-    },
-    description: 'Unclickable badge.',
-  },
-  {
-    props: {
-      label: 'More Information',
-      maxWidth: 80,
-      type: 'info',
-      onClick: () => { alert('We want information!'); }, // eslint-disable-line no-alert
-    },
-    description: 'Clickable badge alerting information.',
-  },
-  {
-    props: {
-      label: 'Success',
-      maxWidth: 100,
-      type: 'success',
-      onClick: () => { alert('This is a success!'); }, // eslint-disable-line no-alert
-    },
-    description: 'Clickable badge alerting success.',
-  },
-  {
-    props: {
-      label: 'Warning',
-      maxWidth: 100,
-      type: 'warning',
-    },
-    description: 'Unclickable badge.',
-  },
-  {
-    props: {
-      label: 'Danger',
-      maxWidth: 100,
-      type: 'danger',
-      onClick: () => { alert('Dangerous!'); }, // eslint-disable-line no-alert
-    },
-    description: 'Clickable badge alerting danger.',
-  },
-];
-
-examples.category = 'Other';
 
 export default examples;
