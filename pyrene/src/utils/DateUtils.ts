74694b0c6ffe4bcf954689138f69258032f8e86f
Sanmi Awi <ola@open.ch>
2021-11-08 15:32:10 +0100

    Some JSDoc annotations

--- pyrene/src/utils/DateUtils.ts
+++ pyrene/src/utils/DateUtils.ts
@@ -42,24 +42,26 @@ export const allowedValueCheck = (valueToCheck:string) : boolean => (/^[0-9.: AP
 
 /**
  * Returns the Date Object of provided timestamp in provided timezone
+ * @param {number} timestamp
+ * @param {string} timezone
  */
 export const convertToZoneTime = (timestamp: number, timezone: string): Date => utcToZonedTime(timestamp, timezone);
 
 /**
  * Returns the Date Object of provided datetime parameter in UTC
+ * @param {string | number | Date} datetime
+ * @param {string} timezone
  */
 export const convertToUTCtime = (datetime: string | number | Date, timezone: string): Date => zonedTimeToUtc(datetime, timezone);
 
 /**
  * Returns the timestamp of a point in time in the future relative to now
- *
  * @param {Duration} duration
  */
 export const getFutureDate = (duration: Duration): number => add(new Date(), duration).getTime();
 
 /**
  * Returns the timestamp of a point in time in the past relative to now
- *
  * @param {Duration} duration
  */
 export const getPastDate = (duration: Duration): number => sub(new Date(), duration).getTime();
@@ -98,6 +100,12 @@ export const isValidTime = (time?: TimeType): boolean => {
   return false;
 };
 
+/**
+ * Returns a Date string formatted to the pattern parameter
+ * @param {Date | string} date
+ * @param {string} pattern
+ * @returns {string | undefined}
+ */
 export const customDateFormat = (date: Date | string, pattern: string): string | undefined => {
   if (typeof date === 'string') {
     const parsed = parse(date, pattern, new Date());
@@ -113,6 +121,13 @@ export const customDateFormat = (date: Date | string, pattern: string): string |
 
 export const customStringToDate = (datestring: string, pattern: string): Date => parse(datestring, pattern, new Date());
 
+/**
+ * Checks if a timestamp is within a certain range
+ * @param {number} timestampToCheck
+ * @param {number} minimumValue
+ * @param {number} maximumValue
+ * @returns {number}
+ */
 export const inRange = (timestampToCheck: number, minimumValue: number, maximumValue: number): number => {
   if (timestampToCheck < minimumValue) {
     return -1;
@@ -123,6 +138,11 @@ export const inRange = (timestampToCheck: number, minimumValue: number, maximumV
   return 0;
 };
 
+/**
+ * Returns the error string of a date validation object
+ * @param dateValObj
+ * @returns {string}
+ */
 export const getErrors = (dateValObj: DateValidationObject): string => {
   if (dateValObj.timeZone && !isValidTimeZone(dateValObj.timeZone)) {
     return 'Invalid time zone.';
@@ -167,6 +187,11 @@ export const getErrors = (dateValObj: DateValidationObject): string => {
   return '';
 };
 
+/**
+ * Checks if a date validation object has any errors
+ * @param dateValObj
+ * @returns {boolean}
+ */
 export const hasDateError = (dateValObj: DateValidationObject): boolean => {
   if (dateValObj && dateValObj.dateString != null) {
 
@@ -180,6 +205,12 @@ export const hasDateError = (dateValObj: DateValidationObject): boolean => {
   return true;
 };
 
+/**
+ * Checks if a time string has errors
+ * @param timestring
+ * @param timeFormat
+ * @returns {boolean}
+ */
 export const hasTimeError = (timestring: string, timeFormat: string): boolean => {
   if (timestring.length <= timeFormat.length) {
     if (timestring.length < timeFormat.length || (timestring.length === timeFormat.length && !Number.isNaN(customStringToDate(timestring, timeFormat).valueOf()))) {
@@ -189,4 +220,8 @@ export const hasTimeError = (timestring: string, timeFormat: string): boolean =>
   return true;
 };
 
+/**
+ * Returns the time zone string from a client's browser
+ * @returns {string}
+ */
 export const getClientTimeZone = (): string => Intl.DateTimeFormat().resolvedOptions().timeZone;
