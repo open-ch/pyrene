68cdc877f1da610897a63167bd77a994c82144e2
Sanmi Awi <ola@open.ch>
2021-11-08 13:20:29 +0100

    Add global Date utils file

---
+++ pyrene/src/utils/DateUtils.ts
@@ -0,0 +1,192 @@
+import isExists from 'date-fns/isExists';
+import sub from 'date-fns/sub';
+import add from 'date-fns/add';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import Duration from 'date-fns';
+
+import {
+  zonedTimeToUtc, utcToZonedTime,
+} from 'date-fns-tz';
+
+export enum DateUnits {
+  DAY = 'day',
+  MONTH = 'month',
+  YEAR = 'year',
+}
+
+export interface DateValidationObject {
+  dateString?: string,
+  isDateInvalid?: boolean,
+  isTimeInvalid?: boolean,
+  minimumValue?: number,
+  maximumValue?: number,
+  timeZone?: string,
+  dateFormat?: string,
+  timeString?: string,
+  timeFormat?: string,
+}
+
+export type DateType = {
+  day: number,
+  month: number,
+  year: number,
+};
+
+export type TimeType = {
+  minutes: number,
+  hours: number,
+};
+
+export const allowedValueCheck = (valueToCheck:string) : boolean => (/^[0-9.: APM]*$/.test(valueToCheck));
+
+/**
+ * Returns the Date Object of provided timestamp in provided timezone
+ */
+export const convertToZoneTime = (timestamp: number, timezone: string): Date => utcToZonedTime(timestamp, timezone);
+
+/**
+ * Returns the Date Object of provided datetime parameter in UTC
+ */
+export const convertToUTCtime = (datetime: string | number | Date, timezone: string): Date => zonedTimeToUtc(datetime, timezone);
+
+/**
+ * Returns the timestamp of a point in time in the future relative to now
+ *
+ * @param {Duration} duration
+ */
+export const getFutureDate = (duration: Duration): number => add(new Date(), duration).getTime();
+
+/**
+ * Returns the timestamp of a point in time in the past relative to now
+ *
+ * @param {Duration} duration
+ */
+export const getPastDate = (duration: Duration): number => sub(new Date(), duration).getTime();
+
+/** Checks if a timezone string is valid
+* @param {string} timezone
+*/
+export const isValidTimeZone = (timezone: string): boolean => {
+  try {
+    utcToZonedTime(new Date(), timezone);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+/**
+ * Checks if a timetype is valid
+ * @param {DateType} date
+ */
+export const isValidDate = (date?: DateType): boolean => {
+  if (date) {
+    return isExists(date.year, date.month - 1, date.day);
+  }
+  return false;
+};
+
+/**
+ * Checks if a timetype is valid
+ * @param {TimeType} time
+ */
+export const isValidTime = (time?: TimeType): boolean => {
+  if (time && time.hours >= 0 && time.hours <= 23 && time.minutes >= 0 && time.minutes <= 59) {
+    return true;
+  }
+  return false;
+};
+
+export const customDateFormat = (date: Date | string, pattern: string): string | undefined => {
+  if (typeof date === 'string') {
+    const parsed = parse(date, pattern, new Date());
+    return format(parsed, pattern);
+  }
+
+  if (date instanceof Date && date.getTime() != null) {
+    return format(date, pattern);
+  }
+
+  return undefined;
+};
+
+export const customStringToDate = (datestring: string, pattern: string): Date => parse(datestring, pattern, new Date());
+
+export const inRange = (timestampToCheck: number, minimumValue: number, maximumValue: number): number => {
+  if (timestampToCheck < minimumValue) {
+    return -1;
+  }
+  if (timestampToCheck > maximumValue) {
+    return 1;
+  }
+  return 0;
+};
+
+export const getErrors = (dateValObj: DateValidationObject): string => {
+  if (dateValObj.timeZone && !isValidTimeZone(dateValObj.timeZone)) {
+    return 'Invalid time zone.';
+  }
+
+  if (dateValObj.isDateInvalid && dateValObj.isTimeInvalid) {
+    return 'Invalid date & time format';
+  }
+  if (dateValObj.isDateInvalid) {
+    return 'Invalid date format';
+  }
+  if (dateValObj.isTimeInvalid) {
+    return 'Invalid time format';
+  }
+
+  if (dateValObj.dateString && dateValObj.dateString.length === dateValObj.dateFormat?.length && dateValObj.timeZone && dateValObj.dateFormat) {
+    const tmpDate = customStringToDate(dateValObj.dateString, dateValObj.dateFormat);
+    if (tmpDate) {
+      if (dateValObj.minimumValue != null && dateValObj.maximumValue != null) {
+        let datetimestring;
+        let datetimeformatstring;
+
+        if (dateValObj.timeFormat && dateValObj.timeString && dateValObj.timeString.length === dateValObj.timeFormat.length) {
+          datetimestring = `${dateValObj.dateString}${dateValObj.timeString}`;
+          datetimeformatstring = `${dateValObj.dateFormat}${dateValObj.timeFormat}`;
+        } else {
+          datetimestring = dateValObj.dateString;
+          datetimeformatstring = dateValObj.dateFormat;
+        }
+
+        const rangePositon = inRange(convertToUTCtime(customStringToDate(datetimestring, datetimeformatstring), dateValObj.timeZone).valueOf(), dateValObj.minimumValue, dateValObj.maximumValue);
+        if (rangePositon === -1) {
+          return 'Less than minimum date.';
+        }
+        if (rangePositon === 1) {
+          return 'Larger than maximum date.';
+        }
+      }
+    }
+  }
+
+  return '';
+};
+
+export const hasDateError = (dateValObj: DateValidationObject): boolean => {
+  if (dateValObj && dateValObj.dateString != null) {
+
+    if (dateValObj.dateFormat && dateValObj.dateString.length <= dateValObj.dateFormat.length) {
+      if ((dateValObj.dateString.length < dateValObj.dateFormat.length || (dateValObj.dateString.length === dateValObj.dateFormat.length && !Number.isNaN(customStringToDate(dateValObj.dateString, dateValObj.dateFormat).valueOf())))) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+export const hasTimeError = (timestring: string, timeFormat: string): boolean => {
+  if (timestring.length <= timeFormat.length) {
+    if (timestring.length < timeFormat.length || (timestring.length === timeFormat.length && !Number.isNaN(customStringToDate(timestring, timeFormat).valueOf()))) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const getClientTimeZone = (): string => Intl.DateTimeFormat().resolvedOptions().timeZone;
