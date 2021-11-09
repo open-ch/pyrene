5bf5b5869dc31e2a2afb13d88bd8e42f71411ff1
Sakai-san <o_edo@hotmail.com>
2021-11-09 13:08:31 +0100

    Revert changes done in PR-160

--- pyrene-graphs/webpack.config.babel.js
+++ pyrene-graphs/webpack.config.babel.js
@@ -51,7 +51,7 @@ const config = {
         ],
       },
       {
-        test: /(node_modules).*(\.css|\.module\.css)$/,
+        test: /(node_modules).*\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
