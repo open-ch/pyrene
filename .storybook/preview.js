9b2f757884513960614b92453ca070f6e699cddb
Eleni Mandilara <madelinegr@gmail.com>
2022-01-28 11:48:39 +0100

    Create a Kitchensink theme for Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import '../pyrene/src/styles/common.module.css';
 import '../pyrene/src/styles/colors.module.css';
 import '../pyrene/src/styles/fonts.module.css';
+import kitchensinkTheme from './KitchensinkTheme';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,5 +10,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  docs: {
+    theme: kitchensinkTheme,
   },
 }
