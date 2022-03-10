cb69314d71c2b93b323f1be865ef349356068061
Yann Bolliger <yann@bolliger.io>
2022-03-10 14:48:18 +0100

    Migrate Collapsible to Storybook
    GitOrigin-RevId: 01846b43088d7667e45eafa50a3090498d0264fe

---
+++ pyrene/src/components/Collapsible/Collapsible.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import Collapsible, { CollapsibleProps } from './Collapsible';
+import Placeholder from '../../examples/Placeholder';
+
+export default {
+  title: 'Components/Layout/Collapsible',
+  component: Collapsible,
+} as Meta;
+
+const Template: Story<CollapsibleProps> = (args) => <Collapsible {...args} />;
+
+export const Simple = Template.bind({});
+Simple.args = {
+  renderCallback: () => <Placeholder />,
+  defaultExpanded: true,
+};
