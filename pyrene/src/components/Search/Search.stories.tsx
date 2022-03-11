59086b09877aa12c96761f0cb609fb3de7398445
Karolina Migon <karomigon@gmail.com>
2022-03-11 12:11:22 +0100

    pyrene migrate search
    GitOrigin-RevId: e37bdecdd93ec0b291a0bc37ace2148b0c84e7bd

---
+++ pyrene/src/components/Search/Search.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { useArgs } from '@storybook/client-api';
+import Search, { SearchProps } from './Search';
+
+export default {
+  title: 'Components/Form/Search',
+  component: Search,
+  args: {
+    value: '',
+    isFocused: true,
+    placeholder: 'Search for...',
+  },
+} as Meta;
+
+const Template: Story<SearchProps> = ({ onChange, ...args }) => {
+  const [, updateArgs] = useArgs();
+  const changeSearchValue = (value: string) => updateArgs({ value: value });
+  return <Search onChange={changeSearchValue} {...args} />;
+};
+
+export const Simple = Template.bind({});
