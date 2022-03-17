e5f910e7cb28aa8da1dc8f78b0236a48fd4c7459
Yann Bolliger <yann@bolliger.io>
2022-03-17 17:42:48 +0100

    fix jest
    GitOrigin-RevId: 3569ddddf99cb60eea012be0f7e3027a809810f1

--- pyrene/jest.bamboo.config.js
+++
@@ -1,5 +0,0 @@
-const baseConf = require('./jest.config');
-
-
-// eslint-disable-next-line prefer-object-spread
-module.exports = Object.assign({}, baseConf, { testResultsProcessor: 'jest-bamboo-formatter' });
