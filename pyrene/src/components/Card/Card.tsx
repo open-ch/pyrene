a8bd5c02739488ff5f5261e21761f35febb98b96
Karolina Migon <karomigon@gmail.com>
2022-03-29 22:28:00 +0200

    add ref to Card component
    GitOrigin-RevId: 82b89cf6cfb21824b078e21fe37e93fe80949577

--- pyrene/src/components/Card/Card.tsx
+++ pyrene/src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, FunctionComponent } from 'react';
+import React, { ReactNode, forwardRef } from 'react';
 import clsx from 'clsx';
 
 import Loader from '../Loader/Loader';
@@ -9,61 +9,65 @@ export interface CardProps {
   /**
    The content enclosed in the card.
    */
-  children: ReactNode,
+  children: ReactNode;
   /**
    * An error to be shown rather than the content.
    */
-  error?: string,
+  error?: string;
   /**
    * An optional footer component.
    */
-  footer?: ReactNode,
+  footer?: ReactNode;
   /**
-    * An optional header component, e.g., a time range selector.
-    */
-  header?: ReactNode,
+   * An optional header component, e.g., a time range selector.
+   */
+  header?: ReactNode;
   /**
    * Indicates whether the card is loading. Displays an overlay loader.
    */
-  loading?: boolean,
+  loading?: boolean;
   /**
    * Sets spacing between card content and edge
    */
-  paddingSize?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge',
+  paddingSize?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
 }
 
 /**
  * Cards are containers for content.
  *
  * They take the full width of the enclosing box and the height they need to fit the content.
-*/
-const Card: FunctionComponent<CardProps> = ({
-  header,
-  footer,
-  children,
-  loading = false,
-  error,
-  paddingSize = 'large',
-}: CardProps) => (
-  <div className={clsx(styles.container, styles[paddingSize])}>
-    {header && <div className={styles.header}>{header}</div>}
-    <div className={clsx(styles.content, {
-      [styles['content--noHeader']]: !header,
-      [styles['content--noFooter']]: !footer,
-    })}
-    >
-      {/* eslint-disable-next-line no-nested-ternary */}
-      {error ? <div className={styles.error}><Banner type="error" styling="standard" label={error} /></div>
-        : loading ? (
+ */
+const Card = forwardRef<HTMLDivElement, CardProps>(
+  (
+    { header, footer, children, loading = false, error, paddingSize = 'large' },
+    ref
+  ) => (
+    <div ref={ref} className={clsx(styles.container, styles[paddingSize])}>
+      {header && <div className={styles.header}>{header}</div>}
+      <div
+        className={clsx(styles.content, {
+          [styles['content--noHeader']]: !header,
+          [styles['content--noFooter']]: !footer,
+        })}
+      >
+        {/* eslint-disable-next-line no-nested-ternary */}
+        {error ? (
+          <div className={styles.error}>
+            <Banner type="error" styling="standard" label={error} />
+          </div>
+        ) : loading ? (
           <div className={styles.loader}>
             <div className={styles.loadingOverlay}>
               <Loader size="large" />
             </div>
           </div>
-        ) : children}
+        ) : (
+          children
+        )}
+      </div>
+      {footer && <div className={styles.footer}>{footer}</div>}
     </div>
-    {footer && <div className={styles.footer}>{footer}</div>}
-  </div>
+  )
 );
 
 Card.displayName = 'Card';
