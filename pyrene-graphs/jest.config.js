eea178df6bbcb1c0df5b76c0cfbab878dcdb650c
Maxime Lovino <maximelovino@gmail.com>
2022-06-01 14:00:47 +0200

    Use global deps for pyrene and pyrene graphs
    GitOrigin-RevId: 46c8f403e086f2698c781a7a2e13013c6a988cb8

--- pyrene-graphs/jest.config.js
+++ pyrene-graphs/jest.config.js
@@ -61,9 +61,9 @@ module.exports = {
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // This is needed so that only the root node_modules is used, not the closest one
-  moduleDirectories: [
-    '<rootDir>/node_modules',
-  ],
+  // moduleDirectories: [
+  //   '<rootDir>/node_modules',
+  // ],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
--- tuktuktwo/webpack.config.babel.js
+++ tuktuktwo/webpack.config.babel.js
@@ -1,4 +1,4 @@
-import path from 'path';
+const path = require('path');
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
@@ -7,7 +7,16 @@ const production = process.env.NODE_ENV === 'production';
 
 const OUTPUT_PATH = path.resolve(__dirname, 'dist');
 
-const config = {
+const entry = production ? {
+  main: './src/index.ts',
+  min: './src/index.ts',
+} : {
+  main: './src/index.ts',
+  min: './src/index.ts',
+  dev: './src/index.ts',
+};
+
+module.exports = () => ({
   mode: production ? 'production' : 'development',
   devtool: production ? undefined : 'source-map',
   resolve: {
@@ -48,18 +57,5 @@ const config = {
       },
     }),
   ],
-};
-
-if (production) {
-  console.warn('webpack is running in production mode\n'); // eslint-disable-line no-console
-} else {
-  console.warn('webpack is running in development mode\n'); // eslint-disable-line no-console
-
-  config.entry = {
-    main: './src/index.ts',
-    min: './src/index.ts',
-    dev: './src/index.ts',
-  };
-}
-
-export default config;
+  entry
+});
