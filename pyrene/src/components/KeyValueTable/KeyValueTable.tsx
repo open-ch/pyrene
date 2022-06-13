c22c58921cfc01f8ef74b42a36a9696fcb705336
Karolina Migon <karomigon@gmail.com>
2022-06-13 16:40:27 +0200

    KeyValueTable create new theme
    GitOrigin-RevId: 5c044b9fca868ae6f7837d460904ad1e13ca809f

--- pyrene/src/components/KeyValueTable/KeyValueTable.tsx
+++ pyrene/src/components/KeyValueTable/KeyValueTable.tsx
@@ -1,4 +1,6 @@
 import React, { CSSProperties, ReactNode } from 'react';
+import clsx from 'clsx';
+
 import styles from './KeyValueTable.module.css';
 
 type Row = {
@@ -12,40 +14,45 @@ export interface KeyValueTableProps {
   /**
    * Rows definition: { key: 'key', value: 'value', rowStyle: {} }, where rowStyle is an object with css properties applied to the whole row
    */
-  rows?: Row[]
+  rows?: Row[];
   /**
    * Title of the table.
    */
   title?: string;
+  /**
+   * Theme of the table.
+   */
+  theme?: 'greyLabel' | 'border';
 }
 
 const KeyValueTable: React.FC<KeyValueTableProps> = ({
   title = '',
   rows = [{ key: 'key', value: 'value' }],
   keyWidth = 256,
+  theme = 'border',
 }: KeyValueTableProps) => (
   <div className={styles.keyValueTable}>
     {title && (
-      <div className={styles.keyValueTableTitle}>
-        {title}
-      </div>
+      <div className={clsx(styles.keyValueTableTitle, styles[`title-${theme}`])}>{title}</div>
     )}
     <table className={styles.keyValueBody}>
       <tbody>
-        {rows && rows.map((row) => (
-          <tr
-            className={styles.keyValueRow}
-            style={row.rowStyle}
-            key={row.key}
-          >
-            <td className={styles.keyValueCellKey} style={{ width: keyWidth, minWidth: keyWidth, maxWidth: keyWidth }}>
-              {row.key}
-            </td>
-            <td className={styles.keyValueCellValue}>
-              {row.value}
-            </td>
-          </tr>
-        ))}
+        {rows &&
+          rows.map((row) => (
+            <tr
+              className={clsx(styles.keyValueRow, styles[`row-${theme}`])}
+              style={row.rowStyle}
+              key={row.key}
+            >
+              <td
+                className={clsx(styles.keyValueCellKey, styles[`key-${theme}`])}
+                style={{ width: keyWidth, minWidth: keyWidth, maxWidth: keyWidth }}
+              >
+                {row.key}
+              </td>
+              <td className={styles.keyValueCellValue}>{row.value}</td>
+            </tr>
+          ))}
       </tbody>
     </table>
   </div>
