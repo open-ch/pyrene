e96f4e4a54225754fdfb1f519617c6f78a4a88cb
Karolina Migon <karomigon@gmail.com>
2022-07-08 16:22:32 +0200

    change Heading padding-bottom
    GitOrigin-RevId: f17ba1b42a6179df217e17fa9fd1dd1fe88265be

--- pyrene/src/components/Heading/Heading.tsx
+++ pyrene/src/components/Heading/Heading.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import clsx from 'clsx';
 
 import styles from './Heading.module.css';
+import Icon, { IconProps } from '../Icon/Icon';
+import { IconNames } from '../types';
 
 export interface HeadingProps {
   /**
@@ -12,13 +14,42 @@ export interface HeadingProps {
    * Size level, corresponds to h1 - h4.
    */
   level?: 1 | 2 | 3 | 4;
+  /**
+   * Icon that goes before the text
+   */
+  icon?: keyof IconNames;
+  /**
+   * Sets the URL of the svg file.
+   */
+  svg?: string;
+  /**
+   * Sets the icon color. ( Hint: see colorConstants.js)
+   */
+  iconColor?: string;
 }
 
+// get the icon size based on Heading level
+const mapIconSize = { 1: 'large', 2: 'large', 3: 'medium', 4: 'medium' } as Record<
+  number,
+  IconProps['size']
+>;
 /**
  * Section or page titles.
  */
-const Heading: React.FC<HeadingProps> = ({ children, level = 1 }: HeadingProps) => (
-  <div className={clsx(styles[`heading${level}`], styles.heading)} title={children}>
+const Heading: React.FC<HeadingProps> = ({
+  children,
+  icon,
+  svg,
+  level = 1,
+  iconColor,
+}: HeadingProps) => (
+  <div
+    className={clsx(styles[`heading${level}`], styles.heading, (icon || svg) && styles.withIcon)}
+    title={children}
+  >
+    {(icon || svg) && (
+      <Icon type="standalone" color={iconColor} name={icon} svg={svg} size={mapIconSize[level]} />
+    )}
     {children}
   </div>
 );
